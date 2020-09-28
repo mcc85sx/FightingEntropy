2df72572ce2829de053081a92972a99b1d62acb2
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-09-28 03:37:36 -0400

    Update Xaml.ps1

--- 2020.09.0/Classes/Xaml.ps1
+++ 2020.09.0/Classes/Xaml.ps1
@@ -1,38 +1,42 @@
+Add-Type -AssemblyName PresentationCore
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName WindowsBase
+
 Class XamlWindow # Originally based on Dr. Weltner's work, but also Jason Adkinson from Pluralsight (His method wasn't able to PassThru variables)
 {
-    Hidden [String]       $Xaml
-    [String[]]           $Names 
-    [String[]]        $Assembly = "PresentationFramework PresentationCore WindowsBase".Split(" ")
-    [XML.XMLReader]       $Node
-    [Object]              $Host
-    [Windows.Window]        $IO
+    Hidden [String]        $Xaml
+    [String[]]            $Names 
+    [String[]]         $Assembly = ("PresentationFramework PresentationCore WindowsBase" -Split " ")
+    [XML.XMLReader]        $Node
+    [Object]               $Host
+    [Windows.Window]         $IO
 
     XamlWindow([String]$XAML)
     {
-        If ( !$Xaml ) 
+        If ( !$Xaml )
         {
             Throw "Invalid XAML Input"
         }
 
-        $This.Xaml              = $Xaml
-        $This.Names             = ([Regex]"((Name)\s*=\s*('|`")\w+('|`"))").Matches($This.Xaml).Value | % { $_ -Replace "(Name|=|'|`"|\s)","" } | Select-Object -Unique
-        $This.Assembly          | % { Add-Type -AssemblyName $_ }
-        $This.Node              = [XML.XMLReader]::Create([IO.StringReader]$This.Xaml)
-        $This.Host              = [Windows.Markup.XAMLReader]::Load($This.Node)
+        $This.Xaml               = $Xaml
+        $This.Names              = ([Regex]"((Name)\s*=\s*('|`")\w+('|`"))").Matches($This.Xaml).Value | % { $_ -Replace "(Name|=|'|`"|\s)","" } | Select-Object -Unique
+        $This.Assembly           | % { Add-Type -AssemblyName $_ -Verbose }
+        $This.Node               = [XML.XMLReader]::Create([IO.StringReader]$This.Xaml)
+        $This.Host               = [Windows.Markup.XAMLReader]::Load($This.Node)
     
         ForEach ( $I in 0..( $This.Names.Count - 1 ) )
         {
-            $This.Host          | Add-Member -MemberType NoteProperty -Name $This.Names[$I] -Value $This.Host.FindName($This.Names[$I]) -Force 
+            $This.Host           | Add-Member -MemberType NoteProperty -Name $This.Names[$I] -Value $This.Host.FindName($This.Names[$I]) -Force 
         }
 
-        $This.IO                = [Windows.Window]::new()
+        $This.IO                 = [Windows.Window]::new()
     }
 
     Invoke()
     {
         $This.IO.Dispatcher.InvokeAsync({
         
-            $Output             = $This.Host.ShowDialog()
+            $Output              = $This.Host.ShowDialog()
             Set-Variable -Name Output -Value $Output -Scope Global
 
         }).Wait()

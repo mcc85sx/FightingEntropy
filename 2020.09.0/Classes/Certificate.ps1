7b173448a6c45296b7b439da1bffe319933ceb19
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-01 16:31:12 -0400

    Update Certificate.ps1

--- 2020.09.0/Classes/Certificate.ps1
+++ 2020.09.0/Classes/Certificate.ps1
@@ -1,37 +1,41 @@
-Class Certificate
-{
-    [String]       $ExternalIP
-    [String]     $Organization
-    [String]       $CommonName
-    [String]         $Location
-    [String]           $Region
-    [String]          $Country
-    [Int32]            $Postal
-    [String]         $TimeZone
-    [String]         $SiteLink
 
-    Certificate(
-    [String]     $Organization ,
-    [String]       $CommonName )
+    Class Certificate
     {
-        If ( ! ( Test-Connection 1.1.1.1 -Count 1 ) ) 
-        { 
-            Throw "Unable to verify internet connection" 
-        }
-            
-        [Net.ServicePointManager]::SecurityProtocol = 3072
-            
-        # This line is all Chrissie Lamaire's
-        Invoke-RestMethod http://ipinfo.io/$( Invoke-WebRequest http://ifconfig.me/ip | % Content ) -Method Get | % { 
+        [String]       $ExternalIP
+        Hidden [Object]      $Ping
+        [String]     $Organization
+        [String]       $CommonName
+        [String]         $Location
+        [String]           $Region
+        [String]          $Country
+        [Int32]            $Postal
+        [String]         $TimeZone
+        [String]         $SiteLink
 
-            $This.ExternalIP   = $_.IP
-            $This.Organization = $Organization
-            $This.CommonName   = $CommonName
-            $This.Location     = $_.City
-            $This.Region       = $_.Region
-            $This.Country      = $_.Country
-            $This.Postal       = $_.Postal
-            $This.TimeZone     = $_.TimeZone
+        Certificate(
+        [String]     $Organization ,
+        [String]       $CommonName )
+        {
+            If ( ! ( Test-Connection 1.1.1.1 -Count 1 ) ) 
+            { 
+                Throw "Unable to verify internet connection" 
+            }
+            
+            [Net.ServicePointManager]::SecurityProtocol = 3072
+            
+            # This (2) lines from Chrissie Lamaire's script, 
+            # https://gallery.technet.microsoft.com/scriptcenter/Get-ExternalPublic-IP-c1b601bb
+
+            $This.ExternalIP       = Invoke-WebRequest http://ifconfig.me/ip | % Content 
+            $This.Ping             = Invoke-RestMethod http://ipinfo.io/$( $This.ExternalIP ) -Method Get
+            $This.Organization     = $Organization
+            $This.CommonName       = $CommonName
+            $This.Location         = $This.Ping.City
+            $This.Region           = $This.Ping.Region
+            $This.Country          = $This.Ping.Country
+            $This.Postal           = $This.Ping.Postal
+            $This.TimeZone         = $This.Ping.TimeZone
         }
     }
-}
+    
+    [Certificate]::New("Secure Digits Plus LLC","securedigitsplus.com")

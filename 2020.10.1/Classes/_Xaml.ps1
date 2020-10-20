d5cedb370e5fa6a08d22ea99c78b42e0e9bc39c1
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-19 23:38:07 -0400

    Update _Xaml.ps1

--- 2020.10.1/Classes/_Xaml.ps1
+++ 2020.10.1/Classes/_Xaml.ps1
@@ -2,7 +2,8 @@ Class _Xaml
 {
     [String] $Author = "Secure Digits Plus LLC"
     [String] $Schema = "http://schemas.microsoft.com/winfx/2006/xaml"
-    [Object] $Info   = [_Info]::().Caption
+    [Object] $Info   = [_Info]::New().Caption
+    [Object] $Glossary
 
     _Xaml([String]$Xaml)
     {

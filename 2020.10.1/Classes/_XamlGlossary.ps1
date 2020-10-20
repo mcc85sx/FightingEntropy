e7427a35d3f1c46661b439f0e2c0af8d9551256d
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-19 22:44:52 -0400

    Create _XamlGlossary.ps1

---
+++ 2020.10.1/Classes/_XamlGlossary.ps1
@@ -0,0 +1,13 @@
+Class _XamlGlossaryItem
+{
+    [Int32]      $Index
+    [String]  $Variable
+    [String]      $Name
+
+    _XamlGlossaryItem([Int32]$Index,[String]$Variable,[String]$Name)
+    {
+        $This.Index    = $Index
+        $This.Variable = $Variable
+        $This.Name     = $Name
+    }
+}

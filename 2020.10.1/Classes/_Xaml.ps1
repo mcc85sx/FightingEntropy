636ce22a6a321633792872d49ab87812a2937623
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-19 22:41:30 -0400

    Create _Xaml.ps1

---
+++ 2020.10.1/Classes/_Xaml.ps1
@@ -0,0 +1,13 @@
+Class _Xaml
+{
+    [String] $Author = "Secure Digits Plus LLC"
+    [String] $Schema = "http://schemas.microsoft.com/winfx/2006/xaml"
+    [Object] $Info   = [_Info]::().Caption
+
+    _Xaml([String]$Xaml)
+    {
+
+    }
+
+    Format(){}
+}

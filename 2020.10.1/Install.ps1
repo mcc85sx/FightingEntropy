22e9343a58b8024c1e9e26599c88903f850c1b5d
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-25 03:07:31 -0700

    Update Install.ps1

--- 2020.10.1/Install.ps1
+++ 2020.10.1/Install.ps1
@@ -1,8 +1,19 @@
 Class Install
 {
     Hidden [Object]  $Master
-    [String]           $Path = "$Env:ProgramData\Secure Digits Plus LLC\FightingEntropy\2020.10.1"
+    [String]           $Name = "FightingEntropy"
+    [String]        $Version = "2020.10.1"
+    [String]       $Provider = "Secure Digits Plus LLC"
+    [String]         $Author = "Michael C. Cook Sr."
+    [String]           $Path
     [String]            $URL
+    [Object]           $File
+    [Object]       $Manifest
+    [Object[]]      $Classes
+    [Object[]]      $Control
+    [Object[]]    $Functions
+    [Object[]]     $Graphics
+    [String[]]         $Load
 
     Hidden [Hashtable] $Hash = @{ 
 
@@ -24,18 +35,15 @@ Class Install
                      
         Graphics  = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp") -Split " "
     }
-
-    [Object[]]   $Classes
-    [Object[]]   $Control
-    [Object[]] $Functions
-    [Object[]]  $Graphics
-    [String[]]      $Load
     
     Install([String]$URL)
     {
         [Net.ServicePointManager]::SecurityProtocol = 3072
 
         $This.URL         = $URL
+        $This.Path        = $env:ProgramData, $This.Provider, $This.Name, $This.Version -join '\'
+        $This.File        = $This.Path, "FightingEntropy.psm1" -join '\'
+        $This.Manifest    = $This.Path, "FightingEntropy.psd1" -join '\'
 
         ForEach ( $I in $This.Hash.Folders )
         {
@@ -53,51 +61,55 @@ Class Install
                     Set-Content -Path "$($This.Path)\Classes\__index.txt" -Value $This.Hash.Classes -Force
                     ForEach ( $X in $This.Hash.Classes )
                     {
-                        $File            = "$($This.Path)\Classes\$X.ps1"
+                        $iFile           = "$($This.Path)\Classes\$X.ps1"
                         $Link            = "$($This.URL )/Classes/$X.ps1"
 
-                        Invoke-RestMethod -URI $Link -Outfile $File -Verbose
+                        Invoke-RestMethod -URI $Link -Outfile $iFile -Verbose
 
-                        $This.Classes   += $File
+                        $This.Classes   += $iFile
                     }
                 }
 
                 Functions
                 {
+                    Set-Content -Path "$($This.Path)\Functions\__index.txt" -Value $This.Hash.Functions -Force
                     ForEach ( $X in $This.Hash.Functions )
                     {
-                        $File            = "$($This.Path)\Functions\$X.ps1"
+                        $iFile           = "$($This.Path)\Functions\$X.ps1"
                         $Link            = "$($This.URL )/Functions/$X.ps1"
 
-                        Invoke-RestMethod -URI $Link -Outfile $File -Verbose
+                        Invoke-RestMethod -URI $Link -Outfile $iFile -Verbose
 
-                        $This.Functions += $File
+                        $This.Functions += $iFile
                     }
                 }
 
                 Control
                 {
+                    Set-Content -Path "$($This.Path)\Control\__index.txt" -Value $This.Hash.Control -Force
                     ForEach ( $X in $This.Hash.Control )
                     {
-                        $File            = "$($This.Path)\Control\$X"
+                        $iFile           = "$($This.Path)\Control\$X"
                         $Link            = "$($This.URL )/Control/$X"
 
-                        Invoke-RestMethod -URI $Link -OutFile $File -Verbose
+                        Invoke-RestMethod -URI $Link -OutFile $iFile -Verbose
 
-                        $This.Control   += $File
+                        $This.Control   += $iFile
                     }
                 }
 
                 Graphics
                 {
+                    Set-Content -Path "$($This.Path)\Graphics\__index.txt" -Value $This.Hash.Functions -Force
+
                     ForEach ( $X in $This.Hash.Graphics )
                     {
-                        $File            = "$($This.Path)\Graphics\$X"
+                        $iFile           = "$($This.Path)\Graphics\$X"
                         $Link            = "$($This.URL )/Graphics/$X"
 
-                        Invoke-RestMethod -URI $Link -OutFile $File -Verbose
+                        Invoke-RestMethod -URI $Link -OutFile $iFile -Verbose
 
-                        $This.Graphics  += $File
+                        $This.Graphics  += $iFile
                     }
                 }
             }
@@ -120,24 +132,27 @@ Class Install
             $This.Load                  += ""
         }
 
-        $This.Master                     =  ( $This.Load -join "`n" )
+        $This.Master                     = $This.Load -join "`n"
 
-        Set-Content -Path "$($This.Path)\FightingEntropy.psm1" -Value $This.Master
-        Import-Module "$($This.Path)\FightingEntropy.psm1" -Verbose
+        Set-Content -Path $This.File -Value $This.Master
+
+        Import-Module $This.File -Verbose
 
         $Module                          = Get-FEModule
         
         @{ 
             GUID                          = "e21f2e0e-36f4-4a22-9094-9206dcef9365"
-            Path                          = $Module.Manifest
-            ModuleVersion                 = $Module.Version
+            Path                          = $This.Manifest
+            ModuleVersion                 = $This.Version
             Copyright                     = "(c) 2020 mcc85sx. All rights reserved."
             CompanyName                   = "Secure Digits Plus LLC" 
             Author                        = "mcc85sx / Michael C. Cook Sr."
             Description                   = "Beginning the fight against Identity Theft, and Cybercriminal Activities"
-            RootModule                    = $Module.File
+            RootModule                    = $This.File
 
         }                                 | % { New-ModuleManifest @_ }
+
+        Write-Theme "Module [+] Loaded"
     }
 }
 
@@ -145,11 +160,11 @@ Add-Type -AssemblyName PresentationFramework
 $Return = [Install]::new("https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.10.1")
 
 @{  
-    Type                          = 4
-    Image                         = "https://raw.githubusercontent.com/secure-digits-plus-llc/FightingEntropy/master/Graphics/logo.jpg"
-    GUID                          = New-GUID
-    Header                        = "Secure Digits Plus LLC"
-    Body                          = "FightingEntropy"
-    Footer                        = "2020.10.1"
-
-}                                 | % { Show-ToastNotification @_ }
+    Type        = 4
+    Image       = "https://raw.githubusercontent.com/secure-digits-plus-llc/FightingEntropy/master/Graphics/logo.jpg"
+    GUID        = New-GUID
+    Header      = "Secure Digits Plus LLC"
+    Body        = "FightingEntropy"
+    Footer      = "2020.10.1"
+    
+}               | % { Show-ToastNotification @_ }

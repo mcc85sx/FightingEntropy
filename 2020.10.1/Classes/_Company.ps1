77bdce80f3e15145caf518eff4ca35d19d752828
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-25 04:59:19 -0700

    Update _Company.ps1

--- 2020.10.1/Classes/_Company.ps1
+++ 2020.10.1/Classes/_Company.ps1
@@ -1,23 +1,39 @@
-﻿Class _Company
+Class _Company
 {
     [String] $Name
     [String] $Branch
-    [String] $Background
-    [String] $Logo
+    [Object] $Background
+    [Object] $Logo
     [String] $Phone
     [String] $Website
     [String] $Hours
 
-    _Company() {}
-
-    Load([String]$Name,[String]$Branch,[String]$Background,[String]$Logo,[String]$Phone,[String]$Website,[String]$Hours)
-    { 
+    _Company([String]$Name,[String]$Branch,[String]$Phone,[String]$Website,[String]$Hours) 
+    {
         $This.Name       = $Name
         $This.Branch     = $Branch
-        $This.Background = $Background
-        $This.Logo       = $Logo
         $This.Phone      = $Phone
         $This.Website    = $Website
         $This.Hours      = $Hours
+    }
+
+    GetLogo([String]$Path)
+    {
+        If ( ! ( Test-Path -Path $Path ) )
+        {
+            Throw "Invalid Path"
+        }
+
+        $This.Logo       = $Path
+    }
+
+    GetBackground([String]$Path)
+    {
+        If ( ! ( Test-Path -Path $Path ) )
+        {
+            Throw "Invalid Path"
+        }
+
+        $This.Background = $Path
     }
 }

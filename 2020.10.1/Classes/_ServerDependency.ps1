48953d96a3438e6d694b0dd7518447b6ee310855
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-28 12:28:55 -0700

    Update _ServerDependency.ps1

--- 2020.10.1/Classes/_ServerDependency.ps1
+++ 2020.10.1/Classes/_ServerDependency.ps1
@@ -1,8 +1,7 @@
-
 Class _ServerDependency
 {
     [Object]         $Registry = @( "" , "\WOW6432Node" | % { "HKLM:\SOFTWARE$_\Microsoft\Windows\CurrentVersion\Uninstall\*"  } )
-    [Object]             $Root = ([_Module]::New().Path)
+    [Object]             $Root = (Get-FEModule | % Path)
     [Object[]]       $Packages = @(@{
         
         Name                   = "MDT"
@@ -21,7 +20,7 @@ Class _ServerDependency
         Resource               = "https://go.microsoft.com/fwlink/?linkid=2086042"
         Path                   = "{0}\Tools\WinADK"
         File                   = "winadk1903.exe"
-        Arguments              = "/quiet /norestart /log $env:temp\win_adk.log /features +" 
+        Arguments              = "/quiet /norestart /log $env:temp\winadk.log /features +" 
         
     };@{  
         
@@ -31,7 +30,7 @@ Class _ServerDependency
         Resource               = "https://go.microsoft.com/fwlink/?linkid=2087112"
         Path                   = "{0}\Tools\WinPE"
         File                   = "winpe1903.exe"
-        Arguments              = "/quiet /norestart /log $env:temp\win_adk.log /features +" 
+        Arguments              = "/quiet /norestart /log $env:temp\winpe.log /features +" 
     })
 
     _ServerDependency()
@@ -43,21 +42,21 @@ Class _ServerDependency
 
         $Uninstall             = Get-ItemProperty $This.Registry
 
-        ForEach ( $I in 0..2 ) 
+        ForEach ( $Package in $This.Packages )
         {
-            $Item              = $Uninstall | ? DisplayName -match $This.Packages[$I].DisplayName
+            $Item              = $Uninstall | ? DisplayName -match $Package.DisplayName
 
-            If ( $Item -eq $Null -or $Item.DisplayVersion -lt $This.Packages[$I].Version )
+            If ( $Item -eq $Null -or $Item.DisplayVersion -lt $Package.Version )
             {
-                Write-Host ( "Installing {0}" -f $This.Packages[$I].DisplayName )
+                Write-Host ( "Installing {0}" -f $Package.DisplayName )
 
-                $Name        = $This.Packages[$I].Name
-                $DisplayName = $This.Packages[$I].DisplayName
-                $Version     = $This.Packages[$I].Version
-                $Resource    = $This.Packages[$I].Resource
-                $Path        = $This.Packages[$I].Path -f $This.Root
-                $File        = $This.Packages[$I].File
-                $Arguments   = $This.Packages[$I].Arguments
+                $Name        = $Package.Name
+                $DisplayName = $Package.DisplayName
+                $Version     = $Package.Version
+                $Resource    = $Package.Resource
+                $Path        = $Package.Path -f (Get-FEModule | % Path)
+                $File        = $Package.File
+                $Arguments   = $Package.Arguments
 
                 [Net.ServicePointManager]::SecurityProtocol = 3072
 
@@ -68,23 +67,21 @@ Class _ServerDependency
 
                 Invoke-RestMethod -URI $Resource -OutFile "$Path\$File"
 
-                $Process          = Start-Process -FilePath "$Path\$File" -ArgumentList $Arguments -WorkingDirectory $Path -PassThru
+                $Process          = Start-Process -FilePath "$Path\$File" -ArgumentList $Arguments -PassThru
 
-                For ( $X = 0; $X -le 100; $X++ )
+                While ( ! ( $Process.HasExited ) )
                 {
-                    Write-Progress -Activity "[Installing] @: $($Name)" -PercentComplete $X -Status "$X% Complete"
-                    Sleep -M 250
-
-                    If ( $Process.HasExited )
+                    For ( $X = 0; $X -le 100; $X++ )
                     {
-                        Write-Progress -Activity "[Installed] @: $($Name)" -Completed
+                        Write-Progress -Activity "[Installing] @: $($Name)" -PercentComplete $X
+                        Start-Sleep -Milliseconds 50
                     }
                 }
             }
 
             Else
             {
-                Write-Host ( "{0} is already installed" -f $This.Packages[$I].DisplayName )
+                Write-Host ( "{0} is installed, and meets version requirements." -f $Package.DisplayName )
             }
         }
     }

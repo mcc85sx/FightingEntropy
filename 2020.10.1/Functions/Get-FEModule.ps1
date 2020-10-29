9acb4f5bea548ac7a9560bc30e269f04c4b8e4ac
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-29 15:53:28 -0700

    Update Get-FEModule.ps1

--- 2020.10.1/Functions/Get-FEModule.ps1
+++ 2020.10.1/Functions/Get-FEModule.ps1
@@ -1,29 +1,32 @@
 Function Get-FEModule
 {
     [CmdLetBinding( DefaultParameterSetName = "Default",
-                    HelpUri                 = 'http://www.github.com/secure-digits-plus-llc')]Param(                
-        [Parameter( ParameterSetName        = "Default"    )][Switch]         $All ,
-        [Parameter( ParameterSetName        = "Registry"   )][Switch]    $Registry ,
-        [Parameter( ParameterSetName        = "Properties" )][Switch]  $Properties ,
-        [Parameter( ParameterSetName        = "Base"       )][Switch]        $Base , 
-        [Parameter( ParameterSetName        = "Classes"    )][Switch]     $Classes , 
-        [Parameter( ParameterSetName        = "Control"    )][Switch]     $Control , 
-        [Parameter( ParameterSetName        = "Functions"  )][Switch]   $Functions , 
-        [Parameter( ParameterSetName        = "Graphics"   )][Switch]    $Graphics ,
-        [Parameter( ParameterSetName        = "Tools"      )][Switch]       $Tools ,
-        [Parameter( ParameterSetName        = "Shares"     )][Switch]      $Shares )
+                    HelpUri                 = "http://www.github.com/mcc85sx/FightingEntropy" )]
+    Param(                
+        [Parameter( ParameterSetName        = "Default"     )][Switch]          $All ,
+        [Parameter( ParameterSetName        = "Registry"    )][Switch]     $Registry ,
+        [Parameter( ParameterSetName        = "Base"        )][Switch]         $Base , 
+        [Parameter( ParameterSetName        = "Classes"     )][Switch]      $Classes , 
+        [Parameter( ParameterSetName        = "Control"     )][Switch]      $Control , 
+        [Parameter( ParameterSetName        = "Functions"   )][Switch]    $Functions , 
+        [Parameter( ParameterSetName        = "Graphics"    )][Switch]     $Graphics , 
+        [Parameter( ParameterSetName        = "Tools"       )][Switch]        $Tools , 
+        [Parameter( ParameterSetName        = "Shares"      )][Switch]       $Shares ,
+        [Parameter( ParameterSetName        = "Drives"      )][Switch]       $Drives ,
+        [Parameter( ParameterSetName        = "Certificates")][Switch] $Certificates )
 
     Switch ($PSCmdlet.ParameterSetName) 
-    { 
-        Default    { [_Module]::New()            }
-        Registry   { [_Module]::New().Registry   }
-        Properties { [_Module]::New().Properties }
-        Base       { [_Module]::New().Base       } 
-        Classes    { [_Module]::New().Classes    }
-        Control    { [_Module]::New().Control    }
-        Functions  { [_Module]::New().Functions  }
-        Graphics   { [_Module]::New().Graphics   }
-        Tools      { [_Module]::New().Tools      }
-        Shares     { [_Module]::New().Shares     }
+    {  
+        Default      { [_Module]::New()              }
+        Registry     { [_Module]::New().Registry     }
+        Base         { [_Module]::New().Base         } 
+        Classes      { [_Module]::New().Classes      }
+        Control      { [_Module]::New().Control      }
+        Functions    { [_Module]::New().Functions    }
+        Graphics     { [_Module]::New().Graphics     }
+        Tools        { [_Module]::New().Tools        }
+        Shares       { [_Module]::New().Shares       }
+        Drives       { [_Module]::New().Drives       }
+        Certificates { [_Module]::New().Certificates }
     }
 }

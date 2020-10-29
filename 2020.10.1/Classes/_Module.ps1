bc4f07b7bbc4a17082eff6e22d68984e6b273a8b
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-29 02:23:07 -0700

    Update _Module.ps1

--- 2020.10.1/Classes/_Module.ps1
+++ 2020.10.1/Classes/_Module.ps1
@@ -3,89 +3,76 @@ Class _Module
     [String]               $Name = "FightingEntropy"
     [String]            $Version = "2020.10.1"
     [String]           $Provider = "Secure Digits Plus LLC"
-    [String]               $Path = $Env:ProgramData
-    [String]                $URL = "https://github.com/mcc85sx/FightingEntropy/blob/master/2020.10.1"
-    [Object]               $File
-    [Object]           $Manifest
-    [String]           $Registry = "HKLM:\SOFTWARE\Policies"
-    [Object]         $Properties
-    [Object]               $Base
-    [Object]            $Classes
-    [Object]            $Control
-    [Object]          $Functions
-    [Object]           $Graphics
+    [String]               $Date
     [String]             $Status
-    [Object]              $Tools
-    [Object]             $Shares
-    
-    Hidden [Hashtable]     $Hash = @{ 
-    
-        Folders   = ("/Classes/Control/Functions/Graphics" -Split "/")
+    [String]           $Resource = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.10.1"
 
-        # _Root _Module
-        Classes   = ("_QMark _File _FirewallRule _Drive _Cache _Icons _Shortcut _Drives _Host _Block _Faces " +
-                     "_Track _Theme _Object _Flag _Banner _UISwitch _Toast _XamlWindow _XamlObject _Root _Mo" + 
-                     "dule _VendorList _V4Network _V6Network _NetInterface _Network _Info _Service _Services" + 
-                     " _ViperBomb _Brand _Branding _Certificate _Company _Key _RootVar _Share _Master _Sourc" + 
-                     "e _Target _ServerDependency _ServerFeature _ServerFeatures _IISFeatures _IIS _DCPromo " + 
-                     "_Xaml _XamlGlossaryItem" ) -Split " "
-                     
-        Control   = ("Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f 
-                     "ClientMod.xml","ServerMod.xml") -Split " "
-                     
-        Functions = ("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write" + 
-                     "-Banner Install-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotifica" + 
-                     "tion New-FECompany") -Split " "
-                     
-        Graphics  = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp") -Split " "
+    [String]           $Registry
+    [String]               $Path
+    [Object]               $File 
+    [Object]           $Manifest 
+    [Object]               $Base 
+
+    [Object[]]          $Classes
+    [Object[]]          $Control
+    [Object[]]        $Functions
+    [Object[]]         $Graphics
+
+    [Object[]]            $Tools
+    [Object[]]           $Shares
+
+    [Hashtable]          $Module = @{
+
+        Path                     = "{0}"
+        Names                    = ("Name Version Provider Date Path Status" -Split " ")
+        File                     = "{0}\FightingEntropy.psm1"
+        Manifest                 = "{0}\FightingEntropy.psd1"
+        Folders                  = "/Classes/Control/Functions/Graphics" -Split "/"
+        Classes                  = ("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
+                                    "Object Flag Banner UISwitch Toast XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
+                                    "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
+                                    "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml " + 
+                                    "XamlGlossaryItem" ).Split(" ") | % { "_$_" }
+        Control                  = ("Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
+                                    "ServerMod.xml") -Split " "
+        Functions                = ("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write" + 
+                                    "-Banner Install-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotifica" + 
+                                    "tion New-FECompany Get-ServerDependency").Split(" ") | % { "$_" }
+        Graphics                 = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp") -Split " "
     }
 
-    Hidden [String[]]   $Folders = "Classes Control Functions Graphics" -Split " "
-    Hidden [String[]]     $Files = ( "{0}.ps1 {0}.psm1" -f "FightingEntropy" ) -Split " "
+    Hidden [Object]        $Load
+    Hidden [Object]      $Output
 
-    [String] GetRoot([String]$Root)
+    [String] Root([String]$Root)
     {
         Return ( $Root, $This.Provider, $This.Name, $This.Version -join '\' )
     }
 
     _Module()
     {
-        $This.Registry           = $This.GetRoot($This.Registry)
-        $This.Path               = $This.GetRoot($This.Path)
-        
-        If ( ! ( Test-Path $This.Path ) )
-        {
-            New-Item -Path $This.Path -ItemType Directory -Verbose
+        $This.Registry           = $This.Root("HKLM:\SOFTWARE\Policies")
+
+        Get-ItemProperty $This.Registry | % { 
+
+            $This.Name           = $_.Name
+            $This.Version        = $_.Version
+            $This.Provider       = $_.Provider
+            $This.Date           = $_.Date
+            $This.Status         = $_.Status
+            $This.Path           = $_.Path
         }
 
-        If ( ! ( Test-Path $This.Registry ) )
-        {
-            ForEach ( $I in 3..5 )
-            { 
-                $This.Registry.Split('\')[0..$I] -join '\' | ? { ! ( Test-Path $_ ) } | % { New-Item $_ -Verbose }
-            }
+        $This.Path               = $This.Root($Env:ProgramData)
+        $This.File               = $This.Module.File -f $This.Path
+        $This.Manifest           = $This.Module.Manifest -f $This.Path
 
-            [_Root]::New( $This.Registry, $This.Name, $This.Version, $This.Provider, $This.Path )
-        }
+        Import-Module $This.Manifest -Verbose -Force
 
-        $This.Properties         = Get-ItemProperty -Path $This.Registry | Select-Object Name, Version, Provider, Date, Path, Status
-        $This.Folders            | % { "{0}\$_" -f $This.Path } | ? { ! ( Test-Path $_ ) } | % { New-Item -Path $_ -ItemType Directory -Verbose }
         $This.Base               = Get-ChildItem -Path $This.Path
-        $This.File               = Get-Item -Path ( "{0}\FightingEntropy.psm1" -f $This.Path )
-        $This.Manifest           = Get-Item -Path ( "{0}\FightingEntropy.psd1" -f $This.Path )
         $This.Classes            = $This.Base | ? Name -eq Classes   | Get-ChildItem
         $This.Control            = $This.Base | ? Name -eq Control   | Get-ChildItem
         $This.Functions          = $This.Base | ? Name -eq Functions | Get-ChildItem 
         $This.Graphics           = $This.Base | ? Name -eq Graphics  | Get-ChildItem
-    }
-    
-    LoadClassLib()
-    {
-        $This.Classes | Get-ChildItem | ? Name -match __index.txt | Get-Content | % { Import-Module -Path ( "{0}\Classes\{1}.ps1" -f $This.Path, $_ ) }
-    }
-    
-    LoadFunctionLib()
-    {
-        $This.Functions | Get-ChildItem | ? Name -match __index.txt | Get-Content | % { Import-Module -Path ( "{0}\Functions\{1}.ps1" -f $This.Path, $_ ) }
     }
 }

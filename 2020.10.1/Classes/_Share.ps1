23a5f127d6c05f8bebc9c233fe6c880a170c22fc
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-10-29 12:13:02 -0700

    Update _Share.ps1

--- 2020.10.1/Classes/_Share.ps1
+++ 2020.10.1/Classes/_Share.ps1
@@ -1,89 +1,103 @@
 Class _Share
 {
-    [String]           $Name = "FE001"
-    [String]     $PSProvider
+    [Object]         $Module = (Get-FEModule)
+    [Object]         $Shares = (Get-SMBShare)
+    [String]      $ShareName
+    [String]      $SharePath
+    [String] $FileSystemPath
+    [String]   $ProviderPath
+    [String]           $Name
+    [String]     $PSProvider = "MDTProvider"
     [String]           $Root
     [String]    $Description = $Null
     [String]    $NetworkPath
     [Object]        $Company
 
-    Hidden [String[]] $Names
-    Hidden [String[]] $Roots
-
-    Hidden [Hashtable] $Hash = @{
+    [Object]            $MDT = @{
 
         Name        = "Microsoft Deployment Toolkit" 
         Path        = ( Get-ChildItem ( Get-ItemProperty "HKLM:\Software\Microsoft\Deployment 4" ).Install_Dir -Filter *Toolkit.psd1 -Recurse ).FullName
         Description = ("The [Microsoft Deployment Toolkit]\\_It is *the* toolkit...\_...that Microsoft themselves uses...\" +
-                        "_...to deploy additional toolkits.\\_It's not that weird.\_You're just thinking way too far into it" +
-                        ", honestly.\\_Regardless... it is quite a fearsome toolkit to have in one's pocket.\_Or, wherever " +
-                        "really...\_...at any given time.\\_When you need to be taken seriously...?\_Well, it *should* be t" +
-                        "he first choice on one's agenda.\_But that's up to you.\\_The [Microsoft Deployment Toolkit].\_Eve" +
-                        "n Mr. Gates thinks it's awesome." ).Replace("_","    ").Split('\')
+                       "_...to deploy additional toolkits.\\_It's not that weird.\_You're just thinking way too far into it" +
+                       ", honestly.\\_Regardless... it is quite a fearsome toolkit to have in one's pocket.\_Or, wherever " +
+                       "really...\_...at any given time.\\_When you need to be taken seriously...?\_Well, it *should* be t" +
+                       "he first choice on one's agenda.\_But that's up to you.\\_The [Microsoft Deployment Toolkit].\_Eve" +
+                       "n Mr. Gates thinks it's awesome." ).Replace("_","    ").Split('\')
     }
 
-    _Share([String]$ShareName,[String]$Root,[String]$Description,[String]$NetworkPath)
+    FEShare([String]$ShareName,[String]$SharePath,[String]$FilesystemPath,[String]$Description)
     {
-        $ShareName            = "{0}$" -f $ShareName.TrimEnd("$")
+        $This.ShareName            = "{0}$" -f $ShareName.TrimEnd("$")
+        $This.SharePath            = $SharePath
+        $This.FileSystemPath       = $FileSystemPath
+        $This.Description          = $Description
+    }
 
-        If ( Test-Path $Root )
+    Get()
+    {
+        Import-Module $This.MDT.Path -Verbose
+
+        $This.Shares       = Get-MDTPersistentDrive
+    }
+
+    New()
+    {
+        If ( Test-Path $This.FileSystemPath )
         {
             Throw "Path exists"
         }
 
-        If ( ! ( Test-Path $This.Hash.Path ) )
+        If ( ! ( Test-Path $This.MDT.Path ) )
         {
             Throw "Microsoft Deployment Toolkit not installed"
         }
 
-        Else
-        {
-            $This.Root = $Root
-        }
-
-        $Shares = Get-SMBShare 
-        
-        If ( $ShareName -in $Shares.Name -or $Root -in $Shares.Path )
+        If ( $This.ShareName -in $This.Shares.Name -or $This.FileSystemPath -in $This.Shares.Path )
         {
             Throw "Share exists"
         }
 
-        If ( !$This.Hash.Path )
-        {
-            Throw ( "{0} is not installed" -f $This.Hash.Name )
-        }
+        $This.Get()
 
-        Import-Module $This.Hash.Path -Verbose
-
-        $Shares = Get-MDTPersistentDrive
-
-        If ( $ShareName -in $Shares.Name -or $Root -in $Shares.Root )
+        If ( $This.ShareName -in $This.Shares.Name -or $This.Root -in $This.Shares.Root )
         {
             Throw "MDT/FE Share exists"
         }
 
         If ( !! $This.Names )
         {
-            $This.Name     = $This.Names        | % { @($_,$_[-1])[[Int32]($_.Count -gt 1)].Replace("DS","") } | % { "FE{0:d3}" -f ( [Int32]$_ + 1 ) }
+            $This.Name     = $This.Shares.Name | % { @($_,$_[-1])[[Int32]($_.Count -gt 1)].Replace("DS","") } | % { "FE{0:d3}" -f ( [Int32]$_ + 1 ) }
         }
 
-        $This.PSProvider   = "MDTProvider" 
-        $This.Root         = $Root
-        $This.Description  = $Description       | % { If ( !$_ ) { "-" } Else { $_ } }
-        $This.NetworkPath  = $NetworkPath
+        @{  Path           = $This.FileSystemPath
+            ItemType       = "Directory"       } | % { New-Item @_ -Verbose }
 
-        @{  Path           = $Root
-            ItemType       = "Directory"      } | % { New-Item @_ -Verbose }
+        @{  Name           = $This.ShareName
+            Path           = $This.FileSystemPath
+            FullAccess     = "Administrators"  } | % { New-SMBShare @_ -Verbose }
 
-        @{  Name           = $ShareName
-            Path           = $Root 
-            FullAccess     = "Administrators" } | % { New-SMBShare @_ -Verbose }
-
-        @{  Name           = $This.Name 
+        @{  Name           = $This.Name
             PSProvider     = $This.PSProvider
-            Root           = $This.Root
+            Root           = $This.FileSystemPath
             Description    = $This.Description
-            NetworkPath    = $This.NetworkPath
+            NetworkPath    = $This.SharePath
             Verbose        = $True            } | % { New-PSDrive @_ | Add-MDTPersistentDrive -Verbose }
+
+        If ( $? -eq $True )
+        {
+            $This.Module   | ? Status -eq Initialized | % { 
+                
+                Set-ItemProperty -Path $_.Registry -Name Status -Value Installed -Verbose
+                New-Item         -Path $_.Registry -Name Shares -Verbose
+            }
+        }
+
+        Get-MDTPersistentDrive | % { 
+
+            New-Item         -Path $Path\Shares            -Name $_.Name                           -Verbose
+            New-ItemProperty -Path $Path\Shares\$($_.Name) -Name Name        -Value $_.Name        -Verbose
+            New-ItemProperty -Path $Path\Shares\$($_.Name) -Name Path        -Value $_.Path        -Verbose
+            New-ItemProperty -Path $Path\Shares\$($_.Name) -Name Description -Value $_.Description -Verbose
+        }
     }
 }

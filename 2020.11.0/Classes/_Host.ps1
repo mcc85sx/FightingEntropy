45a7b565754585bbfd78aaa49ac9477b70ba707b
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-29 17:39:00 -0500

    Update _Host.ps1

--- 2020.11.0/Classes/_Host.ps1
+++ 2020.11.0/Classes/_Host.ps1
@@ -1,20 +1,18 @@
 Class _Host
 {
-    Hidden [String]         $Name = $Env:ComputerName.ToLower()
-    Hidden [String]          $DNS = $env:USERDNSDOMAIN.ToLower()
-    Hidden [String]      $NetBIOS = $Env:UserDomain.ToLower()
+    [String]                $Name = [Environment]::MachineName.ToLower()
+    [String]                 $DNS
+    [String]             $NetBIOS = [Environment]::UserDomainName.ToLower()
 
     [String]            $Hostname
     [String]            $Username = [Environment]::UserName
     [Object]           $Principal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
     [Bool]               $IsAdmin
-
-    [_Drives]             $Drives = [_Drives]::New()
-    [Object]             $Profile = (Get-ChildItem $Env:UserProfile)
-    [Object]             $Content
+    [Object]             $Network
 
     _Host()
     {
+        $This.DNS                 = $env:USERDNSDOMAIN.ToLower()
         $This.Hostname            = @($This.Name;"{0}.{1}" -f $This.Name, $This.DNS)[(Get-CimInstance Win32_ComputerSystem).PartOfDomain].ToLower()
         $This.IsAdmin             = $This.Principal.IsInRole("Administrator") -or $This.Principal.IsInRole("Administrators")
         
@@ -22,7 +20,10 @@ Class _Host
         {
             Throw "Must run as administrator"
         }
+    }
 
-        $This.Content             = Get-ChildItem $This.Profile -Recurse | ? PsIsContainer -eq $False | % { [_File]::New($This.Provider,$_.FullName) }
+    _Network()
+    {
+        $This.Network             = [_Network]::New()
     }
 }

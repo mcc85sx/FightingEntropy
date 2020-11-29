66c47e16f50ad9dc51c3af3b22138f95601a9002
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-29 17:44:14 -0500

    Create _NBTStat.ps1

---
+++ 2020.11.0/Classes/_NBTStat.ps1
@@ -0,0 +1,15 @@
+Class _NBTStat
+{
+    Hidden [Object]   $Object
+    [String]            $Name
+    [String]       $IPAddress
+    [Object[]]         $Hosts
+        
+    _NBTStat([Object[]]$Object)
+    {
+        $This.Object     = $Object
+        $This.Name       = $Object[0].Split(":")[0]
+        $This.IPAddress  = $Object[1].Split("[")[1].Split("]")[0]
+        $This.Hosts      = $Object | ? { $_ -match "Registered" } | % { [_NBTHost]::New($_) }
+    }
+}

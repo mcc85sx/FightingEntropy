931cf2da789c5b5c2826b5e9dca2a79e78efcf74
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-30 08:43:12 -0500

    Update _ArpStat.ps1

--- 2020.11.0/Classes/_ArpStat.ps1
+++ 2020.11.0/Classes/_ArpStat.ps1
@@ -1,15 +1,17 @@
-Class _ArpStat
-{
-    Hidden [String] $Object
-    [String] $IPAddress
-    [String] $IFIndex
-    [Object[]] $Hosts
-
-    _ArpStat([Object]$Object)
+    Class _ArpStat
     {
-        $This.Object    = $Object
-        $This.IPAddress = $Object[0].Replace("Interface: ","").Split(" ")[0]
-        $This.IFIndex   = $Object[0].Split(" ")[-1]
-        $This.Hosts     = $Object | ? { $_ -notmatch "(Interface|static)" } | % { [_ArpHost]::New($_) }
+        Hidden [String] $Object
+        [String] $Name
+        [String] $IPAddress
+        [String] $IFIndex
+        [Object[]] $Hosts
+
+        _ArpStat([Object]$Object)
+        {
+            $This.Object    = $Object
+            $This.IPAddress = $Object[0].Replace("Interface: ","").Split(" ")[0]
+            $This.IFIndex   = Invoke-Expression $Object[0].Split(" ")[-1]
+            $This.Name      = (Get-NetIPInterface | ? IFIndex -eq $This.IFIndex | % IFAlias)[0]
+            $This.Hosts     = $Object | ? { $_ -notmatch "(Interface|static)" } | % { [_ArpHost]::New($_) }
+        }
     }
-}

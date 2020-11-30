200379153f4420e3465ec5bd91f00c3bdf3b6ad5
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-30 08:27:58 -0500

    Update _Network.ps1

--- 2020.11.0/Classes/_Network.ps1
+++ 2020.11.0/Classes/_Network.ps1
@@ -1,33 +1,45 @@
 Class _Network
-{
-    [Object]           $Adapter
-    [Object]            $Vendor
-    [Object]           $NBTScan
-    [Object[]]       $Interface
-
-    _Network()
     {
-        $This.Vendor         = [_VendorList]::New("https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/scratch/VendorList.txt")
-        $This.NBTScan        = [_NBTScan]::New().Output
-        $This.Interface      = @( )
+        Hidden [Object[]]  $Adapter
+        Hidden [Object]     $Vendor
+        Hidden [Object]    $NBTScan
+        Hidden [Object]    $ARPScan
+        [Object[]]       $Interface
 
-        ForEach ( $Interface in Get-NetIPConfiguration -Detailed )
+        _Network()
         {
-            $Item            = [_NetInterface]::New($Interface)
-            $Item.Vendor     = $This.GetVendor($Item.MacAddress)
-            $This.Interface += $Item
-        }
+            Write-Host "Collecting Network Adapters"
+            $This.Adapter        = (Get-NetAdapter)
 
-        $This.Interface      = $This.Interface | Sort-Object Index
-    }
-
-    [String] GetVendor([String]$MacAddress)
-    {
-        If ( $MacAddress -notmatch "([A-Fa-f0-9]{2}(-|:)*){5}[A-Fa-f0-9]{2}" )
-        {
-            Throw "Invalid MacAddress"
-        }
+            Write-Host "Collecting Vendor List"
+            $This.Vendor         = [_VendorList]::New("https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/scratch/VendorList.txt")
             
-        Return $This.Vendor.VenID[( $MacAddress -Replace "(:|-)" , "" ).SubString(0,6)]
+            Write-Host "Scanning NBT Table"
+            $This.NBTScan        = [_NBTScan]::New().Output
+            
+            Write-Host "Scanning ARP Table"
+            $This.ARPScan        = [_ARPScan]::New().Output
+            $This.Interface      = @( )
+
+            ForEach ( $Interface in Get-NetIPConfiguration -Detailed )
+            {
+                $Item            = [_NetInterface]::New($Interface)
+                $Item.Vendor     = $This.GetVendor($Item.MacAddress)
+                $Item.Arp        = $This.ARPScan | ? IFIndex -eq $Item.Index
+                $Item.NBT        = $This.NBTScan | ? Name -eq $Item.Alias
+                $This.Interface += $Item
+            }
+
+            $This.Interface      = $This.Interface | Sort-Object Alias
+        }
+
+        [String] GetVendor([String]$MacAddress)
+        {
+            If ( $MacAddress -notmatch "([A-Fa-f0-9]{2}(-|:)*){5}[A-Fa-f0-9]{2}" )
+            {
+                Throw "Invalid MacAddress"
+            }
+            
+            Return $This.Vendor.VenID[( $MacAddress -Replace "(:|-)" , "" ).SubString(0,6)]
+        }
     }
-}

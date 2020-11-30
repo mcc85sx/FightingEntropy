1de04ff5fa7cf112f9041afb824153294fc4b331
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-29 22:31:02 -0500

    Update _ArpHost.ps1

--- 2020.11.0/Classes/_ArpHost.ps1
+++ 2020.11.0/Classes/_ArpHost.ps1
@@ -11,11 +11,18 @@ Class _ArpHost
         Return @( $This.Line.Substring($Start,$End).Trim(" ") )
     }
 
+    NameHost ([String]$IPAddress)
+    {
+        Write-Host "[~] $IPAddress"
+        $This.Host       = Try { Resolve-DnsName $IPAddress -EA 0 | % NameHost } Catch { "-" }
+    }
+    
     _ArpHost([String]$Line)
     {
         $This.Line       = $Line
         $This.IPAddress  = $This.X(0,24)
         $This.MacAddress = $This.X(24,17)
         $This.Type       = $This.Line.Substring(41).Trim(" ")
+        $This.Namehost($This.IPAddress)
     }
 }

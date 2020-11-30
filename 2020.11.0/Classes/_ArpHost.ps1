2db98d49e7d2e79ab4a8ce01b7224976d73b9b04
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-30 08:41:56 -0500

    Update _ArpHost.ps1

--- 2020.11.0/Classes/_ArpHost.ps1
+++ 2020.11.0/Classes/_ArpHost.ps1
@@ -1,28 +1,22 @@
-Class _ArpHost
-{
-    Hidden [String] $Line
-    [String] $Host
-    [String] $IPAddress
-    [String] $MacAddress
-    [String] $Type
+    Class _ArpHost
+    {
+        Hidden [String] $Line
+        [String] $Name
+        [String] $IPAddress
+        [String] $MacAddress
+        [String] $Type
 
-    [String] X ([Int32]$Start,[Int32]$End)
-    {
-        Return @( $This.Line.Substring($Start,$End).Trim(" ") )
-    }
+        [String] X ([Int32]$Start,[Int32]$End)
+        {
+            Return @( $This.Line.Substring($Start,$End).Trim(" ") )
+        }
 
-    NameHost ([String]$IPAddress)
-    {
-        Write-Host "[~] $IPAddress"
-        $This.Host       = Try { Resolve-DnsName $IPAddress -EA 0 | % NameHost } Catch { "-" }
+        _ArpHost([String]$Line)
+        {
+            $This.Line       = $Line
+            $This.IPAddress  = $This.X(0,24)
+            $This.MacAddress = $This.X(24,17)
+            $This.Type       = $This.Line.Substring(41).Trim(" ")
+            $This.Name       = Try { Resolve-DnsName $This.IPAddress -QuickTimeout -EA 0 | % NameHost } Catch { "-" }
+        }
     }
-    
-    _ArpHost([String]$Line)
-    {
-        $This.Line       = $Line
-        $This.IPAddress  = $This.X(0,24)
-        $This.MacAddress = $This.X(24,17)
-        $This.Type       = $This.Line.Substring(41).Trim(" ")
-        $This.Namehost($This.IPAddress)
-    }
-}

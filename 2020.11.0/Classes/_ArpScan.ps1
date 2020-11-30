65c0b8b1935439b9d5e3ab82df20c02b30a6d634
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-29 20:27:48 -0500

    Create _ArpScan.ps1

---
+++ 2020.11.0/Classes/_ArpScan.ps1
@@ -0,0 +1,49 @@
+Class _ArpScan
+{
+    [Object]               $ARP = (arp -a)
+    Hidden [Object[]]  $Adapter = (Get-NetAdapter)
+    Hidden [Hashtable] $Process
+    [Object[]]          $Output
+
+    _ArpScan()
+    {
+        $This.Process = @{ }
+        $This.Output  = @( )
+        $X            = -1
+            
+        ForEach ( $I in 0..( $This.Arp.Count - 1 ) )
+        {
+            If ( $This.Arp[$I].Length -gt 0 )
+            {   
+                If ( $This.Arp[$I] -match "Interface" )
+                {
+                    $X ++
+                    $This.Process.Add($X,@( ))
+                }
+                    
+                If ( $This.Arp[$I] -notmatch "Internet Address" )
+                {
+                    $This.Process[$X] += $This.Arp[$I]
+                }
+            }
+        }
+            
+        Switch ($This.Process.Count)
+        {
+            1 
+            {
+                $This.Output = [_ArpStat]::New($This.Process[0])
+            }
+
+            Default 
+            { 
+                ForEach ( $X in 0..( $This.Process.Count - 1 ) )
+                {
+                    $This.Output += [_ArpStat]::New($This.Process[$X])
+                }
+            }
+        }
+
+        $This.Output = $This.Output | Sort-Object Name
+    }
+}

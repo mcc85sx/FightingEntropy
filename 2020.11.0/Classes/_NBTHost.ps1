669451ab39c8679d1ce6806b927fab4877882a4d
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-30 08:17:52 -0500

    Update _NBTHost.ps1

--- 2020.11.0/Classes/_NBTHost.ps1
+++ 2020.11.0/Classes/_NBTHost.ps1
@@ -1,21 +1,21 @@
-    Class _NBTHost
+Class _NBTHost
+{
+    Hidden [String]    $Line
+    [String]           $Name
+    [String]             $ID
+    [String]           $Type
+    [String]        $Service
+
+    [String] X ([Int32]$Start,[Int32]$End)
     {
-        Hidden [String]    $Line
-        [String]           $Name
-        [String]             $ID
-        [String]           $Type
-        [String]        $Service
-
-        [String] X ([Int32]$Start,[Int32]$End)
-        {
-            Return @( $This.Line.Substring($Start,$End).TrimEnd(" ") )
-        }
-
-        _NBTHost([String]$Line)
-        {
-            $This.Line    = $Line
-            $This.Name    = $This.X(0,19).TrimStart(" ")
-            $This.ID      = $This.X(20,2)
-            $This.Type    = $This.X(25,12)
-        }
+        Return @( $This.Line.Substring($Start,$End).TrimEnd(" ") )
     }
+
+    _NBTHost([String]$Line)
+    {
+        $This.Line    = $Line
+        $This.Name    = $This.X(0,19).TrimStart(" ")
+        $This.ID      = $This.X(20,2)
+        $This.Type    = $This.X(25,12)
+    }
+}

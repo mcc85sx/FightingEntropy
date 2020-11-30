c82b2e9a080536ad42ed2966fbc8e945e65b0760
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-11-30 08:42:40 -0500

    Update _ArpScan.ps1

--- 2020.11.0/Classes/_ArpScan.ps1
+++ 2020.11.0/Classes/_ArpScan.ps1
@@ -1,68 +1,49 @@
 Class _ArpScan
-{
-    [Object]               $ARP = (arp -a)
-    Hidden [Object[]]  $Adapter = (Get-NetAdapter)
-    Hidden [Hashtable] $Process
-    [Object[]]          $Output
-
-    [String] NameHost ([String]$IPAddress)
     {
-        Return @( Try { Resolve-DnsName $IPAddress | % NameHost } Catch { "-" } )
-    }
+        [Object]               $ARP = (arp -a)
+        Hidden [Object[]]  $Adapter = (Get-NetAdapter)
+        Hidden [Hashtable] $Process
+        [Object[]]          $Output
 
-    _ArpScan()
-    {
-        $This.Process = @{ }
-        $This.Output  = @( )
-        $X            = -1
-            
-        ForEach ( $I in 0..( $This.Arp.Count - 1 ) )
+        _ArpScan()
         {
-            If ( $This.Arp[$I].Length -gt 0 )
-            {   
-                If ( $This.Arp[$I] -match "Interface" )
-                {
-                    $X ++
-                    $This.Process.Add($X,@( ))
-                }
-                    
-                If ( $This.Arp[$I] -notmatch "Internet Address" )
-                {
-                    $This.Process[$X] += $This.Arp[$I]
-                }
-            }
-        }
+            $This.Process = @{ }
+            $This.Output  = @( )
+            $X            = -1
             
-        Switch ($This.Process.Count)
-        {
-            1 
+            ForEach ( $I in 0..( $This.Arp.Count - 1 ) )
             {
-                $Item = [_ArpStat]::New($This.Process[0])
-
-                ForEach ( $X in 0..( $Item.Hosts.Count - 1 ) )
-                {
-                    $Item.Hosts[$X].Host = $This.NameHost($Item.Hosts[$X].IPAddress)
-                }
-
-                $This.Output += $Item
-            }
-
-            Default 
-            {
-                ForEach ( $I in 0..( $This.Process.Count - 1 ) )
-                {
-                    $Item = [_ArpStat]::New($This.Process[$I])
-
-                    ForEach ( $X in 0..($Item.Hosts.Count - 1 ) )
+                If ( $This.Arp[$I].Length -gt 0 )
+                {   
+                    If ( $This.Arp[$I] -match "Interface" )
                     {
-                        $Item.Hosts[$X].Host = $This.NameHost($Item.Hosts[$X].IPAddress)
+                        $X ++
+                        $This.Process.Add($X,@( ))
                     }
-
-                    $This.Output += $Item
+                    
+                    If ( $This.Arp[$I] -notmatch "Internet Address" )
+                    {
+                        $This.Process[$X] += $This.Arp[$I]
+                    }
                 }
             }
-        }
+            
+            Switch ($This.Process.Count)
+            {
+                1 
+                {
+                    $This.Output = [_ArpStat]::New($This.Process[0])
+                }
 
-        $This.Output = $This.Output | Sort-Object IPAddress
+                Default 
+                {
+                    ForEach ( $I in 0..( $This.Process.Count - 1 ) )
+                    {
+                        $This.Output += [_ArpStat]::New($This.Process[$I])
+                    }
+                }
+            }
+
+            $This.Output = $This.Output | Sort-Object IPAddress
+        }
     }
-}

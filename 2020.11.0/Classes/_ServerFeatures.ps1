559ef860a5e5da650be684fca4b7f9242c391ae6
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-01 12:36:34 -0500

    Update _ServerFeatures.ps1

--- 2020.11.0/Classes/_ServerFeatures.ps1
+++ 2020.11.0/Classes/_ServerFeatures.ps1
@@ -1,13 +1,12 @@
 Class _ServerFeatures
 {
-    [String[]]        $Names = ("AD-Domain-Services DHCP DNS GPMC RSAT RSAT-AD-AdminCenter RSAT-AD-PowerShell RSAT-AD-T" +
+    Static [String[]] $Names = ("AD-Domain-Services DHCP DNS GPMC RSAT RSAT-AD-AdminCenter RSAT-AD-PowerShell RSAT-AD-T" +
                                 "ools RSAT-ADDS RSAT-ADDS-Tools RSAT-DHCP RSAT-DNS-Server RSAT-Role-Tools WDS WDS-Admin" + 
                                 "Pack WDS-Deployment WDS-Transport").Split(" ")
     [Object[]]     $Features
 
-    _ServerFeatures(){}
-    Get()
+    _ServerFeatures()
     { 
-        $This.Features       =  Get-WindowsFeature | ? Name -in $This.Names | % { [_ServerFeature]::New($_.Name,$_.DisplayName,$_.Installed) }
+        $This.Features       =  (Get-WindowsFeature | ? Name -in ([_ServerFeature]::Names) | Select Name, DisplayName, Installed)
     }
 }

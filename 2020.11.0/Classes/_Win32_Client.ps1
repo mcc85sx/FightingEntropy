987ec40a25203844d0bcb8ef10a62b4515861c74
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-02 12:18:17 -0500

    Create _Win32_Client.ps1

---
+++ 2020.11.0/Classes/_Win32_Client.ps1
@@ -0,0 +1,20 @@
+Class _Win32_Client
+{
+    [Object] $Host
+    [Object] $Info
+    [Object] $Tools
+    [Object] $Services
+    [Object] $Processes
+    [Object] $Network
+    [Object] $Control
+
+    _Win32_Client()
+    {
+        $This.Host      = [_Host]::New()
+        $This.Info      = [_Info]::New()
+        $This.Tools     = @("ViperBomb","Chocolatey")
+        $This.Services  = [_Services]::new()
+        $This.Processes = (Get-Process)
+        $This.Network   = [_Network]::new().Interface
+    }
+}

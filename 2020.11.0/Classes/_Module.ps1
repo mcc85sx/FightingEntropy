8afc7c4910e0136ca3b3973249882a3ac5fa62a2
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-02 12:04:56 -0500

    Update _Module.ps1

--- 2020.11.0/Classes/_Module.ps1
+++ 2020.11.0/Classes/_Module.ps1
@@ -26,25 +26,21 @@ Class _Module
     [Hashtable]          $Module = @{
 
         Names                    = ("Name Version Provider Date Path Status Type" -Split " ")
+        Role                     = ("{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " ")
         Folders                  = (" Classes Control Functions Graphics Role" -Split " ")
         Classes                  = ("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
                                     "Object Flag Banner UISwitch Toast XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
                                     "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
                                     "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml XamlGlossar" + 
                                     "yItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo FEPromoDomain FEP" + 
-                                    "romoRoles" ).Split(" ") | % { "_$_" }
+                                    "romoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" ).Split(" ") | % { "_$_" }
         Control                  = ("Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
                                     "ServerMod.xml") -Split " "
         Functions                = ("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write" + 
                                     "-Banner Install-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotifica" + 
                                     "tion New-FECompany Get-ServerDependency Get-FEServices Get-FEHost").Split(" ") | % { "$_" }
         Graphics                 = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp" -Split " ")
-
-        Role                     = ("{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " ")
     }
-
-    Hidden [Object]        $Load
-    Hidden [Object]      $Output
 
     [String] Root([String]$Root)
     {
@@ -97,12 +93,10 @@ Class _Module
         $This.Base               = Get-ChildItem -Path $This.Path
         $This.Host               = [_Host]::New()
         $This.Info               = [_Info]::New()
+        $This.Role               = [_Role]::New()
         $This.Classes            = $This.Content("Classes")
         $This.Control            = $This.Content("Control")
         $This.Functions          = $This.Content("Functions")
         $This.Graphics           = $This.Content("Graphics")
-        $This.Role
-        $This.Tools              = Get-ChildItem -Path "$($This.Path)\Tools" -EA 0 
-        $This.Shares             = Get-ChildItem -Path "$($This.Registry)\Shares" -EA 0 | Get-ItemProperty | Select-Object Name, Path, Description
     }
 }

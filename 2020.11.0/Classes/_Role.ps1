4e4aedf5aa381155e260a529a1118905ca773cba
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-02 18:43:13 -0500

    Update _Role.ps1

--- 2020.11.0/Classes/_Role.ps1
+++ 2020.11.0/Classes/_Role.ps1
@@ -2,18 +2,48 @@ Class _Role
 {
     [UInt32] $Type
     [String] $Name
-    [Object] $Root
+    [Object] $Role
 
-    _Role([UInt32]$Slot)
+    [Int32] GetVersion ()
     {
-        If ( $Slot -notin 0..3 )
+        Return @( Get-Item Variable:\PSVersionTable | % Value | % PSVersion | % Major )
+    }
+    
+    [Int32] GetWinType()
+    {
+        Return @( Invoke-Expression "Get-CimInstance -ClassName Win32_OperatingSystem | % Caption" | % { 
+        
+            If ( $_ -match "Windows 10"     ) { 0 } 
+            If ( $_ -match "Windows Server" ) { 1 }
+        })
+    }
+    
+    [Int32] GetOSType()
+    {
+        Return @( If ( $This.GetVersion -gt 5 )
         {
-            Throw "Invalid option"
+            If ( Get-Item Variable:\IsLinux | % Value )
+            {
+                2
+            }
+            
+            Else
+            {
+                $This.GetWinType()
+            }
         }
+        
+        Else
+        {
+            $This.GetWinType()
+        })
+    }   
 
-        $This.Type = ("{0}Client {0}Server UnixBSD RHELCentOS" -f "Win32_" -Split " ")[$Slot]
-       
-        $This.Role = Switch($Slot)
+    _Role()
+    {
+        $This.Type = $This.GetOSType()
+        $This.Name = ("{0}Client {0}Server UnixBSD RHELCentOS" -f "Win32_" -Split " ")[$This.Type]
+        $This.Role = Switch($This.Name)
         {
             Win32_Client { [_Win32_Client]::New() } Win32_Server { [_Win32_Server]::New() } 
             UnixBSD      { [_UnixBSD]::New()      } RHELCentOS   { [_RHELCentOS]::New()   }

c14306c29265540c9edb332fa6b6563c34ee7505
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-06 11:31:12 -0500

    Update _Module.ps1

--- 2020.12.0/Classes/_Module.ps1
+++ 2020.12.0/Classes/_Module.ps1
@@ -1,5 +1,6 @@
 Class _Module
 {
+    [Object]             $Module
     Hidden [Object]         $Env
     Hidden [Object]         $Var
     [String]               $Name = "FightingEntropy"
@@ -24,25 +25,6 @@ Class _Module
     [Object[]]          $Control
     [Object[]]        $Functions
     [Object[]]         $Graphics
-
-    [Hashtable]          $Module = @{
-
-        Names                    = ("Name Version Provider Date Path Status Type" -Split " ")
-        Role                     = ("{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " ")
-        Folders                  = (" Classes Control Functions Graphics Role" -Split " ")
-        Classes                  = ("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
-                                    "Object Flag Banner UISwitch XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
-                                    "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
-                                    "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml XamlGlossar" + 
-                                    "yItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo FEPromoDomain FEP" + 
-                                    "romoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" ).Split(" ") | % { "_$_" }
-        Control                  = ("Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
-                                    "ServerMod.xml") -Split " "
-        Functions                = ("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write" + 
-                                    "-Banner Install-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotifica" + 
-                                    "tion New-FECompany Get-ServerDependency Get-FEServices Get-FEHost").Split(" ") | % { "$_" }
-        Graphics                 = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp" -Split " ")
-    }
 
     [String] Root([String]$Root)
     {
@@ -88,6 +70,7 @@ Class _Module
     
     _Module()
     {
+        $This.Module             = [_Manifest]::New()
         $This.Env                = $This.GetItem("Env:\")
         $This.Var                = $This.GetItem("Variable:\")
         $This.Registry           = $This.Root("HKLM:\SOFTWARE\Policies")

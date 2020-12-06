7a464154639fb008331f4f66226ebea71973b1ec
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-06 12:55:31 -0500

    Update WeatherForecast.cs

--- ASP.Net(Core)/Shared/WeatherForecast.cs
+++ ASP.Net(Core)/Shared/WeatherForecast.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +6,9 @@ namespace SecureWebApp.Shared
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public string Summary { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public DateTime           Date { get; set; }
+        public int        TemperatureC { get; set; }
+        public string          Summary { get; set; }
+        public int        TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }

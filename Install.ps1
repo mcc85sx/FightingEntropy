a1ed76da8d9fb325bfdc2f12454bae0d513d9457
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-06 17:24:53 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,99 +1,98 @@
+Class OS
+{
+    [Object[]] $Environment
+    [Object[]] $Variable
+    [Object]   $PSVersionTable
+    [Object]   $PSVersion
+    [Object]   $Major
+    [Object]   $Type
 
-    Class OS
+    [Object] GetItem([String]$Item)
     {
-        [Object[]] $Environment
-        [Object[]] $Variable
-        [Object]   $PSVersionTable
-        [Object]   $PSVersion
-        [Object]   $Major
-        [Object]   $Type
+        $Return = @{ }
 
-        [Object] GetItem([String]$Item)
-        {
-            $Return = @{ }
-
-            ForEach ( $X in ( Get-Item -Path $Item | % GetEnumerator ) )
-            { 
-                $Return.Add($X.Name,$X.Value)
-            }
-
-            Return $Return
+        ForEach ( $X in ( Get-Item -Path $Item | % GetEnumerator ) )
+        { 
+            $Return.Add($X.Name,$X.Value)
         }
 
-        [String] GetWinType()
-        {
-            Return @( Switch -Regex ( Invoke-Expression "[wmiclass]'\\.\ROOT\CIMV2:Win32_OperatingSystem' | % GetInstances | % Caption" )
-            {
-                "Windows 10" { "Win32_Client" } "Windows Server" { "Win32_Server" }
-            })
-        }
+        Return $Return
+    }
 
-        [String] GetOSType()
+    [String] GetWinType()
+    {
+        Return @( Switch -Regex ( Invoke-Expression "[wmiclass]'\\.\ROOT\CIMV2:Win32_OperatingSystem' | % GetInstances | % Caption" )
         {
-            Return @( If ( $This.Major -gt 5 )
-            {
-                If ( Get-Item Variable:\IsLinux )
-                {
-                    "RHELCentOS"
-                }
+            "Windows 10" { "Win32_Client" } "Windows Server" { "Win32_Server" }
+        })
+    }
 
-                Else
-                {
-                    $This.GetWinType()
-                }
+    [String] GetOSType()
+    {
+        Return @( If ( $This.Major -gt 5 )
+        {
+            If ( Get-Item Variable:\IsLinux | % Value )
+            {
+                "RHELCentOS"
             }
 
             Else
             {
                 $This.GetWinType()
-            })
+            }
         }
 
-        OS()
+        Else
         {
-            $This.Environment    = $This.GetItem("Env:\")
-            $This.Variable       = $This.GetItem("Variable:\")
-            $This.PSVersionTable = $This.Variable.PSVersionTable
-            $This.PSVersion      = $This.PSVersionTable.PSVersion
-            $This.Major          = $This.PSVersion.Major
-            $This.Type           = $This.GetOSType()
-        }
+            $This.GetWinType()
+        })
     }
 
-    Class Manifest
+    OS()
     {
-        [String[]]     $Names = ( "Name Version Provider Date Path Status Type" -Split " " )
-        [String]        $GUID = ( "67b283d9-72c6-413a-aa80-a24af5d4ea8f" )
-        [String[]]      $Role = ( "{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " " )
-        [String[]]   $Folders = ( " Classes Control Functions Graphics Role" -Split " " )
-        [String[]]   $Classes = (("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
-                                  "Object Flag Manifest Banner UISwitch XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
-                                  "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
-                                  "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml XamlGlossar" + 
-                                  "yItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo FEPromoDomain FEP" + 
-                                  "romoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" ).Split(" ") | % { "_$_" })
-        [String[]]   $Control = ( "Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
-                                  "ServerMod.xml" ) -Split " "
-        [String[]] $Functions = (("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write-Banner Instal" +
-                                  "l-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotification New-FECompany Get-Serve" + 
-                                  "rDependency Get-FEServices Get-FEHost").Split(" ") | % { "$_" })
-        [String[]]  $Graphics = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp" -Split " ")
-
-        Manifest(){}
+        $This.Environment    = $This.GetItem("Env:\")
+        $This.Variable       = $This.GetItem("Variable:\")
+        $This.PSVersionTable = $This.Variable.PSVersionTable
+        $This.PSVersion      = $This.PSVersionTable.PSVersion
+        $This.Major          = $This.PSVersion.Major
+        $This.Type           = $This.GetOSType()
     }
+}
 
-    Class Install
-    {
-        [Object]                 $OS
-        [Object]             $Module
+Class Manifest
+{
+    [String[]]     $Names = ( "Name Version Provider Date Path Status Type" -Split " " )
+    [String]        $GUID = ( "67b283d9-72c6-413a-aa80-a24af5d4ea8f" )
+    [String[]]      $Role = ( "{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " " )
+    [String[]]   $Folders = ( " Classes Control Functions Graphics Role" -Split " " )
+    [String[]]   $Classes = (("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
+                              "Object Flag Manifest Banner UISwitch XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
+                              "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
+                              "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml XamlGlossar" + 
+                              "yItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo FEPromoDomain FEP" + 
+                              "romoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" ).Split(" ") | % { "_$_" })
+    [String[]]   $Control = ( "Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
+                              "ServerMod.xml" ) -Split " "
+    [String[]] $Functions = (("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write-Banner Instal" +
+                              "l-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotification New-FECompany Get-Serve" + 
+                              "rDependency Get-FEServices Get-FEHost").Split(" ") | % { "$_" })
+    [String[]]  $Graphics = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp" -Split " ")
 
-        [String]               $Name = "FightingEntropy"
-        [String]            $Version = "2020.12.0"
-        [String]           $Provider = "Secure Digits Plus LLC"
-        [String]               $Date = (Get-Date -UFormat %Y_%m%d-%H%M%S)
-        [String]             $Status = "Initialized"
-        [String]               $Type
-        [String]           $Resource = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.12.0"
+    Manifest(){}
+}
+
+Class Install
+{
+    [Object]                 $OS = [OS]::New()
+    [Object]             $Module = [Manifest]::New()
+
+    [String]               $Name = "FightingEntropy"
+    [String]            $Version = "2020.12.0"
+    [String]           $Provider = "Secure Digits Plus LLC"
+    [String]               $Date = (Get-Date -UFormat %Y_%m%d-%H%M%S)
+    [String]             $Status = "Initialized"
+    [String]               $Type
+    [String]           $Resource = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.12.0"
 
         [String]           $Registry
         [String]               $Path
@@ -211,23 +210,22 @@
             }
         }
 
-        Install([Object]$OS,[Object]$Manifest)
+        Install()
         {
-            $This.OS                 = $OS
-            $This.Module             = $Manifest
+            $This.Type               = $This.OS.Type
 
-            If ( $This.OS.Type -match "Win32" )
+            If ( $This.Type -match "Win32" )
             {
                 $This.Registry       = $This.Root("HKLM:\Software\Policies")
                 $This.Path           = $This.Root($Env:ProgramData)
             }
             
-            Else
+            If ( $This.Type -match "RHELCentOS" )
             {
-                $This.Registry       = $This.Root("/etc/SDP")
-                $This.Path           = $This.Root("/etc/SDP")
+                $This.Registry       = $This.Root("/etc/FEUnix")
+                $This.Path           = $This.Root("/etc/FEUnix")
             }
-            
+
             $This.BuildRegistry()
             $This.BuildPath()
 
@@ -301,30 +299,10 @@
                 }
             }
 
-            $This.BuildModule()
-            $This.BuildManifest()
-
             Import-Module FightingEntropy -Verbose -Force
             
             Write-Theme @("--- Module Installed ---";@{ Info = "To load the module, use Get-FEModule" };" ","Installation variable saved to `$Module")
         }
     }
-
-    $OS       = [OS]::New()
-    $Manifest = [Manifest]::New()
-
-    If ( $OS.Type -match "Win32" )
-    {
-        If ( [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent() | % IsInRole Administrators )
-        {
-            Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-            Add-Type -AssemblyName PresentationFramework
-        }
-
-        Else
-        {
-            Throw "Must run as Administrator"
-        }
-    }
-    
-    $Module = [Install]::New($OS,$Manifest)
+        
+    $Module = [Install]::New()

c5f7e6226130992ba8d53f3cd3a1d42ba9de5335
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-07 18:34:11 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,24 +1,21 @@
-Function Install
+Class FEModule
 {
-    Class FEModule
-    {
-        [String]             $Base  = ("https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.12.0")
-        [String[]]        $Classes_ = ("OS Manifest Hive Install" -Split " " | % { "Classes/_$_.ps1" })
-        Hidden [Object[]] $Classes 
-        Hidden [String[]]    $Swap
-        [String]           $Output
+    [String]             $Base  = ("https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2020.12.0")
+    [String[]]        $Classes_ = ("OS Manifest Hive Install" -Split " " | % { "Classes/_$_.ps1" })
+    Hidden [Object[]] $Classes 
+    Hidden [String[]]    $Swap
+    [String]           $Output
         
-        FEModule()
-        {
-            $This.Classes      = $This.Classes_ | % { Invoke-RestMethod -URI "$($This.Base)/$_" -Verbose }
-            $This.Swap         = $This.Classes -Join "`n"
-            $This.Output       = $This.Swap -Join "`n"
-        }
+    FEModule()
+    {
+        $This.Classes      = $This.Classes_ | % { Invoke-RestMethod -URI "$($This.Base)/$_" -Verbose }
+        $This.Swap         = $This.Classes -Join "`n"
+        $This.Output       = $This.Swap -Join "`n"
     }
-
-    Invoke-Expression ([FEModule]::New().Output)
-
-    [_Install]::New("2020.12.0")
 }
 
-Install
+$Install = [FEModule]::New().Output
+
+Invoke-Expression $Install
+
+$Module = [_Install]::New("2020.12.0")

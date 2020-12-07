7378d3c2228fc32851debef14839eab2ab29a229
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-07 07:33:26 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -232,7 +232,6 @@ Class Install
             {
                 $This.Registry       = $This.Root("/etc/FEUnix")
                 $This.Path           = $This.Root("/etc/FEUnix")
-                
             }
 
             $This.BuildRegistry()
@@ -310,27 +309,27 @@ Class Install
 
             Import-Module FightingEntropy -Verbose -Force
             
-            Write-Theme @("--- Module Installed ---";@{ Info = "To load the module, use Get-FEModule" };" ","Installation variable saved to `$Module")
-            Start-Sleep -Seconds 5
-            
             $PowerShell = @("PowerShell 7 + Windows Terminal, a match made by the gods themselves";"-"*80;("Once upon a time, there was a bunch of peop" +
-              "le sitting around.");" ";("One that, they decided to *really* up the ante on their (widely/historically) known");("ability to engineer t" + 
+              "le sitting around.");" ";("One that just decided to *really* up the ante on their (widely/historically) known");("ability to engineer t" + 
               "he best software on planet Earth.");("After a number of years continually attempting to one-up themselves...?");("They took notice of an" + 
               "ameteur who stood no chance whatsoever, against the cosmos, alone.");" ";("Impressed, with his imbuement of mystical power of (writing/n" + 
               "arration) capabilities...");("...when combined with the total raw horsepower behind PowerShell 7...?");" ";("Maybe you oughtta stop and " + 
               "ask yourself...");"...if you somehow got invited to an action packed cinematic event.";" ";("The only problem with PowerShell 7 + Window" + 
               "s Terminal...");"...is that you won't want to stop using it.";"It's like pringles, but better.";("No contenders on the table, that have " + 
-              "as much potential.";" ";"Could you imagine if perhaps... some guys that have been doing this a long time...";("didn't think that it coul" + 
-              "d come down to this?");" ";"Some senior engineers sat around and watched in shock and awe at the ameteur...";("...as if he were slaying " +
-              "demons in a video game. On Mars. Or, wherever.");"Sometimes in life, that's what ya gotta do. Watch the new kid kick some ass.";("We hav" + 
-              "e no idea if he ever needed any bubble gum... just went around kicking asses, unabated...";" ";" -Microsoft")
-            
-            (Host).UI.PromptForChoice("Funny Stuff?","Do you have a sense of humor?",[String[]]@("&Yes","&Maybe","&No","&Piss off..."),0)
+              "as much potential.");" ";"Could you imagine if perhaps... some guys that have been doing this a long time...";("didn't think that it cou" + 
+              "ld come down to this?");" ";"Some senior engineers sat around and watched in shock and awe at the ameteur...";("...as if he were slaying" +
+              " demons in a video game. On Mars. Or, wherever.");" ";"'Sometimes in life, ya just gotta sit back, and watch the new kid kick some ass.'";
+              "(and)","'We have no idea if he ever needed any bubble gum... just went around kicking asses.'";" ";" -Microsoft")
+
+            Switch((Host).UI.PromptForChoice("Funny Stuff?","Do you have a sense of humor?",[String[]]@("&Yes","&Maybe","&No","&Piss off..."),0))
             {
-                0 { Write-Theme $PowerShell; Start-Sleep -Seconds 60} 1 { Write-Theme PowerShell; Start-Sleep -Seconds 60 }
-                Default {}
+                0 { Write-Theme $PowerShell; Start-Sleep -Seconds 60 }
+                1 { Write-Theme $PowerShell -Palette 11,9,15,0; Start-Sleep -Seconds 60 }
+                2 { Write-Theme "Module [:] Installed" }
+                3 { Write-Theme "Module [:] Installed"; Start-Sleep -Seconds 2; Write-Theme "By the way, you didn't have to be so rude about it..." }
             }
         }
     }
         
     $Module = [Install]::New()
+}

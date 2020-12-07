e63dd816d10228eee6cabbaf0ae0bb502ec99906
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-06 19:54:50 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -182,12 +182,19 @@ Class Install
             }
         }
 
-        BuildRegistry()
+        BuildRegistry([String]$Type)
         {
-            ForEach ( $I in 3..5 ) 
-            {      
-                $This.Registry.Split('\')[0..$I] -join '\' | ? { ! ( Test-Path $_ ) } | % { New-Item $_ -Verbose }
+            If ( $Type -match "Win32" )
+            {
+                ForEach ( $I in 3..5 ) 
+                {      
+                    $This.Registry.Split('\')[0..$I] -join '\' | ? { ! ( Test-Path $_ ) } | % { New-Item $_ -Verbose }
+                }
             }
+            
+            If ( $Type -match "RHELCentOS" )
+            {
+                sudo mkdir /etc/SDP/FEUnix
 
             $Item                    = Get-ItemProperty -Path $This.Registry
             $Names                   = ($This.Module.Names)
@@ -218,12 +225,14 @@ Class Install
             {
                 $This.Registry       = $This.Root("HKLM:\Software\Policies")
                 $This.Path           = $This.Root($Env:ProgramData)
+                
             }
             
             If ( $This.Type -match "RHELCentOS" )
             {
                 $This.Registry       = $This.Root("/etc/FEUnix")
                 $This.Path           = $This.Root("/etc/FEUnix")
+                
             }
 
             $This.BuildRegistry()
@@ -302,6 +311,25 @@ Class Install
             Import-Module FightingEntropy -Verbose -Force
             
             Write-Theme @("--- Module Installed ---";@{ Info = "To load the module, use Get-FEModule" };" ","Installation variable saved to `$Module")
+            Start-Sleep -Seconds 5
+            
+            $PowerShell = @("PowerShell 7 + Windows Terminal, a match made by the gods themselves";"-"*80;("Once upon a time, there was a bunch of peop" +
+              "le sitting around.");" ";("One that, they decided to *really* up the ante on their (widely/historically) known");("ability to engineer t" + 
+              "he best software on planet Earth.");("After a number of years continually attempting to one-up themselves...?");("They took notice of an" + 
+              "ameteur who stood no chance whatsoever, against the cosmos, alone.");" ";("Impressed, with his imbuement of mystical power of (writing/n" + 
+              "arration) capabilities...");("...when combined with the total raw horsepower behind PowerShell 7...?");" ";("Maybe you oughtta stop and " + 
+              "ask yourself...");"...if you somehow got invited to an action packed cinematic event.";" ";("The only problem with PowerShell 7 + Window" + 
+              "s Terminal...");"...is that you won't want to stop using it.";"It's like pringles, but better.";("No contenders on the table, that have " + 
+              "as much potential.";" ";"Could you imagine if perhaps... some guys that have been doing this a long time...";("didn't think that it coul" + 
+              "d come down to this?");" ";"Some senior engineers sat around and watched in shock and awe at the ameteur...";("...as if he were slaying " +
+              "demons in a video game. On Mars. Or, wherever.");"Sometimes in life, that's what ya gotta do. Watch the new kid kick some ass.";("We hav" + 
+              "e no idea if he ever needed any bubble gum... just went around kicking asses, unabated...";" ";" -Microsoft")
+            
+            (Host).UI.PromptForChoice("Funny Stuff?","Do you have a sense of humor?",[String[]]@("&Yes","&Maybe","&No","&Piss off..."),0)
+            {
+                0 { Write-Theme $PowerShell; Start-Sleep -Seconds 60} 1 { Write-Theme PowerShell; Start-Sleep -Seconds 60 }
+                Default {}
+            }
         }
     }
         

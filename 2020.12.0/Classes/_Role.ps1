9d729330844e466a9324ca339c575b491b126607
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-08 11:27:53 -0500

    Update _Role.ps1

--- 2020.12.0/Classes/_Role.ps1
+++ 2020.12.0/Classes/_Role.ps1
@@ -5,8 +5,8 @@ Class _Role
 
     _Role([String]$Name)
     {
-        $This.Name = $Name
-        $This.Role = Switch($Name)
+        $This.Name   = $Name
+        $This.Output = Switch($Name)
         {
             Win32_Client { [_Win32_Client]::New() } Win32_Server { [_Win32_Server]::New() } 
             UnixBSD      { [_UnixBSD]::New()      } RHELCentOS   { [_RHELCentOS]::New()   }

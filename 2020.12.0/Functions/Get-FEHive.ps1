7e0683d5c458f14ba924e856c4f319854feca1e3
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-08 10:21:07 -0500

    Update Get-FEHive.ps1

--- 2020.12.0/Functions/Get-FEHive.ps1
+++ 2020.12.0/Functions/Get-FEHive.ps1
@@ -1,4 +1,13 @@
-Function Get-FEHive ([String]$Type,[String]$Version)
+Function Get-FEHive
 {
+    [CmdLetBinding()]Param(
+    [Parameter(Mandatory)][String]$Type,
+    [Parameter(Mandatory)][String]$Version)
+    
+    If ( $Type -notin "Win32_Client","Win32_Server","RHELCentOS","UnixBSD" )
+    {
+        Throw "Invalid type"
+    }
+
     [_Hive]::new($Type,$Version)
 }

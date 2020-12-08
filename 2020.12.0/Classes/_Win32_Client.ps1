af042366b5a47c879506699c9ca8ca8640d01429
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-08 12:47:50 -0500

    Update _Win32_Client.ps1

--- 2020.12.0/Classes/_Win32_Client.ps1
+++ 2020.12.0/Classes/_Win32_Client.ps1
@@ -13,7 +13,7 @@ Class _Win32_Client
         $This.Host      = [_Host]::New()
         $This.Info      = [_Info]::New()
         $This.Tools     = @("ViperBomb","Chocolatey")
-        $This.Services  = [_Services]::new()
+        $This.Services  = [_Services]::new().Output
         $This.Processes = (Get-Process)
         $This.Network   = [_Network]::new().Interface
     }

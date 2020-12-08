4e854e0ac053ff59709b34a801111c80be11d294
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-07 21:46:32 -0500

    Update _Manifest.ps1

--- 2020.12.0/Classes/_Manifest.ps1
+++ 2020.12.0/Classes/_Manifest.ps1
@@ -1,20 +1,21 @@
 Class _Manifest
 {
     [String[]]     $Names = ( "Name Version Provider Date Path Status Type" -Split " " )
+    [String]     $Version = ( "2020.12.0" )
     [String]        $GUID = ( "67b283d9-72c6-413a-aa80-a24af5d4ea8f" )
-    [String[]]      $Role = ( "{0}Client {0}Server UnixBSD RHEL/CentOS" -f "Win32_" -Split " " )
+    [String[]]      $Role = ( "Win32_Client Win32_Server UnixBSD RHELCentOS" -Split " " )
     [String[]]   $Folders = ( " Classes Control Functions Graphics Role" -Split " " )
-    [String[]]   $Classes = (("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme " + 
-                              "Object Flag Manifest Banner UISwitch XamlWindow XamlObject VendorList V4Network V6Network NetInterface " + 
-                              "Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar Share Master " + 
-                              "Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo Xaml XamlGlossar" + 
-                              "yItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo FEPromoDomain FEP" + 
-                              "romoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" ).Split(" ") | % { "_$_" })
+    [String[]]   $Classes = (("Root Module QMark File FirewallRule Drive Cache Icons Shortcut Drives Host Block Faces Track Theme", 
+                              "Object Flag OS Hive Manifest Banner UISwitch XamlWindow XamlObject VendorList V4Network V6Network",  
+                              "NetInterface Network Info Service Services ViperBomb Brand Branding Certificate Company Key RootVar",
+                              "Share Master Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo", 
+                              "Xaml XamlGlossaryItem Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat FEPromo",
+                              "FEPromoDomain FEPromoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS" -join " " ).Split(" ") | % { "_$_" })
     [String[]]   $Control = ( "Computer.png DefaultApps.xml MDT{0} MDT{1} PSD{0} PSD{1} header-image.png" -f "ClientMod.xml",
                               "ServerMod.xml" ) -Split " "
-    [String[]] $Functions = (("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write-Banner Instal" +
-                              "l-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotification New-FECompany Get-Serve" + 
-                              "rDependency Get-FEServices Get-FEHost").Split(" ") | % { "$_" })
+    [String[]] $Functions = (("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write-Banner", 
+                              "Install-IISServer Add-ACL New-ACLObject Configure-IISServer Show-ToastNotification New-FECompany", 
+                              "Get-ServerDependency Get-FEServices Get-FEHost Get-FEOS Get-FEManifest Get-FEHive" -join " " ).Split(" ") | % { "$_" })
     [String[]]  $Graphics = ("background.jpg banner.png icon.ico OEMbg.jpg OEMlogo.bmp" -Split " ")
 
     _Manifest(){}

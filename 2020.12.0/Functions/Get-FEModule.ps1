b2b69b54b2feba8eb4122ee644f9c355891e43ed
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-07 21:11:59 -0500

    Update Get-FEModule.ps1

--- 2020.12.0/Functions/Get-FEModule.ps1
+++ 2020.12.0/Functions/Get-FEModule.ps1
@@ -4,29 +4,18 @@ Function Get-FEModule
                     HelpUri                 = "http://www.github.com/mcc85sx/FightingEntropy" )]
     Param(                
         [Parameter( ParameterSetName        = "Default"     )][Switch]          $All ,
-        [Parameter( ParameterSetName        = "Registry"    )][Switch]     $Registry ,
-        [Parameter( ParameterSetName        = "Base"        )][Switch]         $Base , 
         [Parameter( ParameterSetName        = "Classes"     )][Switch]      $Classes , 
         [Parameter( ParameterSetName        = "Control"     )][Switch]      $Control , 
         [Parameter( ParameterSetName        = "Functions"   )][Switch]    $Functions , 
         [Parameter( ParameterSetName        = "Graphics"    )][Switch]     $Graphics , 
-        [Parameter( ParameterSetName        = "Tools"       )][Switch]        $Tools , 
-        [Parameter( ParameterSetName        = "Shares"      )][Switch]       $Shares ,
-        [Parameter( ParameterSetName        = "Drives"      )][Switch]       $Drives ,
-        [Parameter( ParameterSetName        = "Certificates")][Switch] $Certificates )
+        [Parameter( ParameterSetName        = "Tools"       )][Switch]        $Tools )
 
     Switch ($PSCmdlet.ParameterSetName) 
     {  
         Default      { [_Module]::New()              }
-        Registry     { [_Module]::New().Registry     }
-        Base         { [_Module]::New().Base         } 
         Classes      { [_Module]::New().Classes      }
         Control      { [_Module]::New().Control      }
         Functions    { [_Module]::New().Functions    }
         Graphics     { [_Module]::New().Graphics     }
-        Tools        { [_Module]::New().Tools        }
-        Shares       { [_Module]::New().Shares       }
-        Drives       { [_Module]::New().Drives       }
-        Certificates { [_Module]::New().Certificates }
     }
 }

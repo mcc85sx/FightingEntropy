9b79f9f74023a89bbf309729bdd8a71c08b73387
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-08 10:10:59 -0500

    Update _RestObject.ps1

--- 2020.12.0/Classes/_RestObject.ps1
+++ 2020.12.0/Classes/_RestObject.ps1
@@ -1,6 +1,7 @@
 Class _RestObject
 {
     [String]           $Name
+    [String]           $Type
     [Object]         $Object
     Hidden [String]     $URI
     Hidden [String] $Outfile
@@ -8,6 +9,7 @@ Class _RestObject
     _RestObject([String]$URI,[String]$Outfile)
     {
         $This.Name    = $URI.Split("/")[-1]
+        $This.Type    = $URI.Split("/")[-2]
         $This.URI     = $URI
         $This.Outfile = $Outfile.Replace("\","/")
 

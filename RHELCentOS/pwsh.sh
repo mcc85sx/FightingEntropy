f6f16e06df13bcde8461246242a894ccfa4071fe
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-12 23:46:21 -0500

    Update pwsh.sh

--- RHELCentOS/pwsh.sh
+++ RHELCentOS/pwsh.sh
@@ -1,1 +1,4 @@
-
+su -                                                                  
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+yum install powershell -y
+sudo pwsh

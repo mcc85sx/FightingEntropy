45adea587ae284869fc52ec44a8bf836c429ca8f
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-14 16:35:14 -0500

    Update Get-ViperBomb.ps1

--- 2020.12.0/Functions/Get-ViperBomb.ps1
+++ 2020.12.0/Functions/Get-ViperBomb.ps1
@@ -3,8 +3,10 @@ Function Get-ViperBomb
     Invoke-Expression "Using Namespace System.Windows.Markup"
     Add-Type -AssemblyName PresentationFramework
     
-    $Viper = [_ViperBomb]::New()
-    $UI    = [_XamlWindow]::New($Viper.Xaml)
+    $Info        = [_Info]::New()
+    $VB          = [_ViperBomb]::New()
+    $Service     = (Get-FEService)
+    $UI          = (Get-XamlWindow -Type FEService)
 
     $UI.Host.Profile_0.Add_Click({0})
     $UI.Host.Profile_1.Add_Click({1})
@@ -17,65 +19,64 @@ Function Get-ViperBomb
     $UI.Host.Profile_8.Add_Click({8})
     $UI.Host.Profile_9.Add_Click({9})
 
-    $UI.Host.Title = ("{0} v{1}" -f $Viper.Name, $Viper.Version )
-
-    $UI.Host.URL.Add_Click(       
-    {
-        Start $Viper.URL
-    })
-
+    $UI.Host.Title = ("{0} v{1}" -f $VB.Name, $VB.Version )
+    $UI.Host.URL.Add_Click(
+        {
+            Start $VB.URL
+        }
+    )
     $UI.Host.About.Add_Click(
-    { 
-        [System.Windows.MessageBox]::New($Viper.About -join "`n","About")
-    })
-
-    $UI.Host.Copyright.Add_Click( 
-    { 
-        [System.Windows.MessageBox]::New($Viper.Copyright -join "`n","Copyright")
-    })
-
-    $UI.Host.MadBomb.Add_Click(   
-    {
-        Start $Viper.MadBomb 
-    })
-
+        {
+            [System.Windows.MessageBox]::New(($VB.About -join "`n"),"About")
+        }
+    )
+    $UI.Host.Copyright.Add_Click(
+        {
+            [System.Windows.MessageBox]::New(($VB.Copyright -join "`n"),"Copyright")
+        }
+    )
+    $UI.Host.MadBomb.Add_Click(
+        {
+            Start $VB.MadBomb
+        }
+    )
     $UI.Host.BlackViper.Add_Click(
-    {
-        Start $Viper.BlackViper
-    })
-
-    $UI.Host.Site.Add_Click(      
-    {
-        Start $Viper.Site
-    })
-
+        {
+            Start $VB.BlackViper
+        }
+    )
+    $UI.Host.Site.Add_Click(
+        {
+            Start $VB.Site
+        }
+    )
     $UI.Host.Help.Add_Click(
     { 
-        [System.Windows.MessageBox]::New($Viper.Help -join "`n","Help")
+        [System.Windows.MessageBox]::New(($VB.Help -join "`n"),"Help")
     })
 
-    $UI.Host.Caption.Content               = $Viper.Info.Caption
-    $UI.Host.ReleaseID.Content             = $Viper.Info.ReleaseID
-    $UI.Host.Version.Content               = $Viper.Info.Version
-    $UI.Host.Chassis.Content               = $Viper.Info.Chassis
+    $UI.Host.Caption.Content               = $Info.Caption
+    $UI.Host.ReleaseID.Content             = $Info.ReleaseID
+    $UI.Host.Version.Content               = $Info.Version
+    $UI.Host.Chassis.Content               = $Info.Chassis
 
-    $UI.Host.DispActive.IsChecked          = $Viper.DispActive
-    $UI.Host.DispInactive.IsChecked        = $Viper.DispInactive
-    $UI.Host.DispSkipped.IsChecked         = $Viper.DispSkipped
+    $UI.Host.DispActive.IsChecked          = $VB.DispActive
+    $UI.Host.DispInactive.IsChecked        = $VB.DispInactive
+    $UI.Host.DispSkipped.IsChecked         = $VB.DispSkipped
 
-    $UI.Host.MiscSimulate.IsChecked        = $Viper.MiscSimulate
-    $UI.Host.MiscXbox.IsChecked            = $Viper.MiscXbox
-    $UI.Host.MiscChange.IsChecked          = $Viper.MiscChange
-    $UI.Host.MiscStopDisabled.IsChecked    = $Viper.MiscStopDisabled
+    $UI.Host.MiscSimulate.IsChecked        = $VB.MiscSimulate
+    $UI.Host.MiscXbox.IsChecked            = $VB.MiscXbox
+    $UI.Host.MiscChange.IsChecked          = $VB.MiscChange
+    $UI.Host.MiscStopDisabled.IsChecked    = $VB.MiscStopDisabled
 
-    $UI.Host.DevErrors.IsChecked           = $Viper.DevErrors 
-    $UI.Host.DevLog.IsChecked              = $Viper.DevLog  
-    $UI.Host.DevConsole.IsChecked          = $Viper.DevConsole
-    $UI.Host.DevReport.IsChecked           = $Viper.DevReport
+    $UI.Host.DevErrors.IsChecked           = $VB.DevErrors 
+    $UI.Host.DevLog.IsChecked              = $VB.DevLog  
+    $UI.Host.DevConsole.IsChecked          = $VB.DevConsole
+    $UI.Host.DevReport.IsChecked           = $VB.DevReport
 
-    $UI.Host.ByBuild.IsChecked             = $Viper.ByBuild
-    $UI.Host.ByEdition.SelectedItem        = $Viper.ByEdition
-    $UI.Host.ByLaptop.IsChecked            = $Viper.ByLaptop 
+    $UI.Host.ByBuild.IsChecked             = $VB.ByBuild
+    $UI.Host.ByEdition.SelectedItem        = $VB.ByEdition
+    $UI.Host.ByLaptop.IsChecked            = $VB.ByLaptop 
         
     $UI.Host.LogSvcSwitch.IsChecked        = 0
     $UI.Host.LogSvcBrowse.IsEnabled        = 0
@@ -129,23 +130,23 @@ Function Get-ViperBomb
         $UI.Host.CsvBrowse.IsEnabled       = 0 
     }
 
-    $UI.Host.LogSvcFile.Text               = $Viper.LogSvcLabel
-    $UI.Host.LogScrFile.Text               = $Viper.LogScrLabel
-    $UI.Host.RegFile.Text                  = $Viper.RegLabel
-    $UI.Host.CsvFile.Text                  = $Viper.CsvLabel 
+    $UI.Host.LogSvcFile.Text               = $VB.LogSvcLabel
+    $UI.Host.LogScrFile.Text               = $VB.LogScrLabel
+    $UI.Host.RegFile.Text                  = $VB.RegLabel
+    $UI.Host.CsvFile.Text                  = $VB.CsvLabel 
 
     $UI.Host.LogSvcBrowse.IsEnabled        = 0
     $UI.Host.LogScrBrowse.IsEnabled        = 0
     $UI.Host.RegBrowse.IsEnabled           = 0
     $UI.Host.CsvBrowse.IsEnabled           = 0
 
-    $UI.Host.ServiceLabel.Content          = $Viper.ServiceLabel
+    $UI.Host.ServiceLabel.Content          = $VB.ServiceLabel
     If ( $UI.Host.ServiceProfile.IsEnabledChanged )
     {
         $UI.Host.ServiceLabel.Content      = $UI.Host.ServiceProfile.SelectedItem.Content
     }
 
-    $UI.Host.ScriptLabel.Content           = $Viper.ScriptLabel
+    $UI.Host.ScriptLabel.Content           = $VB.ScriptLabel
     If ( $UI.Host.ScriptProfile.IsEnabledChanged )
     {
         $UI.Host.ScriptLabel.Content       = $UI.Host.ScriptProfile.SelectedItem.Content
@@ -168,10 +169,10 @@ Function Get-ViperBomb
     
     $DataSource                             = New-Object System.Collections.ObjectModel.ObservableCollection[Object]
     
-    ForEach ( $I in 0..( $Viper.Current.Services.Count - 1 ) )
-    {
+    0..( $Service.Count - 1 ) | % {
+
         $DataSource.Add([Int32]1)
-        $DataSource[$I] = $Viper.Current.Services[$I]
+        $DataSource[$_] = $Service[$_]
     }
     
     $UI.Host.DataGrid.ItemsSource            = $DataSource

b9dbc9dcf154e00e4e3b8d0314b74bf63868f7f3
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-14 14:00:08 -0500

    Delete _QMark.ps1

--- 2020.12.0/Classes/_QMark.ps1
+++
@@ -1,9 +0,0 @@
-Class _QMark
-{
-    [String] $ID
-                
-    _QMark()
-    {
-        $This.ID                = (Get-Service *_* -EA 0 | ? ServiceType -eq 224 | Select-Object -First 1).Name.Split('_')[-1]
-    }
-}

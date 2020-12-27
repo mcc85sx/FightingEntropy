7c726279ac352edf913c74548397c7e10711d817
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-27 09:32:20 -0500

    Update _V4Network.ps1

--- 2020.12.0/Classes/_V4Network.ps1
+++ 2020.12.0/Classes/_V4Network.ps1
@@ -1,5 +1,5 @@
 Class _V4Network
-    {
+{
         [String]            $IPAddress
         [String]                $Class
         [Int32]                $Prefix
@@ -109,11 +109,11 @@ Class _V4Network
 
                         ForEach ( $X in 0..( ( 256 / $Slot ) - 1 ) )
                         {
-                            $Range = ( $X * $Slot ) | % { $_..( $_ + $Slot - 1 ) }
+                            $IRange = ( $X * $Slot ) | % { $_..( $_ + $Slot - 1 ) }
 
-                            If ( $Item[$I] -in $Range )
+                            If ( $Item[$I] -in $IRange )
                             {
-                                "{0}..{1}" -f $Range[0,-1]
+                                "{0}..{1}" -f $IRange[0,-1]
                             }
                         }
                     }
@@ -121,25 +121,25 @@ Class _V4Network
             }) -join '/'
         }
 
-        _V4Network([Object]$Address)
+    _V4Network([Object]$Address)
+    {
+        If ( ! $Address )
         {
-            If ( ! $Address )
-            {
-                Throw "Address Empty"
-            }
-
-            $This.IPAddress = $Address.IPAddress
-            $This.IPCheck()
-
-            $This.Class     = @('N/A';@('A')*126;'Local';@('B')*64;@('C')*32;@('MC')*16;@('R')*15;'BC')[[Int32]$This.IPAddress.Split(".")[0]]
-            $This.Prefix    = $Address.PrefixLength
-            $This.Netmask   = $This.GetNetMask($This.Prefix)
-            $This.Route     = Get-NetRoute -AddressFamily IPV4 | ? InterfaceIndex -eq $Address.InterfaceIndex
-            $This.Network   = $This.Route | ? { ($_.DestinationPrefix -Split "/")[1] -match $This.Prefix } | % { ($_.DestinationPrefix -Split "/")[0] }
-            $This.Gateway   = $This.Route | ? NextHop -ne 0.0.0.0 | % NextHop
-            $This.Subnet    = $This.Route | ? DestinationPrefix -notin 255.255.255.255/32,224.0.0.0/4,0.0.0.0/0 | % DestinationPrefix | Sort-Object
-            $This.Broadcast = ( $This.Subnet | % { ( $_ -Split "/" )[0] } )[-1]
-            $This.GetHostRange()
-            $This.GetRange()
+            Throw "Address Empty"
         }
+
+        $This.IPAddress = $Address.IPAddress
+        $This.IPCheck()
+
+        $This.Class     = @('N/A';@('A')*126;'Local';@('B')*64;@('C')*32;@('MC')*16;@('R')*15;'BC')[[Int32]$This.IPAddress.Split(".")[0]]
+        $This.Prefix    = $Address.PrefixLength
+        $This.Netmask   = $This.GetNetMask($This.Prefix)
+        $This.Route     = Get-NetRoute -AddressFamily IPV4 | ? InterfaceIndex -eq $Address.InterfaceIndex
+        $This.Network   = $This.Route | ? { ($_.DestinationPrefix -Split "/")[1] -match $This.Prefix } | % { ($_.DestinationPrefix -Split "/")[0] }
+        $This.Gateway   = $This.Route | ? NextHop -ne 0.0.0.0 | % NextHop
+        $This.Subnet    = $This.Route | ? DestinationPrefix -notin 255.255.255.255/32,224.0.0.0/4,0.0.0.0/0 | % DestinationPrefix | Sort-Object
+        $This.Broadcast = ( $This.Subnet | % { ( $_ -Split "/" )[0] } )[-1]
+        $This.GetHostRange()
+        $This.GetRange()
     }
+}

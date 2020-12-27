700948057db0f8064ff0177eb9c38a1b3ed78669
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-26 20:48:12 -0500

    Update _NbtScan.ps1

--- 2020.12.0/Classes/_NbtScan.ps1
+++ 2020.12.0/Classes/_NbtScan.ps1
@@ -2,31 +2,17 @@ Class _NbtScan
 {
     Hidden [Object]    $NBTStat = (nbtstat -N)
     Hidden [Object[]]  $Adapter = (Get-NetAdapter)
-    Hidden [String[]]  $Service = (("00/{0}/Workstation {4};01/{0}/Messenger {6};01/{1}/Master Browser;03/{0}/Messenger {6};" + 
-    "06/{0}/RAS Server {6};1F/{0}/NetDDE {6};20/{0}/File Server {6};21/{0}/RAS Client {6};22/{0}/{2} Interchange(MSMail C" + 
-    "onnector);23/{0}/{2} Exchange Store;24/{0}/{2} Directory;30/{0}/{4} Server;31/{0}/{4} Client;43/{0}/{3} Control;44/{" + 
-    "0}/SMS Administrators Remote Control Tool {6};45/{0}/{3} Chat;46/{0}/{3} Transfer;4C/{0}/DEC TCPIP SVC on Windows NT" +
-    ";42/{0}/mccaffee anti-virus;52/{0}/DEC TCPIP SVC on Windows NT;87/{0}/{2} MTA;6A/{0}/{2} IMC;BE/{0}/{5} Agent;BF/{0}" + 
-    "/{5} Application;03/{0}/Messenger {6};00/{1}/{7} Name;1B/{0}/{7} Master Browser;1C/{1}/{7} Controller;1D/{0}/Master " + 
-    "Browser;1E/{1}/Browser {6} Elections;2B/{0}/Lotus Notes Server;2F/{1}/Lotus Notes ;33/{1}/Lotus Notes ;20/{1}/DCA Ir" + 
-    "maLan Gateway Server;01/{1}/MS NetBIOS Browse Service") -f "UNIQUE","GROUP","Microsoft Exchange","SMS Clients Remote",
-    "Modem Sharing","Network Monitor","Service","Domain").Split(";")
+    Hidden [Object[]]  $Service = ([_NBTRef]::New().Output)
     Hidden [Hashtable] $Process 
     [Object[]]          $Output
 
-    [String] SetService ([Object]$Hosts)
+    [String] GetService ([Object]$Hosts)
     {
-        Return @( $This.Service | ? { $_ -match "$($Hosts.ID)/$($Hosts.Type)" } | % { $_.Split("/")[-1] } )
+        Return @( $This.Service | ? ID -match $Hosts.ID | ? Type -eq $Hosts.Type | % Service )
     }
 
     _NbtScan()
     {
-        ForEach ( $I in 0..( $This.Service.Count - 1 ) )
-        { 
-            $This.Service[$I]  = $This.Service[$I]
-        }
-
-        $This.Output           = @( )
         $This.Process          = @{ }
         $X                     = -1
 
@@ -54,7 +40,7 @@ Class _NbtScan
 
                 ForEach ( $I in 0..( $Item.Hosts.Count - 1 ) )
                 {
-                    $Item.Hosts[$I].Service = $This.SetService($Item.Hosts[$I])
+                    $Item.Hosts[$I].Service = $This.GetService($Item.Hosts[$I])
                 }
 
                 $This.Output += $Item
@@ -68,7 +54,7 @@ Class _NbtScan
 
                     ForEach ( $I in 0..( $Item.Hosts.Count - 1 ) )
                     {
-                        $Item.Hosts[$I].Service = $This.SetService($Item.Hosts[$I])
+                        $Item.Hosts[$I].Service = $This.GetService($Item.Hosts[$I])
                     }
 
                     $This.Output += $Item

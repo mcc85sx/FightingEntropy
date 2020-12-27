f5d857af4dd65ecd09fee677e5de88eb2ff7bfc3
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-27 13:36:50 -0500

    Update _PingSweep.ps1

--- 2020.12.0/Classes/_PingSweep.ps1
+++ 2020.12.0/Classes/_PingSweep.ps1
@@ -2,32 +2,35 @@ Class _PingSweep
 {
     [String[]] $IPAddress
     Hidden [Hashtable] $Process
-    [Object] $Buffer            = @( 97..119 + 97..105 | % { "0x{0:x}" -f $_ } )
+    [Object] $Buffer         = @( 97..119 + 97..105 | % { "0x{0:x}" -f $_ } )
     [Object] $Options
     [Object] $Output
+    [Object] $Result
 
     _PingSweep([String[]]$IPAddress)
     {
-        $This.IPAddress         = $IPAddress
+        $This.IPAddress      = $IPAddress
         $This._Refresh()
     }
 
     _Refresh()
     {
-        $This.Process           = @{ }
-        $X                      = 0
+        $This.Process        = @{ }
 
-        ForEach ( $IP in $This.IPAddress )
+        ForEach ( $X in 0..( $This.IPAddress.Count - 1 ) )
         {
-            $This.Options       = [System.Net.NetworkInformation.PingOptions]::new()
-            $This.Process.Add( $X ++ , [System.Net.NetworkInformation.Ping]::new().SendPingAsync($IP,100,$This.Buffer,$This.Options))
+            $IP              = $This.IPAddress[$X]
+
+            $This.Options    = [System.Net.NetworkInformation.PingOptions]::new()
+            $This.Process.Add($X,[System.Net.NetworkInformation.Ping]::new().SendPingAsync($IP,100,$This.Buffer,$This.Options))
         }
 
-        $This.Output            = $This.Process | % GetEnumerator | Sort-Object Name | % Value
-    }
-
-    [Object[]] _Filter()
-    {
-        Return @( $This.Output | % Result | ? Status -ne TimedOut | Sort-Object Address )
+        $This.Output         = @( )
+        
+        ForEach ( $X in 0..( $This.IPAddress.Count - 1 ) ) 
+        {
+            $IP              = $This.IPAddress[$X] 
+            $This.Output    += [_PingObject]::New($X,$IP,$This.Process[$X])
+        }
     }
 }

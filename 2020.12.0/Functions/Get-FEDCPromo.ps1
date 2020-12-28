43baca92f7df16a55ad0abc4468b1dbba188a8de
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-28 11:52:41 -0500

    Update Get-FEDCPromo.ps1

--- 2020.12.0/Functions/Get-FEDCPromo.ps1
+++ 2020.12.0/Functions/Get-FEDCPromo.ps1
@@ -10,11 +10,31 @@ Function Get-FEDCPromo
     $UI.IO.Clone.Add_Click{  $UI.SetMode(3) }
     $UI.IO.Cancel.Add_Click{ $UI.IO.DialogResult = $False }
 
-    $UI.Window.Invoke()
+    $UI.IO.Credential.Add_Click{
 
-    # Credential                    : System.Windows.Controls.TextBox
+        $Popup                             = Get-XAMLWindow -Type Test
+        $Popup.Host.DataGrid.ItemsSource   = $UI.Connection.Output
+        $Popup.Host.DataGrid.SelectedIndex = 0
+        $Popup.Host.DataGrid.Focus()
+
+        $Popup.Host.Cancel.Add_Click(
+        {
+            $Popup.Host.DialogResult       = [Void]$False
+        })
+
+        $Popup.Host.Ok.Add_Click(
+        {
+            $UI.Connection.Return          = $UI.Connection.Output[$Popup.Host.DataGrid.SelectedIndex]
+            $Popup.Host.DialogResult       = [Void]$True
+        })
+
+        $Popup.Invoke()
+        
+    }
      
     # SafeModeAdministratorPassword : System.Windows.Controls.PasswordBox
     # Confirm                       : System.Windows.Controls.PasswordBox
     # Start                         : System.Windows.Controls.Button: Start
+
+    $UI.Window.Invoke()
 }

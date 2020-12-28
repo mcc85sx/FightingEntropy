f34982395c374ebcf834d59a03f8d614faa5d7ff
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-28 17:03:38 -0500

    Update _Manifest.ps1

--- 2020.12.0/Classes/_Manifest.ps1
+++ 2020.12.0/Classes/_Manifest.ps1
@@ -9,7 +9,7 @@ Class _Manifest
                               "Object Flag OS Hive Manifest Banner VendorList V4Network V6Network DNSSuffix DomainName ADLogin",  
                               "NetInterface Network Info ViperBomb Brand Branding Certificate Company Key RootVar PingSweep PingObject",
                               "Share Source Target ServerDependency ServerFeature ServerFeatures IISFeatures IIS DCPromo UISwitch", 
-                              "Image Images Updates ArpHost ArpScan ArpStat NbtHost NbtScan NbtStat NbtObj NbtRef FEPromo",
+                              "Image Images Updates ArpHost ArpScan ArpStat NbtRef NbtHost NbtScan NbtStat NbtObj FEPromo",
                               "FEPromoDomain FEPromoRoles Role Win32_Client Win32_Server UnixBSD RHELCentOS RestObject" -join " " 
                               ).Split(" ") | % { "_$_.ps1" })
     [String[]] $Functions = (("Get-Certificate Get-FEModule Get-ViperBomb Remove-FEShare Write-Theme Write-Flag Write-Banner", 

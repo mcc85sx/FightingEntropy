be4db45e19fa6cb385b0d931ee47fc3b38d035a9
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-28 08:21:43 -0500

    Update _FEPromo.ps1

--- 2020.12.0/Classes/_FEPromo.ps1
+++ 2020.12.0/Classes/_FEPromo.ps1
@@ -1,149 +1,150 @@
 Class _FEPromo
-        {
-            [Object]                              $Window
-            [Object]                                  $IO
-            [Object]                                $Host
-            [Object]                            $Features
-            [Object]                             $Network
-            [Object]                               $Range
-            [Object]                             $HostMap
+{
+    [Object]                              $Window
+    [Object]                                  $IO
+    [Object]                                $Host
+    [Object]                            $Features
+    [Object]                             $Network
+    [Object]                               $Range
+    [Object]                             $HostMap
 
-            [String]                             $Command
-            [Int32]                                 $Mode
-            [String]                                $Slot
-            [String]                          $DomainType
-            [String]                          $ForestMode
-            [String]                          $DomainMode
-            [String]                    $ParentDomainName
+    [String]                             $Command
+    [Int32]                                 $Mode
+    [String]                                $Slot
+    [String]                          $DomainType
+    [String]                          $ForestMode
+    [String]                          $DomainMode
+    [String]                    $ParentDomainName
 
-            [Object]                          $InstallDNS
-            [Object]                 $CreateDNSDelegation
-            [Object]                     $NoGlobalCatalog
-            [Object]             $CriticalReplicationOnly
+    [Object]                          $InstallDNS
+    [Object]                 $CreateDNSDelegation
+    [Object]                     $NoGlobalCatalog
+    [Object]             $CriticalReplicationOnly
 
-            [Object]                        $DatabasePath
-            [Object]                             $LogPath
-            [Object]                          $SysvolPath
+    [Object]                        $DatabasePath
+    [Object]                             $LogPath
+    [Object]                          $SysvolPath
     
-            [Object]                          $Credential
-            [Object]                          $DomainName
-            [Object]                   $DomainNetBIOSName
-            [Object]                       $NewDomainName
-            [Object]                $NewDomainNetBIOSName
-            [Object]                 $ReplicationSourceDC
-            [Object]                            $SiteName
+    [Object]                          $Credential
+    [Object]                          $DomainName
+    [Object]                   $DomainNetBIOSName
+    [Object]                       $NewDomainName
+    [Object]                $NewDomainNetBIOSName
+    [Object]                 $ReplicationSourceDC
+    [Object]                            $SiteName
 
-            [SecureString] $SafeModeAdministratorPassword
-            [String]                             $Profile
+    [SecureString] $SafeModeAdministratorPassword
+    [String]                             $Profile
 
-            [Object]                              $Output
+    [Object]                              $Output
 
-            SetMode([Int32]$Mode)
-            {
-                $This.Mode                              = $Mode
-                $This.Command                           = ("{0}Forest {0}{1} {0}{1} {0}{1}Controller" -f "Install-ADDS","Domain").Split(" ")[$Mode]
-                $This.Slot                              = ("Forest Tree Child Clone" -Split " ")[$Mode]
+    SetMode([Int32]$Mode)
+    {
+        $This.Mode                              = $Mode
+        $This.Command                           = ("{0}Forest {0}{1} {0}{1} {0}{1}Controller" -f "Install-ADDS","Domain").Split(" ")[$Mode]
+        $This.Slot                              = ("Forest Tree Child Clone" -Split " ")[$Mode]
 
-                $This.IO.Forest.IsChecked               = $False
-                $This.IO.Tree.IsChecked                 = $False
-                $This.IO.Child.IsChecked                = $False
-                $This.IO.Clone.IsChecked                = $False
+        $This.IO.Forest.IsChecked               = $False
+        $This.IO.Tree.IsChecked                 = $False
+        $This.IO.Child.IsChecked                = $False
+        $This.IO.Clone.IsChecked                = $False
 
-                $This.IO.$($This.Slot).IsChecked        = $True
+        $This.IO.$($This.Slot).IsChecked        = $True
 
-                $Tray                                   = @("Visible","Collapsed")[@((0,0,1),(1,0,1),(1,1,0),(1,1,0))[$Mode]]
-                $This.IO.ForestModeBox.Visibility       = $Tray[0]
-                $This.IO.DomainModeBox.Visibility       = $Tray[1]
-                $This.IO.ParentDomainNameBox.Visibility = $Tray[2]
-                $This.IO.ParentDomainName.Text          = "<Domain Name>"
+        $Tray                                   = @("Visible","Collapsed")[@((0,0,1),(1,0,1),(1,1,0),(1,1,0))[$Mode]]
+        $This.IO.ForestModeBox.Visibility       = $Tray[0]
+        $This.IO.DomainModeBox.Visibility       = $Tray[1]
+        $This.IO.ParentDomainNameBox.Visibility = $Tray[2]
+        $This.IO.ParentDomainName.Text          = "<Domain Name>"
 
-                $This.DomainType                        = @("-","TreeDomain","ChildDomain","-")[$Mode]
+        $This.DomainType                        = @("-","TreeDomain","ChildDomain","-")[$Mode]
                 
-                $Tray                                   = Switch ($Mode)
-                {
-                    0 { $This.IO.ForestMode.SelectedIndex,$This.IO.DomainMode.SelectedIndex,"-" }
-                    1 { $This.IO.ForestMode.SelectedIndex,"-","-" }
-                    2 { "-","-","<Domain Name>" }
-                    3 { "-","-","<Domain Name>" }
-                }
-                
-                $This.ForestMode                        = $Tray[0]
-                $This.DomainMode                        = $Tray[1]
-                $This.ParentDomainName                  = $Tray[2]
-
-                # Roles
-                $This.InstallDNS                        = [_FEPromoRoles]::New(              "InstallDNS", (1,1,1,1)[$Mode], (1,1,1,1)[$Mode])
-                $This.CreateDNSDelegation               = [_FEPromoRoles]::New(     "CreateDNSDelegation", (1,1,1,1)[$Mode], (0,0,1,0)[$Mode])
-                $This.NoGlobalCatalog                   = [_FEPromoRoles]::New(         "NoGlobalCatalog", (0,1,1,1)[$Mode], (0,0,0,0)[$Mode])
-                $This.CriticalReplicationOnly           = [_FEPromoRoles]::New( "CriticalReplicationOnly", (0,0,0,1)[$Mode], (0,0,0,0)[$Mode])
-
-                ForEach ( $Item in "InstallDNS CreateDNSDelegation NoGlobalCatalog CriticalReplicationOnly".Split(" ") )
-                {
-                    $This.Set_FEPromo_Roles($This.$($Item))
-                }
-
-                # Names
-                $This.Credential                        = [_FEPromoDomain]::New(             "Credential", (0,1,1,1)[$Mode])
-                $This.DomainName                        = [_FEPromoDomain]::New(             "DomainName", (1,0,0,1)[$Mode])
-                $This.DomainNetBIOSName                 = [_FEPromoDomain]::New(      "DomainNetBIOSName", (1,0,0,0)[$Mode])
-                $This.NewDomainName                     = [_FEPromoDomain]::New(          "NewDomainName", (0,1,1,0)[$Mode])
-                $This.NewDomainNetBIOSName              = [_FEPromoDomain]::New(   "NewDomainNetBIOSName", (0,1,1,0)[$Mode])
-                $This.ReplicationSourceDC               = [_FEPromoDomain]::New(    "ReplicationSourceDC", (0,0,0,1)[$Mode])
-                $This.SiteName                          = [_FEPromoDomain]::New(               "SiteName", (0,1,1,1)[$Mode])
-
-                ForEach ( $Item in "Credential DomainName DomainNetBIOSName NewDomainName NewDomainNetBIOSName ReplicationSourceDC SiteName".Split(" ") )
-                {    
-                    $This.Set_FEPromo_Text($This.$($Item))
-                }
-            }
-
-            Set_FEPromo_Roles([Object]$Obj)
-            {
-                $This.IO.$( $Obj.Name ).IsEnabled       = $Obj.IsEnabled
-                $This.IO.$( $Obj.Name ).IsChecked       = $Obj.IsChecked
-            }
-
-            Set_FEPromo_Text([Object]$Obj)
-            {
-                $This.IO."$( $Obj.Name    )".IsEnabled  = $Obj.IsEnabled
-                $This.IO."$( $Obj.Name )Box".Visibility = @("Collapsed","Visible")[$Obj.IsEnabled]
-                $This.IO."$( $Obj.Name    )".Text       = ""
-            }
-
-            _FEPromo([Object]$Window,[Int32]$Mode)
-            {
-                $This.Window                            = $Window
-                $This.IO                                = $Window.Host
-                $This.Host                              = Get-FEModule | % Role | % Host
-                $This.Host._Network()
-                $This.Network                           = $This.Host.Network
-                $HostRange                              = $This.Host.Network.Interface.IPV4 | ? Gateway | % Range
-
-                $This.Range                             = [_PingSweep]::New($HostRange -Split "`n")
-                $This.HostMap                           = $This.Range._Filter()
-
-                ForEach ( $I in $This.HostMap )
-                {
-                    $I.NetBIOS                          = nbtstat -a $I.Address | ? { $_ -match "Registered" } | % { [_NbtHost]::New($This.Network.NBT,$_) }
-                }
-
-                $This.Features                          = [_ServerFeatures]::New().Output
-                
-                ForEach ( $Feature in $This.Features ) 
-                {
-                    $This.IO.$($Feature.Name).IsEnabled = !$Feature.Installed
-                    $This.IO.$($Feature.Name).IsChecked = "True"
-                }
-
-                $This.DatabasePath                      = "$Env:SystemRoot\NTDS"
-                $This.IO.DatabasePath.Text              = $This.DatabasePath
-
-                $This.LogPath                           = "$Env:SystemRoot\NTDS"
-                $This.IO.LogPath.Text                   = $This.LogPath 
-
-                $This.SysvolPath                        = "$Env:SystemRoot\NTDS"
-                $This.IO.SysvolPath.Text                = $This.SysvolPath
-
-                $This.SetMode($Mode)
-            }
+        $Tray                                   = Switch ($Mode)
+        {
+            0 { $This.IO.ForestMode.SelectedIndex,$This.IO.DomainMode.SelectedIndex,"-" }
+            1 { $This.IO.ForestMode.SelectedIndex,"-","-" }
+            2 { "-","-","<Domain Name>" }
+            3 { "-","-","<Domain Name>" }
         }
+                
+        $This.ForestMode                        = $Tray[0]
+        $This.DomainMode                        = $Tray[1]
+        $This.ParentDomainName                  = $Tray[2]
+
+        # Roles
+        $This.InstallDNS                        = [_FEPromoRoles]::New(              "InstallDNS", (1,1,1,1)[$Mode], (1,1,1,1)[$Mode])
+        $This.CreateDNSDelegation               = [_FEPromoRoles]::New(     "CreateDNSDelegation", (1,1,1,1)[$Mode], (0,0,1,0)[$Mode])
+        $This.NoGlobalCatalog                   = [_FEPromoRoles]::New(         "NoGlobalCatalog", (0,1,1,1)[$Mode], (0,0,0,0)[$Mode])
+        $This.CriticalReplicationOnly           = [_FEPromoRoles]::New( "CriticalReplicationOnly", (0,0,0,1)[$Mode], (0,0,0,0)[$Mode])
+
+        ForEach ( $Item in "InstallDNS CreateDNSDelegation NoGlobalCatalog CriticalReplicationOnly".Split(" ") )
+        {
+            $This.Set_FEPromo_Roles($This.$($Item))
+        }
+
+        # Names
+        $This.Credential                        = [_FEPromoDomain]::New(             "Credential", (0,1,1,1)[$Mode])
+        $This.DomainName                        = [_FEPromoDomain]::New(             "DomainName", (1,0,0,1)[$Mode])
+        $This.DomainNetBIOSName                 = [_FEPromoDomain]::New(      "DomainNetBIOSName", (1,0,0,0)[$Mode])
+        $This.NewDomainName                     = [_FEPromoDomain]::New(          "NewDomainName", (0,1,1,0)[$Mode])
+        $This.NewDomainNetBIOSName              = [_FEPromoDomain]::New(   "NewDomainNetBIOSName", (0,1,1,0)[$Mode])
+        $This.ReplicationSourceDC               = [_FEPromoDomain]::New(    "ReplicationSourceDC", (0,0,0,1)[$Mode])
+        $This.SiteName                          = [_FEPromoDomain]::New(               "SiteName", (0,1,1,1)[$Mode])
+
+        ForEach ( $Item in "Credential DomainName DomainNetBIOSName NewDomainName NewDomainNetBIOSName ReplicationSourceDC SiteName".Split(" ") )
+        {    
+            $This.Set_FEPromo_Text($This.$($Item))
+        }
+    }
+
+    Set_FEPromo_Roles([Object]$Obj)
+    {
+        $This.IO.$( $Obj.Name ).IsEnabled       = $Obj.IsEnabled
+        $This.IO.$( $Obj.Name ).IsChecked       = $Obj.IsChecked
+    }
+
+    Set_FEPromo_Text([Object]$Obj)
+    {
+        $This.IO."$( $Obj.Name    )".IsEnabled  = $Obj.IsEnabled
+        $This.IO."$( $Obj.Name )Box".Visibility = @("Collapsed","Visible")[$Obj.IsEnabled]
+        $This.IO."$( $Obj.Name    )".Text       = ""
+    }
+
+    _FEPromo([Object]$Window,[Int32]$Mode)
+    {
+        $This.Window                            = $Window
+        $This.IO                                = $Window.Host
+        $This.Host                              = Get-FEModule | % Role | % Host
+        $This.Host._Network()
+        $This.Network                           = $This.Host.Network
+        $HostRange                              = $This.Host.Network.Interface.IPV4 | ? Gateway | % Range
+
+        $This.Range                             = [_PingSweep]::New($HostRange -Split "`n")
+        $This.HostMap                           = $This.Range._Filter()
+
+        ForEach ( $I in $This.HostMap )
+        {
+            Write-Host "[+] $($I.HostName)/$($I.Address)"
+            $I.NetBIOS                          = nbtstat -a $I.Address | ? { $_ -match "Registered" } | % { [_NbtHost]::New($This.Network.NBT,$_) }
+        }
+
+        $This.Features                          = [_ServerFeatures]::New().Output
+                
+        ForEach ( $Feature in $This.Features ) 
+        {
+            $This.IO.$($Feature.Name).IsEnabled = !$Feature.Installed
+            $This.IO.$($Feature.Name).IsChecked = "True"
+        }
+
+        $This.DatabasePath                      = "$Env:SystemRoot\NTDS"
+        $This.IO.DatabasePath.Text              = $This.DatabasePath
+
+        $This.LogPath                           = "$Env:SystemRoot\NTDS"
+        $This.IO.LogPath.Text                   = $This.LogPath 
+
+        $This.SysvolPath                        = "$Env:SystemRoot\NTDS"
+        $This.IO.SysvolPath.Text                = $This.SysvolPath
+
+        $This.SetMode($Mode)
+    }
+}

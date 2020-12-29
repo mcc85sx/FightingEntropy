bd6cadecc898c8a02c215645fe31399190689342
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-29 12:38:12 -0500

    Update _FEDCPromoProfile.ps1

--- 2020.12.0/Classes/_FEDCPromoProfile.ps1
+++ 2020.12.0/Classes/_FEDCPromoProfile.ps1
@@ -1,37 +1,72 @@
 Class _FEDCPromoProfile
 {
-    [UInt32]           $Mode
-    Hidden [Hashtable] $Hash = @{ 
+    [Object]                $UI
+    [UInt32]              $Mode
+    Hidden [Hashtable]    $Tags = @{ 
 
-        Slot  = "Forest Tree Child Clone" -Split " "
-        Types = "ForestMode DomainMode DomainType ParentDomainName " -Split " "
-        Names = "DomainName DomainNetBIOSName SiteName NewDomainName NewDomainNetBIOSName" -Split " "
-        Roles = "InstallDns CreateDnsDelegation CriticalReplicationOnly NoGlobalCatalog" -Split " "
+        Slot                    = "Forest Tree Child Clone" -Split " "
+        Type                    = "ForestMode DomainMode DomainType" -Split " "
+        Text                    = "Parent{0} {0} Domain{1} SiteName New{0} NewDomain{1}" -f "DomainName","NetBIOSName" -Split " "
+        Role                    = "InstallDns CreateDnsDelegation CriticalReplicationOnly NoGlobalCatalog" -Split " "
     }
 
-    Hidden [Hashtable] $Grid = @{ 
+    [Object]              $Slot
+    [Object]              $Type
+    [Object]              $Text
+    [Object]              $Role
 
-        Types = ((0,1),(1..3),(1..3),(4))
-        Names = ((0,1),(2..4),(2..4),(0,2))
-        Roles = ((0,1),(0,1,3),(0,1,3),(0..3))
-    }
-
-    [String]           $Slot
-    [Object[]]        $Types
-    [Object[]]        $Names
-    [Object[]]        $Roles
-
-    _FEDCPromoProfile([UInt32]$Mode)
+    _FEDCPromoProfile([Object]$UI,[UInt32]$Mode)
     {
         If ( $Mode -notin 0..3 )
         {
             Throw "Invalid Entry"
         }
 
+        $This.UI    = $UI
         $This.Mode  = $Mode
-        $This.Slot  = $This.Hash.Slot[$Mode]
-        $This.Types = $This.Hash.Types[$This.Grid.Types[$Mode]]
-        $This.Names = $This.Hash.Names[$This.Grid.Names[$Mode]]
-        $This.Roles = $This.Hash.Roles[$This.Grid.Roles[$Mode]]
+        $This.Slot  = $This.Tags.Slot[$Mode]
+        $This.Type  = $This.Tags.Type | % { $This.GetFEDCPromoType($Mode,$_) }
+        $This.Text  = $This.Tags.Text | % { $This.GetFEDCPromoText($Mode,$_) }
+        $This.Role  = $This.Tags.Role | % { $This.GetFEDCPromoRole($Mode,$_) }
+    }
+
+    [Object] GetFEDCPromoType([UInt32]$Mode,[String]$Type)
+    {
+        $Item = Switch($Type)
+        {
+            ForestMode            {1,0,0,0}
+            DomainMode            {1,1,1,0}
+            DomainType            {0,1,1,0}
+        }
+
+        Return @([_FEDCPromoType]::New($Type,$Item[$Mode]) )
+    }
+
+    [Object] GetFEDCPromoText([UInt32]$Mode,[String]$Type)
+    {
+        $Item = Switch($Type)
+        {
+            ParentDomainName      {0,1,1,0}
+	        DomainName            {1,0,0,1}
+	        DomainNetbiosName     {1,0,0,0}
+	        SiteName              {0,1,1,1}
+	        NewDomainName         {0,1,1,0}
+	        NewDomainNetbiosName  {0,1,1,0}
+        }
+
+        Return @([_FEDCPromoText]::New($Type,$Item[$Mode]))
+    }
+
+    [Object] GetFEDCPromoRole([UInt32]$Mode,[String]$Type)
+    {
+        $Item = Switch($Type)
+        {
+            InstallDNS              {(1,1,1,1),(1,1,1,1)}
+            CreateDNSDelegation     {(1,1,1,1),(0,0,1,0)}
+            NoGlobalCatalog         {(0,1,1,1),(0,0,0,0)}
+            CriticalReplicationOnly {(0,0,0,1),(0,0,0,0)}
+        }
+
+        Return @([_FEDCPromoRole]::New($Type,$Item[0][$Mode],$Item[1][$Mode]))
     }
 }

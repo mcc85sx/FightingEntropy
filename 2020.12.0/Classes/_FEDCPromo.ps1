00622b5359fa64678c2c8aef0961f03989af9a9c
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-29 23:33:20 -0500

    Update _FEDCPromo.ps1

--- 2020.12.0/Classes/_FEDCPromo.ps1
+++ 2020.12.0/Classes/_FEDCPromo.ps1
@@ -37,87 +37,86 @@ Class _FEDCPromo
 
     SetMode([Int32]$Mode)
     {
-        $This.Command                            = ("{0}Forest {0}{1} {0}{1} {0}{1}Controller" -f "Install-ADDS","Domain").Split(" ")[$Mode]
-        $This.Mode                               = $Mode
-        $This.Profile                            = (Get-FEDCPromoProfile -Mode $Mode)
+        $This.Command                              = ("{0}Forest {0}{1} {0}{1} {0}{1}Controller" -f "Install-ADDS","Domain").Split(" ")[$Mode]
+        $This.Mode                                 = $Mode
+        $This.Profile                              = (Get-FEDCPromoProfile -Mode $Mode)
 
-        $This.IO.Forest.IsChecked                = $False
-        $This.IO.Tree.IsChecked                  = $False
-        $This.IO.Child.IsChecked                 = $False
-        $This.IO.Clone.IsChecked                 = $False
+        $This.IO.Forest.IsChecked                  = $False
+        $This.IO.Tree.IsChecked                    = $False
+        $This.IO.Child.IsChecked                   = $False
+        $This.IO.Clone.IsChecked                   = $False
 
-        $This.IO.$($This.Profile.Slot).IsChecked = $True
+        $This.IO.$($This.Profile.Slot).IsChecked   = $True
 
-        ForEach ( $Item in $This.Profile.Type )
+        # Domain Type/Parent/RepDC
+        ForEach ( $Type in $This.Profile.Type )
         {
-            If ( $Item.Name -ne "DomainType" )
+            $This.IO."_$($Type.Name)".Visibility   = @("Collapsed","Visible")[$Type.IsEnabled]
+            
+            If ( $Type.IsEnabled )
             {
-                $This.IO."_$($Item.Name)".Visibility = @("Collapsed","Visible")[$Item.IsEnabled]
-            }
-
-            If (!$Item.IsEnabled)
-            {
-                $Item.Value = ""
-            }
-
-            Else
-            {
-                $Item.Value = Switch($Item.Name)
+                $Type.Value                        = Switch($Type.Name)
                 {
                     ForestMode          { $This.IO.ForestMode.SelectedIndex  }
                     DomainMode          { $This.IO.DomainMode.SelectedIndex  }
-                    DomainType          {     $This.Profile.Slot + "Domain"  }
                     ParentDomainName    {                         "<Domain>" }
                     ReplicationSourceDC {                            "<Any>" }
                 }
             }
+
+            Else 
+            {
+                $Type.Value                        = ""
+            }
+
+            $This.IO.$($Type.Name).IsEnabled       = $Type.IsEnabled
+            $This.IO.$($Type.Name).$(@("Text","SelectedIndex")[$Type.Name -match "Mode"]) = $Type.Value
         }
 
         # Domain/Text
-        $This.ParentDomainName                    = $This.Profile.Text.ParentDomainName
-        $This.Credential                          = $This.Profile.Text.Credential
-        $This.DomainName                          = $This.Profile.Text.DomainName
-        $This.DomainNetBIOSName                   = $This.Profile.Text.DomainNetBIOSName
-        $This.NewDomainName                       = $This.Profile.Text.NewDomainName
-        $This.NewDomainNetBIOSName                = $This.Profile.Text.NewDomainNetBIOSName
-        $This.SiteName                            = $This.Profile.Text.Sitename
+
+        ForEach ( $Text in $This.Profile.Text )
+        {
+            $This.IO."$(  $Text.Name )".IsEnabled  = $Text.IsEnabled
+            $This.IO."_$( $Text.Name )".Visibility = @("Collapsed","Visible")[$Text.IsEnabled]
+            $This.IO."$(  $Text.Name )".Text       = $Text.Text
+        }
+        #$This.IO.DomainName                       = $This.Profile.Text.DomainName
+        #$This.IO.DomainNetBIOSName                = $This.Profile.Text.DomainNetBIOSName
+        #$This.IO.NewDomainName                    = $This.Profile.Text.NewDomainName
+        #$This.IO.NewDomainNetBIOSName             = $This.Profile.Text.NewDomainNetBIOSName
+        #$This.IO.SiteName                         = $This.Profile.Text.Sitename
 
         # Roles
-        $This.InstallDNS                          = $This.Profile.Role.InstallDNS
-        $This.CreateDNSDelegation                 = $This.Profile.Role.CreateDNSDelegation
-        $This.NoGlobalCatalog                     = $This.Profile.Role.NoGlobalCatalog
-        $This.CriticalReplicationOnly             = $This.Profile.Role.CriticalReplicationOnly
+        $This.InstallDNS                           = $This.Profile.Role.InstallDNS
+        $This.CreateDNSDelegation                  = $This.Profile.Role.CreateDNSDelegation
+        $This.NoGlobalCatalog                      = $This.Profile.Role.NoGlobalCatalog
+        $This.CriticalReplicationOnly              = $This.Profile.Role.CriticalReplicationOnly
+
+        ForEach ( $Role in $This.Profile.Role )
+        {
+            $This.IO.$( $Role.Name ).IsEnabled     = $Role.IsEnabled
+            $This.IO.$( $Role.Name ).IsChecked     = $Role.IsChecked
+        }
 
         If ( !!$This.Connection.Credential )
         {
-            $This.IO.Credential.Text            = $This.Connection.Credential.Username
-            $This.IO.Credential.IsEnabled       = $False
+            $This.IO.Credential.Text             = $This.Connection.Credential.Username
+            $This.IO.Credential.IsEnabled        = $False
         }
 
-        $This.Output                            = @( )
+        $This.Output                             = @( )
     }
 
-    [Void] SetFEDCPromoRole([Object]$Obj)
+    GetADConnection()
     {
-        $This.IO.$($Obj.Name).IsEnabled         = $Obj.IsEnabled
-        $This.IO.$($Obj.Name).IsChecked         = $Obj.IsChecked
-    }
-
-    [Void] SetFEDCPromoText([Object]$Obj)
-    {
-        $This.IO."$($Obj.Name)".IsEnabled       = $Obj.IsEnabled
-        $This.IO."_$($Obj.Name)".Visibility     = @("Collapsed","Visible")[$Obj.IsEnabled]
-        $This.IO."$($Obj.Name)".Text            = $Obj.Text
-    }
-
-    Get_ADConnection()
-    {
-        $This.Connection                        = [_ADConnection]::New($This.HostMap)
+        $This.Connection                         = [_ADConnection]::New($This.HostMap)
     }
 
     HostRange()
     {
-        $This.Range = [_PingSweep]::New(($This.Host.Network.Interface.IPV4 | ? Gateway | % Range | Select -Unique | % Split `n))
+        $This.Range                              = [_PingSweep]::New(
+        ($This.Host.Network.Interface.IPV4 | ? Gateway | % Range | Select -Unique | % Split `n))
     }
 
     _FEDCPromo([Object]$Window,[Int32]$Mode)
@@ -129,6 +128,11 @@ Class _FEDCPromo
         $This.Network                           = $This.Host.Network
         $This.HostRange()
         $This.HostMap                           = $This.Range._Filter()
+
+        If ( $This.Hostmap )
+        {
+            $This.GetADConnection()
+        }
 
         $This.Features                          = [_ServerFeatures]::New().Output
                 

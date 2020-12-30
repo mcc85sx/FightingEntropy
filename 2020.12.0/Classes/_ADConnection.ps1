2e26fe97ca1258a0b9c598fc20c32b0a3226fc7b
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-29 22:03:10 -0500

    Update _ADConnection.ps1

--- 2020.12.0/Classes/_ADConnection.ps1
+++ 2020.12.0/Classes/_ADConnection.ps1
@@ -3,31 +3,22 @@ Class _ADConnection
     [Object] $Primary
     [Object] $Secondary
     [Object] $Swap
-    [Object] $Return
     [Object] $Target
     [Object] $Credential
     [Object] $Output
 
     _ADConnection([Object]$Hostmap)
     {
-        $This.Primary               = $HostMap | ? { $_.NBT.ID -match "1b" }
-        $This.Secondary             = $HostMap | ? { $_.NBT.ID -match "1c" }
-        $This.Swap                  = @( )
+        $This.Primary                        = $HostMap | ? { $_.NBT.ID -match "1b" }
+        $This.Secondary                      = $HostMap | ? { $_.NBT.ID -match "1c" }
+        $This.Swap                           = @( )
         
-        $This.Target                = $Null
-        $This.Credential            = $Null
+        $This.Target                         = $Null
+        $This.Credential                     = $Null
 
-        If ( $This.Primary )
-        {
-            $This.Swap              += $This.Primary.NBT   | ? ID -eq 1b
-        }
+        If ( $This.Primary   ) { $This.Swap += $This.Primary   }
+        If ( $This.Secondary ) { $This.Swap += $This.Secondary }
 
-        If ( $This.Secondary )
-        {
-            $This.Swap              += $This.Secondary.NBT | ? ID -eq 1c
-        }
-
-        $This.Return                = $This.Swap   | Select-Object -Unique
-        $This.Output                = $This.Return | Select-Object IPAddress, Hostname, NetBIOS
+        $This.Output                         = $This.Swap  | Select-Object -Unique IPAddress, Hostname, NetBIOS
     }
 }

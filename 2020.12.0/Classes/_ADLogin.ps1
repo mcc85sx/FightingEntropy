705e26c436040a315b975a720c195cb5c0ceaf44
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2020-12-30 19:16:21 -0500

    Update _ADLogin.ps1

--- 2020.12.0/Classes/_ADLogin.ps1
+++ 2020.12.0/Classes/_ADLogin.ps1
@@ -13,7 +13,7 @@ Class _ADLogin
 
     [String]                                  $DC
     [String]                           $Directory
-    Hidden [Object]                         $Test
+    [Object]                                $Test
     [Object]                            $Searcher
     [Object]                              $Result
 
@@ -27,60 +27,12 @@ Class _ADLogin
         $This.Port         = 389
         $This.DC           = $This.DNSName.Split(".")[0]
         $This.Domain       = $This.DNSName.Replace($This.DC + '.','')
-        $This.Directory    = "LDAP://$( $This.DC )/CN=Partitions,CN=Configuration,DC=$( $This.Domain.Split('.') -join ',DC=')"
     }
 
-    TestCredential()
-    {
-        $Username      = $This.IO.Username
-        $Password      = $This.IO.Password
-        $Confirm       = $This.IO.Confirm
-
-        If (!$Username.Text)
-        {
-            [System.Windows.MessageBox]::Show("Invalid Username","Error")
-        }
-
-        ElseIf (!$Password.Password)
-        {
-            [System.Windows.MessageBox]::Show("Invalid Password","Error")
-        }
-
-        ElseIf (!$Password.Password -notmatch $Confirm.Password)
-        {
-            [System.Windows.MessageBox]::Show("Passwords do not match","Error")
-        }
-
-        Else
-        {
-            Try 
-            {
-                $This.Credential           = [System.Management.Automation.PSCredential]::New($Username.Text,$Confirm.SecurePassword)
-                $This.Test                 = [System.DirectoryServices.DirectoryEntry]::New($This.Directory,$Username.Text,$Password.Password)
-
-                If ( $? -eq $True )
-                {
-                    $This.Initialize()
-                }
-            }
-
-            Catch
-            {
-                $This.Credential           = $Null
-                $This.Test                 = $Null
-                $Username                  = $Null
-                $Password                  = $Null
-                $Confirm                   = $Null
-
-                [System.Windows.MessageBox]::Show("Invalid Account","Error")
-            }
-        }
-    }
-
-    Initialize()
+    Initialize([Object]$SearchRoot)
     {
         $This.Searcher                     = [System.DirectoryServices.DirectorySearcher]::New()
-        $This.Searcher.SearchRoot          = $This.Test
+        $This.Searcher.SearchRoot          = $SearchRoot
         $This.Searcher.PageSize            = 1000
         $This.Searcher.PropertiesToLoad.Clear()
         $This.Result                       = $This.Searcher.FindAll()

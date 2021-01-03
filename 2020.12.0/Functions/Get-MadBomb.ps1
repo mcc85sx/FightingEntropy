452e5975a53a919bcf99598407c3681e7b38a6ae
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-03 01:29:44 -0500

    Update Get-MadBomb.ps1

--- 2020.12.0/Functions/Get-MadBomb.ps1
+++ 2020.12.0/Functions/Get-MadBomb.ps1
@@ -221,24 +221,137 @@ Function Get-MadBomb
         }
     }
 
+    Class AppXObject
+    {
+        Hidden [String[]] $Line
+        [String] $AppXName
+        [String] $CName
+        [String] $VarName
+        
+        AppX([String]$Line)
+        {
+            $This.Line     = $Line.Split(";")
+            $This.AppXName = $This.Line[0]
+            $This.CName    = $This.Line[1]
+            $This.VarName  = "`${0}" -f $This.Line[2]
+        }
+    }
+
+    Class AppXCollection
+    {
+        [String] $List     = ('Microsoft.3DBuilder;3DBuilder;APP_3DBuilder,Microsoft.Microsoft3DViewer;3DViewer;APP_3DViewer,Microsoft' +
+                              '.BingWeather;Bing Weather;APP_BingWeather,Microsoft.CommsPhone;Phone;APP_CommsPhone,Microsoft.windowsco' +
+                              'mmunicationsapps;Calendar & Mail;APP_Communications,Microsoft.GetHelp;Microsofts Self-Help;APP_GetHelp,' +
+                              'Microsoft.Getstarted;Get Started Link;APP_Getstarted,Microsoft.Messaging;Messaging;APP_Messaging,Micros' + 
+                              'oft.MicrosoftOfficeHub;Get Office Link;APP_MicrosoftOffHub,Microsoft.MovieMoments;Movie Moments;APP_Mov' + 
+                              'ieMoments,4DF9E0F8.Netflix;Netflix;APP_Netflix,Microsoft.Office.OneNote;Office OneNote;APP_OfficeOneNot' + 
+                              'e,Microsoft.Office.Sway;Office Sway;APP_OfficeSway,Microsoft.OneConnect;One Connect;APP_OneConnect,Micr' + 
+                              'osoft.People;People;APP_People,Microsoft.Windows.Photos;Photos;APP_Photos,Microsoft.SkypeApp;Skype;APP_' + 
+                              'SkypeApp1,Microsoft.MicrosoftSolitaireCollection;Microsoft Solitaire;APP_SolitaireCollect,Microsoft.Mic' + 
+                              'rosoftStickyNotes;Sticky Notes;APP_StickyNotes,Microsoft.WindowsSoundRecorder;Voice Recorder;APP_VoiceR' + 
+                              'ecorder,Microsoft.WindowsAlarms;Alarms and Clock;APP_WindowsAlarms,Microsoft.WindowsCalculator;Calculat' +
+                              'or;APP_WindowsCalculator,Microsoft.WindowsCamera;Camera;APP_WindowsCamera,Microsoft.WindowsFeedback;Win' + 
+                              'dows Feedback;APP_WindowsFeedbak1,Microsoft.WindowsFeedbackHub;Windows Feedback Hub;APP_WindowsFeedbak2' +
+                              ',Microsoft.WindowsMaps;Maps;APP_WindowsMaps,Microsoft.WindowsPhone;Phone Companion;APP_WindowsPhone,Mic' +
+                              'rosoft.WindowsStore;Microsoft Store;APP_WindowsStore,Microsoft.Wallet;Stores Credit and Debit Card Info' +
+                              'rmation;APP_WindowsWallet,$Xbox_Apps;Xbox Apps (All);APP_XboxApp,Microsoft.ZuneMusic;Groove Music;APP_Z' +
+                              'uneMusic,Microsoft.ZuneVideo;Groove Video;APP_ZuneVideo')
+        [Object] $Output
+        
+        Reference()
+        {
+            $This.Output = @( )
+
+            ForEach ( $Item in $This.List -Split "," )
+            {
+                $This.Output += [AppxObject]::New($Item)
+            }
+        }
+    }
+
+    Class Script
+    {
+        # Script Revised by mcc85sx
+        [String] $Author  = 'MadBomb122'
+        [String] $Version = '3.7.0'
+        [String] $Date    = 'Jan-02-2021'
+        [String] $Release = 'Stable'
+        [String] $Site    = 'https://GitHub.com/madbomb122/Win10Script'
+        [String] $URL
+        [String] $Donate  = 'https://www.amazon.com/gp/registry/wishlist/YBAYWBJES5DE/'
+
+        Script(){}
+    }
+
+    Class Control
+    {
+        [Object] $UI_RestorePoint
+        [Object] $UI_ShowSkipped
+        [Object] $UI_Restart
+        [Object] $UI_VersionCheck
+        [Object] $UI_InternetCheck
+        [Object] $UI_Save
+        [Object] $UI_Load
+        [Object] $UI_WinDefault
+        [Object] $UI_ResetDefault
+
+        Control(){}
+    }
+
     Class Madbomb
     {
         [Object]                     $Window
         [Object]                         $IO
         [Object]                     $Config
+        [Object]                     $Script
+        [Object]                    $Control
 
         MadBomb([Object]$Window)
         {
             $This.Window                     = $Window
             $This.IO                         = $Window.IO
             $This.Config                     = [Config]::New()
+            $This.Script                     = [Script]::New()
+            $This.Control                    = [Control]::New()
+        }
+
+        [Void] Toggle([Object]$Item)
+        {
+            $Item = Switch ($Item)
+            {
+                0 { 1 }
+                1 { 0 }
+            }
         }
     }
 
     $UI = [MadBomb]::New((Get-XamlWindow -Type MBWin10))
+
+    $UI.IO.UI_Feedback.Add_Click({      Start https://github.com/madbomb122/Win10Script/issues })
+    $UI.IO.UI_FAQ.Add_Click({           Start https://github.com/madbomb122/Win10Script/blob/master/README.md })
+    $UI.IO.UI_About.Add_Click({         [System.Windows.Messagebox]::Show('This script performs various settings/tweaks for Windows 10.','About','OK') })
+    $UI.IO.UI_Copyright.Add_Click({     [System.Windows.Messagebox]::Show($Copyright) })
+    $UI.IO.UI_Contact.Add_Click({ })
+    $UI.IO.UI_Donation.Add_Click({      Start https://www.amazon.com/gp/registry/wishlist/YBAYWBJES5DE/ })
+    $UI.IO.UI_Madbomb.Add_Click({       Start https://github.com/madbomb122/ })
+    
+    $UI.IO.UI_RestorePoint.Add_Click({  $UI.Toggle($UI.Control.UI_RestorePoint)  })  
+    $UI.IO.UI_ShowSkipped.Add_Click({   $UI.Toggle($UI.Control.UI_ShowSkipped)   }) 
+    $UI.IO.UI_Restart.Add_Click({       $UI.Toggle($UI.Control.UI_Restart)       }) 
+    $UI.IO.UI_VersionCheck.Add_Click({  $UI.Toggle($UI.Control.UI_VersionCheck)  }) 
+    $UI.IO.UI_InternetCheck.Add_Click({ $UI.Toggle($UI.Control.UI_InternetCheck) }) 
+    $UI.IO.UI_Save.Add_Click({          $UI.Toggle($UI.Control.UI_Save)          }) 
+    $UI.IO.UI_Load.Add_Click({          $UI.Toggle($UI.Control.UI_Load)          }) 
+    $UI.IO.UI_WinDefault.Add_Click({    $UI.Toggle($UI.Control.UI_WinDefault)    }) 
+    $UI.IO.UI_ResetDefault.Add_Click({  $UI.Toggle($UI.Control.UI_ResetDefault)  }) 
+
+    #$UI.IO._Telemetry
     
     # Controls go here, everything pivots around the above variable.
     
     # Once the controls are linked, then use $UI.Window.Invoke()
+    #
     $UI.Window.Invoke()
+
+    $UI
 }

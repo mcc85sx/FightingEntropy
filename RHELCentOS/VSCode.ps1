d0c7eed17f4afcbc858a9e964871ec16b3cae4ad
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-05 17:49:08 -0500

    Create VSCode.ps1

---
+++ RHELCentOS/VSCode.ps1
@@ -0,0 +1,25 @@
+Class VSCode # To install VSCode on CentOS
+{
+    [String]       $Path = "/etc/yum.repos.d/vscode.repo" 
+    Hidden [String] $URL = "https://packages.microsoft.com"
+    [String]        $Key
+    [String]       $Repo
+    [Object]        $GPG
+
+    VSCode()
+    {
+        $This.Key  = "{0}/keys/microsoft.asc" -f $This.URL
+        $This.Repo = "{0}/yumrepos/vscode" -f $This.URL
+        $This.GPG = ("[code];name=Visual Studio Code;baseurl={0};enabled=1;gpgcheck=1;gpgkey={1}" -f $This.Key, $This.Repo -Split ';')
+    }
+
+    RPM()
+    {
+        sudo rpm --import $this.Key
+    }
+
+    [Void] Output()
+    {
+        Set-Content -Path $This.Path -Value $This.GPG -Verbose
+    }
+}

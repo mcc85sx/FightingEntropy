21b69c2820cf5fa1c6f9ca92359b4d12f6c6552b
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-05 18:53:24 -0500

    Create microsoftedge.ps1

---
+++ RHELCentOS/microsoftedge.ps1
@@ -0,0 +1,4 @@
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-dev.repo
+sudo dnf install microsoft-edge-dev

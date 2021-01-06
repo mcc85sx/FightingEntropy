c53cc1827ebded4cf5f1ea4ea2778ebf43945dc2
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-06 15:15:29 -0500

    Update SELinux.ps1

--- RHELCentOS/SELinux.ps1
+++ RHELCentOS/SELinux.ps1
@@ -2,15 +2,16 @@ Class SELinux
 {
     [String]      $Path = "/etc/sysconfig/selinux"
     [String[]] $Content
+    [String[]]  $Output
 
     SELinux()
     {
         $This.Content = Get-Content -Path $This.Path
-        $This.Content = $This.Content -Replace "SELINUX=enforcing","SELINUX=disabled"
+        $This.Output  = ($This.Content -join "`n").Replace("SELINUX=enforcing","SELINUX=disabled")
     }
 
     Save()
     {
-        Set-Content -Path $This.Path -Value $This.Content -Verbose
+        Set-Content -Path $This.Path -Value $This.Output -Verbose -Force
     }
 }

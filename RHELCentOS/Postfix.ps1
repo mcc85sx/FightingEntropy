7c5e047983eb8733cafd4e836e39cd1d2b6580aa
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-08 19:54:46 -0500

    Update Postfix.ps1

--- RHELCentOS/Postfix.ps1
+++ RHELCentOS/Postfix.ps1
@@ -1,55 +1,60 @@
-lass PostFix
+Class PostFix
 {
-    [String] $Hostname
-    [String]   $Domain
-    [String]  $Network
-    [String[]]   $List = @( )
-    [String]     $Path = "/etc/postfix/main.cf"
-    [String[]]  $Value
+    [String]  $Hostname
+    [String]    $Domain
+    [String]   $Network
+    [String]      $Path = "/etc/postfix/main.cf"
+    [String[]] $Content
+    [String[]]  $Output
 
     PostFix([String]$Network)
     {
         $This.Hostname = (hostname)
         $This.Domain   = (realm discover | ? { $_ -match "domain" } | % Substring 15)
         $This.Network  = $Network
+
+        If (!(Test-Path $This.Path))
+        {
+            Write-Host "Checking/Installing [:] Postfix"
+            sudo yum install postfix -y
+        }
+
+        $This.Content    = Get-Content -Path $This.Path
     }
 
-    Install()
+    Configure()
     {
-        sudo yum install postfix -y
-        $This.Value    = Get-Content -Path $This.Path
-    }
-
-    Swap()
-    {
-        $This.Value = $This.Value -Replace '#myhostname = host.domain.tld',
+        $This.Output = $This.Content
+        $This.Output = $This.Output -Replace '#myhostname = host.domain.tld',
         ('myhostname = {0}' -f $This.Hostname)
         
-        $This.Value = $This.Value -Replace '#mydomain = domain.tld',
+        $This.Output = $This.Output -Replace '#mydomain = domain.tld',
         ('mydomain = {0}' -f $This.Domain)
 
-        $This.Value = $This.Value -Replace "#myorigin = ",
+        $This.Output = $This.Output -Replace "#myorigin = ",
         "myorigin = "
         
-        $This.Value = $This.Value -Replace '#inet_interfaces = all', 
+        $This.Output = $This.Output -Replace '#inet_interfaces = all', 
         'inet_interfaces = all' 
         
-        $This.Value = $This.Value -Replace "#mydestination = ","mydestination = "
+        $This.Output = $This.Output -Replace "#mydestination = ",
+        "mydestination = "
 
-        $This.Value = $This.Value -Replace "(\#\s*mail\.)","mail."
+        $This.Output = $This.Output -Replace "(\#\s*mail\.)",
+        "mail."
 
-        $This.Value = $This.Value -Replace '#local_recipient_maps',
+        $This.Output = $This.Output -Replace '#local_recipient_maps',
         'local_recipient_maps'
 
-        $This.Value = $This.Value -Replace "#mynetworks_style = subnet",
+        $This.Output = $This.Output -Replace "#mynetworks_style = subnet",
         "mynetworks_style = subnet"
 
-        $This.Value = $This.Value -Replace "#mynetworks = 168.100.189.0/28, 127.0.0.0/8",
+        $This.Output = $This.Output -Replace "#mynetworks = 168.100.189.0/28, 127.0.0.0/8",
         ('mynetworks = {0}, 127.0.0.0/8' -f $This.Network)
     }
 
-    WriteCFG()
+    Save()
     {
-        Set-Content -Path $This.Path -Value $This.Value -Verbose -Force
+        Set-Content -Path $This.Path -Value $This.Output -Verbose -Force
     }
 }

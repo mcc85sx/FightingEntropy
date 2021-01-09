1a5ed6115c582b59376cabfc31d5a9ee7bb166a4
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-08 19:57:42 -0500

    Update RHELCentos.ps1

--- RHELCentOS/RHELCentos.ps1
+++ RHELCentOS/RHELCentos.ps1
@@ -1,11 +1,13 @@
 Class RHELCentOS
 {
+    [Object] $SELinux
     [Object] $Apache
     [Object] $PostFix
     [Object] $RoundCube
 
     RHELCentOS()
     {
+        $This.SELinux   = [SELinux]::New()
         $This.Apache    = [Apache]::New()
         $This.PostFix   = [PostFix]::New("192.168.1.0/24")
         $This.RoundCube = [RoundCube]::New()

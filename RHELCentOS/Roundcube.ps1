9c7cf4e0291ff923c68273ee3a3f6ac43972b3f0
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-09 08:27:12 -0500

    Update Roundcube.ps1

--- RHELCentOS/Roundcube.ps1
+++ RHELCentOS/Roundcube.ps1
@@ -49,12 +49,12 @@ Class RoundCube
     VirtualHost([String]$VHost)
     {
         $This.VirtualHostName = $VHost
-        $This.Path   = "/etc/httpd/conf.d/{0}" -f $This.VirtualHostname
-        $This.Output = ("<VirtualHost *:{0}>;  ServerName {1};  DocumentRoot {2};;  ErrorLog {3}/_error.log;" +
+        $This.Path   = "/etc/httpd/conf.d/{0}.conf" -f $This.VirtualHostname
+        $This.Output = (("<VirtualHost *:{0}>;  ServerName {1};  DocumentRoot {2};;  ErrorLog {3}/_error.log;" +
                         "CustomLog {3}/_access.log combined;;  <Directory />;    Options FollowSymLinks;    " +
                         "AllowOverride All;  </Directory>;;  <Directory {2}>;    Options FollowSymLinks Mult" + 
                         "iViews;    AllowOverride All;    Order allow,deny;    allow from all;  </Directory>" + 
-                        ";;</VirtualHost>") -f $This.Port,$This.ServerName,$This.Root,$This.Logs -Split ";"
+                        ";;</VirtualHost>") -f $This.Port,$This.ServerName,$This.Path,$This.Logs) -Split ";"
     }
 
     Save()

4c3ef1ebae51f597fcefcacfa4b7eb0c87196b45
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-16 16:39:30 -0500

    Create _Install.ps1

---
+++ 2021.1.1/Classes/_Install.ps1
@@ -0,0 +1,156 @@
+Class _Install
+{
+    [Object]                 $OS
+    [Object]           $Manifest
+    [Object]               $Hive
+
+    [String]               $Name = "FightingEntropy"
+    [String]            $Version = "2021.1.0"
+    [String]           $Provider = "Secure Digits Plus LLC"
+    [String]               $Date = (Get-Date -UFormat %Y_%m%d-%H%M%S)
+    [String]             $Status = "Initialized"
+    [Object]               $Type
+    
+    [String]           $Resource
+    [Object[]]          $Classes
+    [Object[]]        $Functions
+    [Object[]]          $Control
+    [Object[]]         $Graphics
+
+    Hidden [String[]]      $Load
+    Hidden [String]      $Output
+
+    BuildTree()
+    {
+        ForEach ( $Path in $This.Manifest.Folders )
+        {
+            $Item = $This.Hive.Path,$Path -join "\"
+            If ( ! ( Test-Path $Item ) )
+            {
+                New-Item $Item -ItemType Directory -Force -Verbose
+            }
+            
+            Switch ($Path)
+            {
+                Classes 
+                {
+                    ForEach ( $X in $This.Manifest.Classes )
+                    {
+                        $This.Classes   += ([_RestObject]::New("$($This.Resource)/Classes/$X","$($This.Hive.Path)/Classes/$X"))
+                    }
+                }
+
+                Functions
+                {
+                    ForEach ( $X in $This.Manifest.Functions )
+                    {
+                        $This.Functions += ([_RestObject]::New("$($This.Resource)/Functions/$X","$($This.Hive.Path)/Functions/$X"))
+                    }
+                }
+
+                Control
+                {
+                    ForEach ( $X in $This.Manifest.Control )
+                    {
+                        $This.Control   += ([_RestObject]::New("$($This.Resource)/Control/$X","$($This.Hive.Path)/Control/$X"))
+                    }
+                }
+
+                Graphics
+                {
+                    ForEach ( $X in $This.Manifest.Graphics )
+                    {
+                        $This.Graphics  += ([_RestObject]::New("$($This.Resource)/Graphics/$X","$($This.Hive.Path)/Graphics/$X"))
+                    }
+                }
+
+                Default {}
+            }
+        }
+    }
+    
+    BuildModule()
+    {
+        $This.Load              += "# FightingEntropy.psm1 [Module]"
+
+            "{0}.AccessControl {0}.Principal Management.Automation DirectoryServices" -f "Security" -Split " " | % { 
+
+            $This.Load          += "using namespace System.$_" 
+        }
+
+        $This.Load              += "using namespace Windows.UI.Notifications"
+        $This.Load              += "Add-Type -AssemblyName PresentationFramework"
+
+        $This.Manifest.Classes   | % { 
+
+            $This.Load          += ""
+            $This.Load          += "# Class/$_"
+            $This.Load          += @( Get-Content "$($This.Hive.Path)\Classes\$_" )
+        }
+
+        $This.Manifest.Functions | % {
+
+            $This.Load          += ""
+            $This.Load          += "# Function/$_"
+            $This.Load          += @( Get-Content "$($This.Hive.Path)\Functions\$_" )
+        }
+
+        $This.Output             = $This.Load -join "`n"
+        
+        $This.WriteModule()
+    }
+    
+    WriteModule()
+    {
+        Set-Content -Path $This.Hive.Module -Value $This.Output -Force -Verbose
+    }
+   
+    BuildManifest()
+    {
+        @{  GUID                 = "d2402c18-0529-4e55-919f-ac477c49d4fe"
+            Path                 = $This.Hive.Manifest
+            ModuleVersion        = $This.Hive.Version
+            Copyright            = "(c) 2021 mcc85sx. All rights reserved."
+            CompanyName          = "Secure Digits Plus LLC" 
+            Author               = "mcc85sx / Michael C. Cook Sr."
+            Description          = "Beginning the fight against Identity Theft, and Cybercriminal Activities"
+            RootModule           = $This.Hive.Module
+            
+        }                        | % { New-ModuleManifest @_ }
+        
+        Switch -Regex ($This.Type)
+        {
+            "Win32"      { $This.Scaffold("Program Files") } "RHELCentOS" { $This.Scaffold("microsoft") }
+        }
+    }
+    
+    Scaffold([String]$String)
+    {
+        ForEach ( $ModulePath in $This.Hive.PSModule )
+        {
+            If ( $ModulePath -match $String )
+            {
+                Copy-Item $This.Hive.Manifest -Destination $ModulePath -Verbose
+            }
+        }
+    }
+
+    _Install([String]$Version)
+    {
+        $This.OS                 = [_OS]::New()
+        $This.Type               = $This.OS.Type
+        $This.Manifest           = [_Manifest]::New()
+        $This.Hive               = [_Hive]::New($This.Type,$Version)
+
+        $This.Resource           = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/{0}" -f $This.Version
+        $This.Classes            = @( )
+        $This.Functions          = @( )
+        $This.Control            = @( )
+        $This.Graphics           = @( )
+        $This.Load               = @( )
+        
+        $This.BuildTree()
+        $This.BuildModule()
+        $This.BuildManifest()
+    }
+}

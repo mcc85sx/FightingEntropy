f89b9958c8b963cdb8ced0e6d19939392ccdb1ff
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-17 19:06:04 -0500

    Update _RestObject.ps1

--- 2021.1.1/Classes/_RestObject.ps1
+++ 2021.1.1/Classes/_RestObject.ps1
@@ -1,18 +1,18 @@
 Class _RestObject
 {
+    [String]           $Path
     [String]           $Type
     [String]           $Name
     [Object]         $Object
-    [String]           $Path
     Hidden [String]     $URI
     
     _RestObject([String]$URI,[String]$Outfile)
     {
+        $This.Path    = $Outfile.Replace("\","/")
         $This.Type    = $URI.Split("/")[-2]
         $This.Name    = $URI.Split("/")[-1]
         $This.URI     = $URI
-        $This.Object  = Invoke-RestMethod -URI $This.URI -Verbose
-        $This.Path    = $Outfile.Replace("\","/")
+        $This.Object  = Invoke-RestMethod -URI $This.URI -Outfile $This.Path -Passthru -Verbose
     }
 
     Content()

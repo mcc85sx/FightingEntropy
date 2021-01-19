02b5359a5e250d53372c499592bf5f3974929262
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-19 11:15:11 -0500

    Update _Win32_Server.ps1

--- 2021.1.1/Classes/_Win32_Server.ps1
+++ 2021.1.1/Classes/_Win32_Server.ps1
@@ -1,47 +1,84 @@
 Class _Win32_Server
 {
-    [String]                $Name = [Environment]::MachineName.ToLower()
+    [String]                $Name
     [String]                 $DNS
-    [String]             $NetBIOS = [Environment]::UserDomainName.ToLower()
+    [String]             $NetBIOS
 
     [String]            $Hostname
-    [String]            $Username = [Environment]::UserName
-    [Object]           $Principal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+    [String]            $Username
+    [Object]           $Principal
     [Bool]               $IsAdmin
-    [Object]             $Network
 
-    [Object]             $PSDrive
-    [Object]             $Profile
+    [String]             $Caption
+    [String]             $Version
+    [UInt32]               $Build
+    [UInt32]          $ReleasedID
+    [String]                $Code
+    [String]                 $SKU
+    [String]             $Chassis
+    [Object]               $Drive
+    [Object]             $Process
+    
+    [Object]             $Network
+    [Object]             $Service
+
     Hidden [String[]]      $Tools = ("ViperBomb Chocolatey MDT WinPE WinADK WDS IIS/BITS ASP.Net DNS DHCP ADDS" -Split " ")
     [Object]                $Tool
     [Object]             $Feature
-    [Object]             $Service
-    [Object]             $Process
 
     _Win32_Server()
     {
-        $This.DNS                 = @($Env:UserDNSDomain,"-")[!$env:USERDNSDOMAIN]
-        $This.Hostname            = @($This.Name;"{0}.{1}" -f $This.Name, $This.DNS)[(Get-CimInstance Win32_ComputerSystem).PartOfDomain].ToLower()
+        $This.Principal           = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
         $This.IsAdmin             = $This.Principal.IsInRole("Administrator") -or $This.Principal.IsInRole("Administrators")
         
         If ( $This.IsAdmin -eq 0 )
         {
             Throw "Must run as administrator"
         }
+
+        [_Info]::New()             | % { 
+
+            $This.Caption          = $_.Caption
+            $This.Version          = $_.Version
+            $This.Build            = $_.Build
+            $This.ReleasedID       = $_.ReleaseID
+            $This.Code             = "{0}/{1}" -f $_.CodeName,$_.Name
+            $This.SKU              = $_.SKU
+            $This.Chassis          = $_.Chassis
+        }
+
+        $This.Initialize()
+    }
+
+    Initialize()
+    {
+        $This.Name                = [Environment]::MachineName.ToLower()
+        $This.DNS                 = @($Env:UserDNSDomain,"-")[!$env:USERDNSDOMAIN]
+        $This.NetBIOS             = [Environment]::UserDomainName.ToLower()
+
+        $This.Hostname            = @($This.Name;"{0}.{1}" -f $This.Name, $This.DNS)[(Get-CimInstance Win32_ComputerSystem).PartOfDomain].ToLower()
+        $This.Username            = [Environment]::UserName
+        $This.Network             = (Get-FENetwork)
+        $This.Drive               = (Get-PSDrive)
     }
     
     GetServices()
     {
-        $This.Services  = (Get-FEService)
+        $This.Service             = (Get-FEService)
     }
     
     GetProcesses()
     {
-        $This.Processes = (Get-Process)
+        $This.Process             = (Get-Process)
     }
     
     GetNetwork()
     {
-        $This.Network   = (Get-FENetwork)
+        $This.Network             = (Get-FENetwork)
+    }
+
+    GetFeatures()
+    {
+        
     }
 }

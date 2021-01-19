2edfb0d5ff8f40a3f1ae392904779d1961e6ff14
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-19 08:34:29 -0500

    Update _Win32_Server.ps1

--- 2021.1.1/Classes/_Win32_Server.ps1
+++ 2021.1.1/Classes/_Win32_Server.ps1
@@ -1,18 +1,33 @@
 Class _Win32_Server
 {
-    [Object] $Host
-    [Object] $Info
-    [Object] $Tools
-    [Object] $Services
-    [Object] $Processes
-    [Object] $Network
-    [Object] $Control
+    [String]                $Name = [Environment]::MachineName.ToLower()
+    [String]                 $DNS
+    [String]             $NetBIOS = [Environment]::UserDomainName.ToLower()
+
+    [String]            $Hostname
+    [String]            $Username = [Environment]::UserName
+    [Object]           $Principal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+    [Bool]               $IsAdmin
+    [Object]             $Network
+
+    [Object]             $PSDrive
+    [Object]             $Profile
+    Hidden [String[]]      $Tools = ("ViperBomb Chocolatey MDT WinPE WinADK WDS IIS/BITS ASP.Net DNS DHCP ADDS" -Split " ")
+    [Object]                $Tool
+    [Object]             $Feature
+    [Object]             $Service
+    [Object]             $Process
 
     _Win32_Server()
     {
-        $This.Host      = (Get-FEHost)
-        $This.Info      = [_Info]::New()
-        $This.Tools     = ("ViperBomb Chocolatey MDT WinPE WinADK WDS IIS/BITS ASP.Net DNS DHCP ADDS" -Split " ")
+        $This.DNS                 = @($Env:UserDNSDomain,"-")[!$env:USERDNSDOMAIN]
+        $This.Hostname            = @($This.Name;"{0}.{1}" -f $This.Name, $This.DNS)[(Get-CimInstance Win32_ComputerSystem).PartOfDomain].ToLower()
+        $This.IsAdmin             = $This.Principal.IsInRole("Administrator") -or $This.Principal.IsInRole("Administrators")
+        
+        If ( $This.IsAdmin -eq 0 )
+        {
+            Throw "Must run as administrator"
+        }
     }
     
     GetServices()

bc570db75c21295e06a4ca2770f2907930490f39
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-01-20 17:41:40 -0500

    Update Remove-FEShare.ps1

--- 2021.1.1/Functions/Remove-FEShare.ps1
+++ 2021.1.1/Functions/Remove-FEShare.ps1
@@ -1,15 +1,10 @@
 Function Remove-FEShare
 {
-    [CmdLetBinding()]Param(
-        
-        [Parameter(Mandatory,Position=0)][String]$Name,
-        [Parameter(Mandatory,Position=1)][String]$Path
-    )
+    [CmdLetBinding()]Param([Parameter(Mandatory,Position=0)][String]$Path)
 
-    Import-Module ( ( Get-ChildItem -Path ( Get-ItemProperty -Path "HKLM:\Software\Microsoft\Deployment 4" ).Install_Dir "*Toolkit.psd1" -Recurse ).FullName )
+    Import-Module (Get-MDTModule)
 
-    Get-SMBShare           | ? Name -eq $Name -and Path -eq $Path | Remove-SMBShare -Force -EA 0
-    Get-MDTPersistentDrive | ? Path -eq $Path | % { Remove-MDTPersistentDrive -Name $_.Name -VB -EA 0 }
-    Remove-Item -Path $Path                                            -Recurse -Force -EA 0
-    Remove-Item -Path "HKLM:\SOFTWARE\Policies\Secure Digits Plus LLC" -Recurse -Force -EA 0
+    Get-SMBShare           | ? Path -eq $Path | Remove-SMBShare -Force -Verbose
+    Get-MDTPersistentDrive | ? Path -eq $Path | % { Remove-MDTPersistentDrive -Name $_.Name -Verbose }
+    Remove-Item -Path $Path -Verbose
 }

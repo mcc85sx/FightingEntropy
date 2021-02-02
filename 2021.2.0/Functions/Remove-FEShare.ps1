6c7c3e80ef2a0ae7c56461ff5abb93dc2358273a
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-02-01 21:44:19 -0500

    Update Remove-FEShare.ps1

--- 2021.2.0/Functions/Remove-FEShare.ps1
+++ 2021.2.0/Functions/Remove-FEShare.ps1
@@ -1,10 +1,11 @@
 Function Remove-FEShare
 {
-    [CmdLetBinding()]Param([Parameter(Mandatory,Position=0)][String]$Path)
-
-    Import-Module (Get-MDTModule)
-
-    Get-SMBShare           | ? Path -eq $Path | Remove-SMBShare -Force -Verbose
-    Get-MDTPersistentDrive | ? Path -eq $Path | % { Remove-MDTPersistentDrive -Name $_.Name -Verbose }
-    Remove-Item -Path $Path -Verbose
+    [CmdLetBinding()]Param(
+    [ValidateNotNullOrEmpty()]
+    [Parameter(ValueFromPipeline=$True,Mandatory)][Object]$Share)
+    
+    If (Get-PSDrive -Name $Share.Name)
+    {
+        Remove-PSDrive -Name $Share.Name
+    }
 }

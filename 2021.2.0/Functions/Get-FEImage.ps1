742de7827de83e977242e14ae6cdf3b7d40d4c24
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-02-09 12:29:21 -0800

    Update Get-FEImage.ps1

--- 2021.2.0/Functions/Get-FEImage.ps1
+++ 2021.2.0/Functions/Get-FEImage.ps1
@@ -1,4 +1,3 @@
-
 Function Get-FEImage
 {
     [CmdLetBinding()]Param(
@@ -87,7 +86,6 @@ Function Get-FEImage
             $This.Target = $Target
             $This.Drive  = [Char]( [Int32]( Get-Volume | ? DriveLetter | % DriveLetter )[-1] + 1 )
             $This.Store  = @( )
-            $This.Output = $This.GetOutput()
         }
 
         AddImage([String]$Type,[String]$Name)
@@ -103,7 +101,7 @@ Function Get-FEImage
             {
                 ForEach ( $Index in $Image.Index )
                 {
-                    $This.Output += [_ImageIndex]::new($Index,$Image.Path,"$($This.Target)\$($Image.DisplayName)[$Index]","$($Image.DisplayName)[$Index]")
+                    $This.Output += [_ImageIndex]::new($Index,$Image.Path,"$($This.Target)\$($Image.DisplayName)[$Index].wim","$($Image.DisplayName)[$Index]")
                 }
             }
         }
@@ -113,44 +111,64 @@ Function Get-FEImage
             $Item = $Null
             $Last = $Null
             $Swap = $Null
+            $Path = $Null
+            $Info = $Null
 
             If ( ! $This.Output )
             {
                 Throw "No images detected"
             }
 
-            ForEach ( $Image in $This.Output )
+            ForEach ( $Image in 0..($This.Output.Count - 1) )
             {
-                $Item = $Image.SourceImagePath
-                $Swap = Get-DiskImage $Item
+                $Item = $This.Output[$Image]
+                $Swap = Get-DiskImage -ImagePath $Item.SourceImagePath
 
                 If (!$Swap.Attached)
                 {
-                    If ( $Last )
+                    If ( $Last -ne $Null )
                     {
                         If ( Get-DiskImage -ImagePath $Last | % Attached )
                         {
-                            Dismount-DiskImage -ImagePath $Last
-                            Start-Sleep -Seconds 2
+                            Write-Theme "Dismounting... $Last"
+                            Dismount-DiskImage -ImagePath $Last -Verbose
+                            Start-Sleep -Seconds 10
                         }
                     }
 
-                    Write-Theme "Mounting... $Last"
-                    Mount-DiskImage -ImagePath $Item
+                    Write-Theme "Mounting... $($Item.SourceImagePath)"
+                    Mount-DiskImage -ImagePath $Item.SourceImagePath -Verbose
+
+                    $Info = Get-WindowsImage -ImagePath "$(Get-DiskImage -ImagePath $Item.SourceImagePath | Get-Volume | % DriveLetter ):\sources\install.wim" -Verbose
                 }
 
-                @{ 
-                    SourceIndex          = $Image.SourceIndex
-                    SourceImagePath      = "$($This.Drive):\sources\install.wim"
-                    DestinationImagePath = $Image.DestinationImagePath
-                    DestinationName      = $Image.DestinationName
-            
-                } | % { Export-WindowsImage @_ -Verbose }
+                $ISO                     = @{ 
 
-                $Last = $Item
+                    SourceIndex          = $Item.SourceIndex
+                    SourceImagePath      = $Item.SourceImagePath
+                    DestinationImagePath = $Item.DestinationImagePath
+                    DestinationName      = $Info | ? ImageIndex -eq $Item.SourceIndex | % ImageName
+                }
+
+                If ( $Iso.DestinationName -match "Server" )
+                {
+                    $Iso.DestinationName = "{0} (x64)" -f [Regex]::Matches($Iso.DestinationName,"(Windows Server )+(\d){4}( Datacenter| Standard)").Value
+                }
+
+                Else
+                {
+                    $Arch                = Switch -Regex ($Item.DestinationImagePath) { x64 {"64"} x32 {"86"} x86 {"86"} }
+                    $Iso.DestinationName = "{0} (x$Arch)" -f [Regex]::Matches($Iso.DestinationName,"(Windows 10 )+(Pro*|Education|Home)").Value
+                }
+                
+                Write-Theme "Extracting [~] $($ISO.DestinationImagePath)"
+
+                Export-WindowsImage @ISO
+
+                $Last = $Item.SourceImagePath
             }
 
-            Dismount-DiskImage -ImagePath $Item
+            Dismount-DiskImage -ImagePath $Item.SourceImagePath
         }
     }
 
@@ -168,4 +186,70 @@ Function Get-FEImage
     $Images.AddImage("Client","Win10_20H2_English_x32.iso")
     $Images.Store[$Index].AddMap((4,1,6))
     $Index ++
+
+    $Images.GetOutput()
+
+    $Item = $Null
+    $Last = $Null
+    $Swap = $Null
+    $Path = $Null
+    $Info = $Null
+
+    If ( ! $Images.Output )
+    {
+        Throw "No images detected"
+    }
+    
+    ForEach ( $Image in 0..($Images.Output.Count - 1 ) )
+    {
+        $Item = $Images.Output[$Image]
+        $Swap = Get-DiskImage -ImagePath $Item.SourceImagePath
+
+        If (!$Swap.Attached)
+        {
+            If ( $Last -ne $Null )
+            {
+                If ( Get-DiskImage -ImagePath $Last | % Attached )
+                {
+                    Write-Theme "Dismounting... $Last"
+                    Dismount-DiskImage -ImagePath $Last -Verbose
+                    Start-Sleep -Seconds 10
+                }
+            }
+
+            Write-Theme "Mounting... $($Item.SourceImagePath)"
+            Mount-DiskImage -ImagePath $Item.SourceImagePath -Verbose
+
+            $Letter = Get-DiskImage -ImagePath $Item.SourceImagePath | Get-Volume | % DriveLetter
+            $Path   = "$Letter`:\sources\install.wim"
+            $Info   = Get-WindowsImage -ImagePath $Path -Verbose
+        }
+
+        $ISO                     = @{ 
+
+            SourceIndex          = $Item.SourceIndex
+            SourceImagePath      = $Path
+            DestinationImagePath = $Item.DestinationImagePath
+            DestinationName      = $Info | ? ImageIndex -eq $Item.SourceIndex | % ImageName
+        }
+        
+        If ( $Iso.DestinationName -match "Server" )
+        {
+            $Iso.DestinationName = "{0} (x64)" -f [Regex]::Matches($Iso.DestinationName,"(Windows Server )+(\d){4}( Datacenter| Standard)").Value
+        }
+        
+        Else
+        {
+            $Arch                = Switch -Regex ($Swap.ImagePath) { x64 {"64"} x32 {"86"} x86 {"86"} }
+            $Iso.DestinationName = "Windows 10 {0} (x{1})" -f [Regex]::Matches($Iso.DestinationName,"(Pro|Education|Home)").Value, $Arch
+        }
+                
+        Write-Theme "Extracting [~] $($ISO.DestinationImagePath)"
+
+        Export-WindowsImage @ISO
+
+        $Last = $Item.SourceImagePath
+    }
+
+    Dismount-DiskImage -ImagePath $Item.SourceImagePath
 }

7d82484fb74128849f60e153965921207de64cc0
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-02-24 15:11:51 -0500

    Update _DeploymentShare.ps1

--- 2021.2.0/Classes/_DeploymentShare.ps1
+++ 2021.2.0/Classes/_DeploymentShare.ps1
@@ -1,164 +1,303 @@
-Class _DeploymentShare
+Function New-FEDeploymentShare
 {
-    [Object]            $Window
-    [Object]                $IO
-    [Object]          $Graphics
-    Hidden [Bool]     $IsDomain
-    [Object]            $Domain
-    [Object]          $Provider = "Secure Digits Plus"
-    [String]      $Organization
-    [String]        $CommonName
-    [String]        $ExternalIP = (Invoke-RestMethod http://ifconfig.me/ip)
-    [Object]              $Ping = (Invoke-RestMethod http://ipinfo.io/$(Invoke-RestMethod http://ifconfig.me/ip))
-    [String]          $Location
-    [String]            $Region
-    [String]           $Country
-    [String]            $Postal
-    [String]          $SiteLink
-    [String]          $TimeZone
-    [String]            $Branch
-    [String]             $Phone
-    [String]             $Hours
-    [String]           $Website
-    [String]              $Logo
-    [String]        $Background
-    [String]        $DCUsername
-    [SecureString]  $DCPassword
-    [SecureString]   $DCConfirm
-    [String]           $DNSName
-    [String]       $NetBIOSName
-    [String]            $OUName
-    [String]        $IISInstall
-    [String]           $IISName
-    [String]        $IISAppPool
-    [String]    $IISVirtualHost
-    [String]         $ISOSource
-    [String]         $WIMSource
-    [String]        $LMUsername
-    [SecureString]  $LMPassword
-    [SecureString]   $LMConfirm
-    [String]              $Path
-    [String]         $Sharename
-    [String]       $Description
-    [PSCredential]  $Credential
-    [Object]               $Key
-
-    # Main()
-    _DeploymentShare()
+    Class _DeploymentShare
     {
-        $This.Window     = Get-XamlWindow -Type FEShare
-        $This.IO         = $This.Window.IO
-        $This.Graphics   = Get-FEModule -Graphics
-        $This.IsDomain   = Get-CimInstance Win32_ComputerSystem | % PartOfDomain
+        [Object]            $Window
+        [Object]                $IO
+        [Object]          $Graphics
+        Hidden [Bool]     $IsDomain
+        [Object]            $Domain
+        [Object]          $Provider = "Secure Digits Plus"
+        [String]      $Organization
+        [String]        $CommonName
+        [String]        $ExternalIP = (Invoke-RestMethod http://ifconfig.me/ip)
+        [Object]              $Ping
+        [String]          $Location
+        [String]            $Region
+        [String]           $Country
+        [String]            $Postal
+        [String]          $SiteLink
+        [String]          $TimeZone
+        [String]            $Branch
+        [String]             $Phone
+        [String]             $Hours
+        [String]           $Website
+        [String]              $Logo
+        [String]        $Background
+        [String]        $DCUsername
+        [String]        $DCPassword
+        [String]         $DCConfirm
+        [String]           $DNSName
+        [String]       $NetBIOSName
+        [String]            $OUName
+        [Object]        $IISInstall
+        [String]           $IISName
+        [String]        $IISAppPool
+        [String]    $IISVirtualHost
+        [String]         $ISOSource
+        [String]         $WIMSource
+        [String]        $LMUsername
+        [String]        $LMPassword
+        [String]         $LMConfirm
+        [Object]        $MDTInstall
+        [String]              $Path
+        [String]         $Sharename
+        [String]       $Description
+        [PSCredential]  $Credential
+        [Object]               $Key
 
-        Switch([UInt32]($This.IsDomain))
+        _DeploymentShare()
         {
-            0 # Non-domain
-            {
-                $This.Domain               = "-"
-                $This.DNSName              = Get-Item Env:\UserDNSDomain | % Value
-                $This.IO._DNS.Text         = $This.DNSName
+            $This.Window      = Get-XamlWindow -Type FEShare
+            $This.IO          = $This.Window.IO
+            $This.Graphics    = Get-FEModule -Graphics
+            $This.GetGraphics()
+            $This.GetDomain()
+            $This.GetMDT()
+            $This.GetIIS()
 
-                $This.NetBIOSName          = Get-Item Env:\UserDomain    | % Value
-                $This.IO._NetBIOS.Text     = $This.NetBIOSName
-            }
+            $This.GetPing()
+            $This.GetSiteLink()
 
-            1 # Domain Member
-            {
-                Import-Module ActiveDirectory
-                $This.Domain               = Get-ADDomain
-                $This.DNSName              = $This.Domain.DNSRoot
-                $This.IO._DNS.Text         = $This.DNSName
-
-                $This.NetBIOSName          = $This.Domain.NetBIOSName
-                $This.IO._NetBIOS.Text     = $This.NetBIOSName
-            }
+            $This.Description          = $This.GetDate()
+            $This.IO._Description.Text = $This.Description
         }
 
-        $This.GetSiteLink($This.Ping)
-        $This.SetIIS()
-        $This.GetGraphics()
-    }
-
-    GetIIS()
-    {
-        If ($This.IO._InstallIIS.IsChecked)
+        GetGraphics()
         {
-            $This.InstallIIS = 0
+            $This.Logo                = $This.Graphics | ? Name -match OEMlogo.bmp | % FullName
+            $This.IO._Logo.Text       = $This.Logo
+
+            $This.Background          = $This.Graphics | ? Name -match OEMbg.jpg | % FullName
+            $This.IO._Background.Text = $This.Background
         }
 
-        If ($This.IO._InstallIIS.IsChecked)
+        GetDomain()
         {
-            $This.InstallIIS = 1
-        }
-    }
+            $This.IsDomain            = Get-CimInstance Win32_ComputerSystem | % PartOfDomain
 
-    SetIIS()
-    {
-        Switch ($This.IISInstall)
-        {
-            0
+            Switch([UInt32]($This.IsDomain))
             {
-                $This.IISName         = "N/A"
-                $This.IISAppPool      = "N/A"
-                $This.IISVirtualHost  = "N/A"
-            }
+                0 # Non-domain
+                {
+                    $This.Domain               = "-"
+                    $This.DNSName              = Get-Item Env:\UserDNSDomain | % Value
+                    $This.IO._DNS.Text         = $This.DNSName
 
-            1
-            {
-                $This.IISName         = "<Enter IIS Name>"
-                $This.IISAppPool      = "<Enter App Pool Name>"
-                $This.IISVirtualHost  = "<Enter Virtual Hostname>"
+                    $This.NetBIOSName          = Get-Item Env:\UserDomain    | % Value
+                    $This.IO._NetBIOS.Text     = $This.NetBIOSName
+                }
+
+                1 # Domain Member
+                {
+                    Import-Module ActiveDirectory
+                    $This.Domain               = Get-ADDomain
+                    $This.DNSName              = $This.Domain.DNSRoot
+                    $This.IO._DNS.Text         = $This.DNSName
+
+                    $This.NetBIOSName          = $This.Domain.NetBIOSName
+                    $This.IO._NetBIOS.Text     = $This.NetBIOSName
+                }
             }
         }
 
-        $This.IO._IISName.Text        = $This.IISName
-        $This.IO._IISAppPool.Text     = $This.IISAppPool
-        $This.IO._IISVirtualHost.Text = $This.IISVhost
+        GetMDT()
+        {
+            $This.MDTInstall = @("MDT","PSD")[$This.IO._MDTInstall.SelectedIndex]
+        }
+
+        GetIIS()
+        {
+            $This.IISInstall = @(0,1)[$This.IO._IISInstall.SelectedIndex]
+            
+            Switch ($This.IISInstall)
+            {
+                0
+                {
+                    $This.IISName                      = "N/A"
+                    $This.IO._IISName.IsEnabled        = $False
+                    $This.IISAppPool                   = "N/A"
+                    $This.IO._IISAppPool.IsEnabled     = $False
+                    $This.IISVirtualHost               = "N/A"
+                    $This.IO._IISVirtualHost.IsEnabled = $False
+                }
+
+                1
+                {
+                    $This.IISName                      = "<Enter IIS Name>"
+                    $This.IO._IISName.IsEnabled        = $True
+                    $This.IISAppPool                   = "<Enter App Pool Name>"
+                    $This.IO._IISAppPool.IsEnabled     = $True
+                    $This.IISVirtualHost               = "<Enter Virtual Hostname>"
+                    $This.IO._IISVirtualHost.IsEnabled = $True
+                }
+            }
+
+            $This.IO._IISName.Text        = $This.IISName
+            $This.IO._IISAppPool.Text     = $This.IISAppPool
+            $This.IO._IISVirtualHost.Text = $This.IISVirtualHost
+        }
+
+        GetPing()
+        {
+            $This.Ping              = Invoke-RestMethod "http://ipinfo.io/$($This.ExternalIP)"
+            $This.Location          = $This.Ping.City
+            $This.IO._Location.Text = $This.Ping.City
+            
+            $This.Region            = $This.Ping.Region
+            $This.IO._Region.Text   = $This.Ping.Region
+
+            $This.Country           = $This.Ping.Country
+            $This.IO._Country.Text  = $This.Ping.Country
+            
+            $This.Postal            = $This.Ping.Postal
+            $This.IO._Postal.Text   = $This.Ping.Postal
+            
+            $This.TimeZone          = $This.Ping.TimeZone
+            $This.IO._TimeZone.Text = $This.Ping.TimeZone
+
+            $This.SiteLink          = $This.GetSiteLink()
+            $This.IO._SiteLink.Text = $This.SiteLink
+        }
+         
+        [String] GetSiteLink()
+        {
+            $Return = @( )
+            $Return += ( $This.Ping.City -Split " " | % { $_[0] } ) -join ''
+            $Return += ( $This.Ping.Region -Split " " | % { $_[0] } ) -join ''
+            $Return += $This.Ping.Country
+            $Return += $This.Ping.Postal
+
+            Return @( $Return -join '-' )
+        }
+
+        [String] ToString()
+        {
+            Return $This.SiteLink
+        }
+
+        [String] GetBranch([String]$Domain)
+        {
+            Return "$($This.GetSiteLink()).$Domain".ToLower()
+        }
+
+        [String] FileDialog([String]$Default)
+        {
+            $Item = New-Object System.Windows.Forms.OpenFileDialog
+            $Item.InitialDirectory = $This.Graphics | Select-Object -Unique | % Directory | % FullName
+            $Item.ShowDialog()
+                
+            Return @( If ($Item.FileName) { $Item.Filename } Else { $Default } )
+        }
+
+        [String] GetDate()
+        {
+            Return @( Get-Date -UFormat "[%Y-%m%d(MCC/SDP)]")
+        }
+
+        [Bool] Validate([SecureString]$Password,[SecureString]$Confirm)
+        {
+            Return @( $Password -match $Confirm )
+        }
+
     }
 
-    GetGraphics()
-    {
-        $This.Logo                = $This.Graphics | ? Name -match OEMlogo.bmp | % FullName
-        $This.IO._Logo.Text       = $This.Logo
+    $Root = [_DeploymentShare]::New()
 
-        $This.Background          = $This.Graphics | ? Name -match OEMbg.jpg | % FullName
-        $This.IO._Background.Text = $This.Background
-    }
+    $Root.IO._Cancel.Add_Click({ $Root.IO.DialogResult = $False })
+    
+    $Root.IO._Start.Add_Click(
+    {  
+        $Root.DCUsername = $Root.IO._DCUsername.Text
+        If (!$Root.DCUsername)
+        {
+            [System.Windows.MessageBox]::Show("Invalid Username","[!] Error")
+        }
 
-    GetSiteLink([Object]$Ping)
-    {
-        $Return = @( )
-        $This.Location          = $Ping.City
-        $This.IO._Location.Text = $Ping.City
+        $Root.DCPassword      = $Root.IO._DCPassword.Password
+        $Root.DCConfirm       = $Root.IO._DCConfirm.Password
         
-        $This.Region            = $Ping.Region
-        $This.IO._Region.Text   = $Ping.Region
+        If ( $Root.DCPassword -notmatch $Root.DCConfirm )
+        {
+            [System.Windows.MessageBox]::Show("Invalid Password/Confirm","[!] Error")
+        }
 
-        $This.Country           = $Ping.Country
-        $This.IO._Country.Text  = $Ping.Country
-        
-        $This.Postal            = $Ping.Postal
-        $This.IO._Postal.Text   = $Ping.Postal
-        
-        $This.TimeZone          = $Ping.TimeZone
-        $This.IO._TimeZone.Text = $Ping.TimeZone
-        
-        $Return += ( $Ping.City -Split " " | % { $_[0] } ) -join ''
-        $Return += ( $Ping.Region -Split " " | % { $_[0] } ) -join ''
-        $Return += $Ping.Country
-        $Return += $Ping.Postal
+        $Root.Credential      = New-Object System.Management.Automation.PSCredential -ArgumentList $Root.DCUsername,$Root.IO._DCPassword.SecurePassword
 
-        $This.Sitelink = $Return -join '-'
-    }
+        $Root.LMUsername      = $Root.IO._LMUsername.Text
+        If (!$Root.LMUsername)
+        {
+            $Root.LMUsername  = "Administrator"
+        }
 
-    [String] ToString()
+        $Root.LMPassword      = $Root.IO._LMPassword.Password
+        $Root.LMConfirm       = $Root.IO._LMConfirm.Password
+        
+        If ( $Root.LMPassword -notmatch $Root.LMConfirm )
+        {
+            [System.Windows.MessageBox]::Show("Invalid Password/Confirm","[!] Error")
+        }
+
+        $Root.ISOSource       = $Root.IO._ImageRoot.Text
+        $Root.WIMSource       = $Root.IO._ImageSwap.Text
+        $Root.OUName          = $Root.IO._OU.Text
+        $Root.IO.DialogResult = $True 
+    })
+
+    $Root.IO._LogoDialog.Add_Click(
     {
-        Return $This.SiteLink
-    }
+        $Root.Logo            = $Root.FileDialog($Root.Logo)
+        $Root.IO._Logo.Text   = $Root.Logo
+    })
 
-    [String] GetBranch([String]$Domain)
+    $Root.IO._BackgroundDialog.Add_Click(
     {
-        Return "$($This.Sitelink).$Domain"
-    }
+        $Root.Background          = $Root.FileDialog($Root.Background)
+        $Root.IO._Background.Text = $Root.Background
+    })
+
+    $Root.IO._IISInstall.Add_SelectionChanged({ $Root.GetIIS() })
+    $Root.IO._MDTInstall.Add_SelectionChanged({ $Root.GetMDT() })
+
+    $Root.IO._Organization.Add_TextChanged(    
+    {
+        $Root.Organization        = $Root.IO._Organization.Text
+    })
+
+    $Root.IO._CommonName.Add_TextChanged(    
+    {
+        $Root.CommonName          = $Root.IO._CommonName.Text.ToString()
+        $Root.IO._CommonName.Text = $Root.CommonName
+
+        $Root.Branch              = ("{0}.{1}" -f ($Root.SiteLink -Replace "-","."),$Root.CommonName).ToLower()
+        $Root.IO._Branch.Text     = $Root.Branch
+    })
+
+    $Root.IO._Phone.Add_TextChanged(
+    { 
+        $Root.Phone               = $Root.IO._Phone.Text.ToString()
+    })
+
+    $Root.IO._Hours.Add_TextChanged(
+    {
+        $Root.Hours               = $Root.IO._Hours.Text.ToString()
+    })
+
+    $Root.IO._Website.Add_TextChanged(
+    {
+        $Root.Website             = $Root.IO._Website.Text.ToString()
+    })
+
+    $Root.IO._Path.Add_TextChanged(
+    {
+        $Root.Path                = $Root.IO._Path.Text.ToString()
+    })
+
+    $Root.IO._ShareName.Add_TextChanged(
+    {
+        $Root.ShareName           = $Root.IO._ShareName.Text.ToString()
+    })
+
+    $Root.Window.Invoke()
+    $Root
 }

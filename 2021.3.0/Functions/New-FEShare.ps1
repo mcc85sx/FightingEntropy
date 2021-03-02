87042dc335c6a3d850feb5266eac32477ad9e1ab
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-03-02 13:41:00 -0500

    Update New-FEShare.ps1

--- 2021.3.0/Functions/New-FEShare.ps1
+++ 2021.3.0/Functions/New-FEShare.ps1
@@ -45,7 +45,7 @@ Function New-FEShare
             Import-Module (Get-MDTModule)
 
             $This.Shares       = Get-MDTPersistentDrive
-            $This.Label        = If ($This.Shares) { "FE{0:d3}" -f ($This.Shares.Count + 1) } Else { "FE001" }
+            $This.Label        = Switch($This.Shares.Count) { 0 { "FE001" } Default { "FE{0:d3}" -f ($This.Shares.Count + 1) } }
             $This.Hostname     = Resolve-DNSName ([Environment]::MachineName) | % Name | Select-Object -Unique
             $This.ShareName    = "{0}$" -f $Name.TrimEnd("$")
             $This.NetworkPath  = "\\{0}\{1}" -f $This.HostName, $This.ShareName
@@ -121,90 +121,7 @@ Function New-FEShare
 
     ForEach ($File in $Key.Background, $Key.Logo)
     {
-        $Name = $File | Split-Path -Leaf
-        $Item = "$Script\$Name"
-
-        If (!(Test-Path $Item))
-        {
-            Copy-Item -Path $File -Destination $Script -Verbose
-        }
-
-        If ($File -notlike $Key.NetworkPath)
-        {
-            $Item = ("{0}\Scripts\$Name" -f $Key.NetworkPath)
-        }
-
-        Switch ($File)
-        {
-            $Key.Logo       { $Key.Logo       = $Item }
-            $Key.Background { $Key.Background = $Item }
-        }
-    }
-
-    [Net.ServicePointManager]::SecurityProtocol = 3072
-
-    $Install = @( ) 
-    $Install += "[Net.ServicePointManager]::SecurityProtocol = 3072"
-    $Install += (Invoke-RestMethod https://github.com/mcc85sx/FightingEntropy/blob/master/Install.ps1?raw=true)
-    $Install += "`$Key = '$($Key | ConvertTo-Json)'`n"
-    $Install += "`New-EnvironmentKey -Key `$Key | % Apply `n"
-
-    Set-Content -Path $Script\Install.ps1 -Value $Install -Force -Verbose
-
-    # Share Settings
-    Set-ItemProperty $Root -Name Comments    -Value $("[FightingEntropy({0})]{1}" -f [Char]960,(Get-Date -UFormat "[%Y-%m%d (MCC/SDP)]") ) -Verbose
-    Set-ItemProperty $Root -Name MonitorHost -Value $Share.HostName -Verbose
-
-    # Image Names/Background
-    $Names  = 64 , 86 | % { "Boot.x$_" } | % { "$_.Generate{0}ISO $_.{0}WIMDescription $_.{0}ISOName $_.BackgroundFile" -f "LiteTouch" -Split " " }
-    $Values = 64 , 86 | % { "$($Module.Name)(x$_)" } | % { "True;$_;$_.iso;$($Key.Background)" -Split ";" }
-
-    ForEach ( $X in 0..($Names.Count - 1 ) )
-    {
-        If ( ( Get-ItemProperty -Path $Root | % $Names[$X] ) -ne $Values[$X] )
-        {
-            Set-ItemProperty -Path $Root -Name $Names[$X] -Value $Values[$X] -Verbose
-        }
-    }
-
-    # Service
-    If (!(Get-CimInstance Win32_Service | ? Name -eq MDT_Monitor))
-    {
-        Enable-MDTMonitorService -EventPort 9800 -DataPort 9801 -Verbose
-    }
-
-    # Bootstrap
-    Export-Ini -Path $Control\Bootstrap.ini -Value @{ 
-
-        Settings           = @{ Priority             = "Default"                      }
-        Default            = @{ DeployRoot           = $Key.NetworkPath
-                                UserID               = $Credential.Username
-                                UserPassword         = $Credential.GetNetworkCredential().Password
-                                UserDomain           = $Key.CommonName
-                                SkipBDDWelcome       = "YES"                          }
-    }
-
-    # CustomSettings
-    Export-Ini -Path $Control\CustomSettings.ini -Value @{
-
-        Settings           = @{ Priority             = "Default" 
-                                Properties           = "MyCustomProperty" }
-        Default            = @{ _SMSTSOrgName        = $Key.Organization
-                                JoinDomain           = $Key.CommonName
-                                DomainAdmin          = $Credential.Username
-                                DomainAdminPassword  = $Credential.GetNetworkCredential().Password
-                                DomainAdminDomain    = $Key.CommonName
-                                MachineObjectOU      = "OU=Computers,DC=$($Key.CommonName.Split(".") -join ',DC=')"
-                                SkipDomainMembership = "YES"
-                                OSInstall            = "Y"
-                                SkipCapture          = "NO"
-                                SkipAdminPassword    = "YES" 
-                                SkipProductKey       = "YES" 
-                                SkipComputerBackup   = "NO" 
-                                SkipBitLocker        = "YES" 
-                                KeyboardLocale       = "en-US" 
-                                TimeZoneName         = Get-TimeZone | % ID
-                                EventService         = "http://{0}:9800" -f $Key.NetworkPath.Split("\")[2] }
+        Copy-Item -Path $File -Destination $Script -Verbose
     }
 
     ForEach ( $File in $Module.Control | ? Extension -eq .png )

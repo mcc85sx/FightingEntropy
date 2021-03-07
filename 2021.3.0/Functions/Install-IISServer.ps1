1e29c6212de0bc1f04c358947a50613d83c79641
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-03-07 15:57:00 -0500

    Update Install-IISServer.ps1

--- 2021.3.0/Functions/Install-IISServer.ps1
+++ 2021.3.0/Functions/Install-IISServer.ps1
@@ -2,7 +2,7 @@ Function Install-IISServer
 {
     Get-WindowsFeature | ? Name -in ( [_iisfeatures]::New().Features ) | ? Installed -eq 0 | % { 
         
-        Write-Host "{0}" -f $_.Name
+        Write-Host ("{0}" -f $_.Name)
         Install-WindowsFeature -Name $_.Name -IncludeAllSubFeature -IncludeManagementTools
     }
 }

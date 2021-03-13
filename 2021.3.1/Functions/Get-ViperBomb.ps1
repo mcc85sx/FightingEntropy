2e00e5383e2f7a6d032703aea5996fce76a80bcc
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-03-13 17:13:48 -0500

    Update Get-ViperBomb.ps1

--- 2021.3.1/Functions/Get-ViperBomb.ps1
+++ 2021.3.1/Functions/Get-ViperBomb.ps1
@@ -6,7 +6,7 @@ Function Get-ViperBomb
     Class _ViperBombConfig
     {
         [String]               $Name = "FightingEntropy/ViperBomb"
-        [String]            $Version = "2021.3.0"
+        [String]            $Version = "2021.3.1"
         [String]            $Release = "Development"
         [String]           $Provider = "Secure Digits Plus LLC"
         [String]                $URL = "https://github.com/mcc85sx/FightingEntropy"

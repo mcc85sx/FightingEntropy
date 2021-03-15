e1eb7ac8d52e2d99ea89e8b4bb6d07088a3761af
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-03-15 08:32:39 -0400

    Update _ADConnection.ps1

--- 2021.3.1/Classes/_ADConnection.ps1
+++ 2021.3.1/Classes/_ADConnection.ps1
@@ -1,40 +1,40 @@
 Class _ADConnection
-{
-    [Object] $Primary
-    [Object] $Secondary
-    [Object] $Swap
-    [Object] $Target
-    [Object] $Credential
-    [Object] $Output
-    [Object] $Return
-
-    _ADConnection([Object]$Hostmap)
     {
-        $This.Primary                        = $Hostmap | ? { "<1C>" -in $_.NBT.ID }
-        $This.Secondary                      = $Hostmap | ? { "<1B>" -in $_.NBT.ID }
-        $This.Swap                           = @( )
-        
-        $This.Target                         = $Null
-        $This.Credential                     = $Null
+        [Object] $Primary
+        [Object] $Secondary
+        [Object] $Swap
+        [Object] $Target
+        [Object] $Credential
+        [Object] $Output
+        [Object] $Return
 
-        If ( $This.Primary )
-        { 
-            $This.Swap += $This.Primary   
-        }
-        
-        If ( $This.Secondary )
-        { 
-            $This.Swap += $This.Secondary 
-        }
-
-        $This.Output                         = @( ) 
-        
-        ForEach ( $Item in $This.Swap ) 
+        _ADConnection([Object]$Hostmap)
         {
-            If ( $Item.IPAddress -notin $This.Output.IPAddress )
+            $This.Primary                        = $Hostmap | ? { "<1C>" -in $_.NBT.ID }
+            $This.Secondary                      = $Hostmap | ? { "<1B>" -in $_.NBT.ID }
+            $This.Swap                           = @( )
+        
+            $This.Target                         = $Null
+            $This.Credential                     = $Null
+
+            If ( $This.Primary )
+            { 
+                $This.Swap += $This.Primary   
+            }
+        
+            If ( $This.Secondary )
+            { 
+                $This.Swap += $This.Secondary 
+            }
+
+            $This.Output                         = @( ) 
+        
+            ForEach ( $Item in $This.Swap ) 
             {
-                $This.Output += $Item
+                If ( $Item.IPAddress -notin $This.Output.IPAddress )
+                {
+                    $This.Output += $Item
+                }
             }
         }
     }
-}

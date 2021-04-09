eb44d376756bf634ded4e585f0fcb5dd2aa94b87
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 06:18:10 -0400

    Create _Client.ps1

---
+++ cim-db/Classes/_Client.ps1
@@ -0,0 +1,86 @@
+Class _Client
+{
+    [Object]         $UID
+    [Object]       $Index
+    [Object]        $Slot
+    [Object]        $Type
+    [Object]        $Date
+    [Object]        $Time
+    [UInt32]        $Rank
+    [Object]        $Name
+    [String]       $First
+    [String]        $Last
+    [Object]         $DOB
+    [Object]     $Address
+    [Object]        $City
+    [Object]      $Region
+    [Object]     $Country
+    [Object]      $Postal
+    [Object[]]     $Phone
+    [Object[]]     $Email
+    [Object[]]    $Device
+    [Object[]]   $Invoice
+
+    GetStage()
+    {
+        $This.First   = $Null
+        $This.Last    = $Null
+        $This.DOB     = $Null
+        $This.Phone   = @( )
+        $This.Email   = @( )
+    }
+
+    AddPhone([Object]$Phone)
+    {
+        $This.Phone  += $Phone
+    }
+    
+    AddEmail([Object]$Email)
+    {
+        $This.Email  += $Email
+    }
+
+    SetStage([String]$First,[String]$Last,[Object]$DOB,[Object]$Phone,[Object]$Email)
+    {
+        $This.First   = $First
+        $This.Last    = $Last
+        $This.DOB     = $DOB
+    
+        $This.AddPhone($Phone)
+        $This.AddEmail($Email)
+    }
+
+    [String] GetName()
+    {
+        If (!$This.First -or !$This.Last)
+        {
+            If (!$This.First) 
+            { 
+                Throw "Missing first name" 
+            }
+
+            If (!$This.Last) 
+            {
+                Throw "Missing last name"  
+            }
+        }
+
+        Return "{0}, {1}" -f $This.Last, $This.First
+    }
+
+    SetName([String]$First,[String]$Last)
+    {
+        $This.Name = $This.GetName($First,$Last)
+    }
+
+    _Client([Object]$UID)
+    {
+        $This.UID  = $UID.UID
+        $This.Slot = 0
+        $This.Type = $UID.Type
+        $This.Date = $UID.Date
+        $This.Time = $UID.Time
+
+        $This.GetStage()
+    }
+}

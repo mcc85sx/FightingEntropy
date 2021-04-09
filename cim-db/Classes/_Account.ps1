5e80a0a967bebc1f0c67ecd367a974228d861e9a
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 06:25:52 -0400

    Create _Account.ps1

---
+++ cim-db/Classes/_Account.ps1
@@ -0,0 +1,21 @@
+Class _Account
+{
+    [Object]           $UID
+    [UInt32]         $Index
+    [Object]          $Slot
+    [Object]          $Type
+    [Object]          $Date
+    [Object]          $Time
+    [UInt32]          $Rank
+
+    [Object]        $Object
+
+    _Expense([Object]$UID) 
+    {
+        $This.UID  = $UID.UID
+        $This.Slot = 6
+        $This.Type = $UID.Type
+        $This.Date = $UID.Date
+        $This.Time = $UID.Time
+    }
+}

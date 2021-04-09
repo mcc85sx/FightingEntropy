af588f0f059fd44dca64181c51ba105e42950103
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 06:21:01 -0400

    Create _Purchase.ps1

---
+++ cim-db/Classes/_Purchase.ps1
@@ -0,0 +1,27 @@
+Class _Purchase
+{
+    [Object]           $UID
+    [UInt32]         $Index
+    [Object]          $Slot
+    [Object]          $Type
+    [Object]          $Date
+    [Object]          $Time
+    [UInt32]          $Rank
+
+    [Object]   $Distributor
+    [Object]   $DisplayName
+    [String]        $Vendor
+    [String]        $Serial
+    [String]         $Model
+    [Object]         $Title  
+    [Object]          $Cost
+
+    _Purchase([Object]$UID) 
+    {
+        $This.UID  = $UID.UID
+        $This.Slot = 5
+        $This.Type = $UID.Type
+        $This.Date = $UID.Date
+        $This.Time = $UID.Time
+    }
+}

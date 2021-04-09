1ba56e08f6553f6dbad24d71ce2718a3f15d1763
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 06:18:34 -0400

    Create _Service.ps1

---
+++ cim-db/Classes/_Service.ps1
@@ -0,0 +1,66 @@
+Class _Service
+{
+    [Object]           $UID
+    [UInt32]         $Index
+    [Object]          $Slot
+    [Object]          $Type
+    [Object]          $Date
+    [Object]          $Time
+    [UInt32]          $Rank     
+    [String]          $Name
+    [String[]] $Description
+    [Float]           $Cost
+
+    _Service([Object]$UID) 
+    { 
+        $This.UID  = $UID.UID
+        $This.Slot = 1
+        $This.Type = $UID.Type
+        $This.Date = $UID.Date
+        $This.Time = $UID.Time
+    }
+    
+    [Object] GetName([String]$Name)
+    {
+        If ( $Name -in $This.Service.Name )
+        {
+            Return @( $This.Service | ? Name -eq $Name )
+        }
+
+        Else 
+        {
+            Return $Null    
+        }
+    }
+
+    [Object] GetUID([Object]$UID)
+    {
+        If ( $UID -in $This.Service.UID )
+        {
+            Return @( $This.Service | ? UID -eq $UID )
+        }
+
+        Else 
+        {
+            Return $Null
+        }
+    }
+
+    SetName([String]$Name)
+    {
+        If ( $Name -notin $This.Service.Name )
+        {
+            $This.Name = $Name
+        }
+    }
+
+    SetDescription([String[]]$Description)
+    {
+        $This.Description = $Description
+    }
+
+    SetCost([Object]$Cost)
+    {
+        $This.Cost        = $Cost
+    }
+}

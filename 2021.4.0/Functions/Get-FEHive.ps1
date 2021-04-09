094e66dc41531bdbbd21c857905b2b66999b1d5a
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 14:36:48 -0400

    Update Get-FEHive.ps1

--- 2021.4.0/Functions/Get-FEHive.ps1
+++ 2021.4.0/Functions/Get-FEHive.ps1
@@ -4,7 +4,7 @@ Function Get-FEHive
     [ValidateSet("Win32_Client","Win32_Server","RHELCentOS","UnixBSD")]
     [Parameter(Mandatory)][String]$Type,
 
-    [ValidateSet("2021.1.0","2021.1.1","2021.2.0","2021.3.0","2021.3.1")]
+    [ValidateSet("2021.1.0","2021.1.1","2021.2.0","2021.3.0","2021.3.1","2021.4.0")]
     [Parameter(Mandatory)][String]$Version)
 
     [_Hive]::new($Type,$Version)

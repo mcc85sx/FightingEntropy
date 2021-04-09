fede9e3cff20c1b7620b8879adeb2097642f9907
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 09:22:22 -0400

    Update init.cim-db.ps1

--- cim-db/Functions/init.cim-db.ps1
+++ cim-db/Functions/init.cim-db.ps1
@@ -34,6 +34,11 @@ Function init.cim-db ([String]$Base)
             $This.Item    = $This.Index -Replace "\s+"," " -Split " "
             $This.Class   = $This.Item | % { [_ClassObject]::New($Base,$_) }
         }
+        
+        [String] ToString()
+        {
+            Return ( "init.cim-db -> {0}" -f $This.Index )
+        }
 
         BuildModule()
         {
@@ -46,7 +51,6 @@ Function init.cim-db ([String]$Base)
 
             $This.Module = ( $This.Module -join "`n" )
         }
-
     }
 
     $DB = [_Install]::New($Base)

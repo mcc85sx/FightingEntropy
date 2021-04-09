0563e2886751d0b46d10a0d3e3a416100fb09f61
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 06:21:21 -0400

    Create _Expense.ps1

---
+++ cim-db/Classes/_Expense.ps1
@@ -0,0 +1,24 @@
+Class _Expense
+{
+    [Object]           $UID
+    [UInt32]         $Index
+    [Object]          $Slot
+    [Object]          $Type
+    [Object]          $Date
+    [Object]          $Time
+    [UInt32]          $Rank
+
+    [Object]     $Recipient
+    [Object]   $DisplayName
+    [Object]       $Account
+    [UInt32]          $Cost
+
+    _Expense([Object]$UID) 
+    {
+        $This.UID  = $UID.UID
+        $This.Slot = 6
+        $This.Type = $UID.Type
+        $This.Date = $UID.Date
+        $This.Time = $UID.Time
+    }
+}

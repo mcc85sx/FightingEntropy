a64e15b4116e6214300ef159847b6f3e28929170
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-09 14:35:13 -0400

    Update _Module.ps1

--- 2021.4.0/Classes/_Module.ps1
+++ 2021.4.0/Classes/_Module.ps1
@@ -5,13 +5,13 @@ Class _Module
     [Object]               $Hive
     
     [String]               $Name = "FightingEntropy"
-    [String]            $Version = "2021.3.1"
+    [String]            $Version = "2021.4.0"
     [String]           $Provider = "Secure Digits Plus LLC"
     [String]               $Date
     [String]             $Status
     [String]               $Type
 
-    [String]           $Resource = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2021.3.1"
+    [String]           $Resource = "https://raw.githubusercontent.com/mcc85sx/FightingEntropy/master/2021.4.0"
     
     [Object[]]          $Classes
     [Object[]]        $Functions

3a1bf247a0734722e10d36cd930013292321bf92
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-04-25 09:45:11 -0400

    Create productkey.ps1

---
+++ scratch/productkey.ps1
@@ -0,0 +1,37 @@
+# https://answers.microsoft.com/en-us/windows/forum/windows_10-windows_install-winpc/how-to-recover-your-windows-product-key/8687ef5d-4d32-41fc-9310-158f8e5f02e3
+
+"$Home\Desktop\productkey.vbs" | % { 
+
+  Set-Content -Path $_ -Value @"
+Set WshShell = CreateObject("WScript.Shell")
+MsgBox ConvertToKey(WshShell.RegRead("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\DigitalProductId"))
+
+Function ConvertToKey(Key)
+Const KeyOffset = 52
+i = 28
+Chars = "BCDFGHJKMPQRTVWXY2346789"
+Do
+Cur = 0
+x = 14
+Do
+Cur = Cur * 256
+Cur = Key(x + KeyOffset) + Cur
+Key(x + KeyOffset) = (Cur \ 24) And 255
+Cur = Cur Mod 24
+x = x -1
+Loop While x >= 0
+i = i -1
+KeyOutput = Mid(Chars, Cur + 1, 1) & KeyOutput
+If (((29 - i) Mod 6) = 0) And (i <> -1) Then
+i = i -1
+KeyOutput = "-" & KeyOutput
+End If
+Loop While i >= 0
+ConvertToKey = KeyOutput
+End Function
+"@
+
+  Start-Process -FilePath $_
+  Remove-Item $_ 
+   
+}

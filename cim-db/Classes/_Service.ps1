4b58bfae830102cb0932acdcdb748d36e80bcb42
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-05-06 16:52:43 -0400

    Update _Service.ps1

--- cim-db/Classes/_Service.ps1
+++ cim-db/Classes/_Service.ps1
@@ -1,48 +1,30 @@
-Class _Service
-{
-    [Object]           $UID
-    [UInt32]         $Index
-    [Object]          $Slot
-    [Object]          $Type
-    [Object]          $Date
-    [Object]          $Time
-    [UInt32]          $Rank
-    [String]          $Name
-    [String[]] $Description
-    [Float]           $Cost
-
-    _Service([Object]$UID) 
-    { 
-        $This.UID  = $UID.UID
-        $This.Slot = 1
-        $This.Type = "Service"
-        $This.Date = $UID.Date
-        $This.Time = $UID.Time
-    }
-    
-    [Object] GetName([String]$Name)
+    Class _Service
     {
-        If ( $Name -in $This.Service.Name )
-        {
-            Return @( $This.Service | ? Name -eq $Name )
+        [Object]           $UID
+        [UInt32]         $Index
+        [Object]          $Slot
+        [Object]          $Type
+        [Object]          $Date
+        [Object]          $Time
+
+        [UInt32]          $Rank
+        [String]   $DisplayName
+
+        [String]          $Name
+        [String]   $Description
+        [Object]          $Cost
+
+        _Service([Object]$UID) 
+        { 
+            $This.UID  = $UID.UID
+            $This.Slot = 1
+            $This.Type = "Service"
+            $This.Date = $UID.Date
+            $This.Time = $UID.Time
         }
 
-        Else 
+        [String] ToString()
         {
-            Return $Null    
+            Return "Service"
         }
     }
-
-    [Object] GetUID([Object]$UID)
-    {
-        If ( $UID -in $This.Service.UID )
-        {
-            Return @( $This.Service | ? UID -eq $UID )
-        }
-
-        Else 
-        {
-            Return $Null
-        }
-    }
-}

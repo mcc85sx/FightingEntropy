b2e8d57393fdf586834c1f209b3ad6d1ec54fa02
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-05-06 16:54:45 -0400

    Update _Inventory.ps1

--- cim-db/Classes/_Inventory.ps1
+++ cim-db/Classes/_Inventory.ps1
@@ -1,27 +1,34 @@
-Class _Inventory
-{
-    [Object]           $UID
-    [UInt32]         $Index
-    [Object]          $Slot
-    [Object]          $Type
-    [Object]          $Date
-    [Object]          $Time
-    [UInt32]          $Rank
-        
-    [String]        $Vendor
-    [String]        $Serial
-    [String]         $Model
-    [Object]         $Title
-    [Bool]        $IsDevice
-    [Object[]]      $Device
-    [Object]          $Cost
-
-    _Inventory([Object]$UID) 
+    Class _Inventory
     {
-        $This.UID  = $UID.UID
-        $This.Slot = 4
-        $This.Type = "Inventory"
-        $This.Date = $UID.Date
-        $This.Time = $UID.Time
+        [Object]           $UID
+        [UInt32]         $Index
+        [Object]          $Slot
+        [Object]          $Type
+        [Object]          $Date
+        [Object]          $Time
+
+        [UInt32]          $Rank
+        [String]   $DisplayName
+
+        [String]        $Vendor
+        [String]        $Serial
+        [String]         $Model
+        [Object]         $Title
+        [Bool]        $IsDevice
+        [Object[]]      $Device
+        [Object]          $Cost
+
+        _Inventory([Object]$UID) 
+        {
+            $This.UID  = $UID.UID
+            $This.Slot = 4
+            $This.Type = "Inventory"
+            $This.Date = $UID.Date
+            $This.Time = $UID.Time
+        }
+
+        [String] ToString()
+        {
+            Return "Inventory"
+        }
     }
-}

2efc1f6f8cb0e55b6127992af309820ef0488b54
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-05-31 12:06:42 -0400

    Update BossMode.ps1

--- ISETheme/BossMode.ps1
+++ ISETheme/BossMode.ps1
@@ -1,4 +1,18 @@
-$Name    = "BossMode.StorableColorTheme.ps1xml"
-$Path    = "$home\Desktop\{0}" -f $Name
+$ID      = "BossMode" 
+$Ext     = "StorableColorTheme.ps1xml"
+$Name    = "$ID.$Ext"
 $Value   = "https://github.com/mcc85sx/FightingEntropy/blob/master/ISETheme/$Name`?raw=true"
-$Content = Invoke-WebRequest -Uri $Value -UseBasicParsing -OutFile $Path | % Content
+$Content = Invoke-WebRequest -Uri $Value -UseBasicParsing | % Content
+$Reg     = "HKCU:\Software\Microsoft\PowerShell\3\Hosts\PowerShellISE\ColorThemes"
+
+If ( ( Get-ItemProperty $Reg ) -ne $Null )
+{
+    If ( Get-ItemProperty $Reg | Get-Member | ? Name -match $ID )
+    {
+        Remove-ItemProperty $Reg -Name $ID -Verbose
+
+        Write-Host " [Flushing theme...] " -F 10 -B 12
+    }
+
+    Set-ItemProperty $Reg -Name $ID -Value $Content -Verbose
+}

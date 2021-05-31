8843b8dae01735e2750c22ebd10c54066e351a70
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-05-31 12:22:01 -0400

    Update BossMode.ps1

--- ISETheme/BossMode.ps1
+++ ISETheme/BossMode.ps1
@@ -5,14 +5,36 @@ $Value   = "https://github.com/mcc85sx/FightingEntropy/blob/master/ISETheme/$Nam
 $Content = Invoke-WebRequest -Uri $Value -UseBasicParsing | % Content
 $Reg     = "HKCU:\Software\Microsoft\PowerShell\3\Hosts\PowerShellISE\ColorThemes"
 
-If ( ( Get-ItemProperty $Reg ) -ne $Null )
+If ( ( Get-ItemProperty -Path $Reg ) -ne $Null )
 {
-    If ( Get-ItemProperty $Reg | Get-Member | ? Name -match $ID )
+    If ( Get-ItemProperty -Path $Reg | Get-Member | ? Name -match $ID )
     {
-        Remove-ItemProperty $Reg -Name $ID -Verbose
+        Remove-ItemProperty -Path $Reg -Name $ID -Verbose
 
         Write-Host " [Flushing theme...] " -F 10 -B 12
     }
 
-    Set-ItemProperty $Reg -Name $ID -Value $Content -Verbose
+    Set-ItemProperty -Path $Reg -Name $ID -Value $Content -Verbose
+}
+
+$XML     = [XML]( Get-ItemProperty -Path $Reg ).$ID
+
+$Array   = @( )
+$Hash    = @{ }
+
+ForEach ( $X in 0..72 )
+{
+    $ItemName  = $XML.StorableColorTheme.Keys.String[$X]
+    $ItemValue = $XML.StorableColorTheme.Values.Color[$X]
+
+    $Array    += $ItemName
+    $Hash.Add($ItemName,$ItemValue)
+}
+
+ForEach ( $Item in $Array )
+{
+    "________________________________________"
+    "[ $Item ]"
+    "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+    $Hash[$ItemName] | FT
 }

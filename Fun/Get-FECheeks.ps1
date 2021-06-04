728991ec284689e06e1710fd2ebe9dece2d4b5d8
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-06-04 11:09:39 -0400

    Create Get-FECheeks.ps1

---
+++ Fun/Get-FECheeks.ps1
@@ -0,0 +1,60 @@
+Function Get-FECheeks
+{
+    Class _Fun
+    {
+        [String] $Name
+        [String] $Url
+        [Object] $Content
+
+        _Fun([String]$Name)
+        {
+            $This.Name = $Name
+            $This.Url  = "https://github.com/mcc85sx/FightingEntropy/blob/master/Fun/$Name.txt?raw=true"
+            $This.Content = Invoke-WebRequest $This.Url -UseBasicParsing | % Content
+        }
+    }
+
+    $Stories = "&MicrosoftDeploymentToolkit","&TopShelfToolkit","&ShapedByFire","My&Briefcase"
+    $Titles  = $Stories -Replace "&",""
+
+    $Item    = @{ }
+    $Item.Add(0,@{"[Title]"="FightingEntropy";Version="2021.6.0";Company="Secure Digits Plus LLC";Author="Michael C. Cook Sr.";Description="Beginning the fight against ID theft and cybercrime"})
+    
+    Switch($Host.UI.PromptForChoice("Choose story","Select a story",$Stories,0))
+    {
+        0 
+        {
+            $Cap = $Titles[0] | % { [_Fun]::New($_) }
+            $Item.Add(1,@{"[Document]"= $Titles[0]})
+            $Item.Add(2,$Cap[0].Content -Split "`n")
+        }
+
+        1 
+        { 
+            $Cap = $Titles[1] | % { [_Fun]::New($_) }
+            $Item.Add(1,@{"[Document]"= $Titles[1]})
+            $Item.Add(2,$Cap[1].Content -Split "`n")
+        }
+
+        2 
+        {
+            $Cap = $Titles[2] | % { [_Fun]::New($_) }
+            $Item.Add(1,@{"[Document]"= $Titles[2]})
+            $Item.Add(2,$Cap[2].Content -Split "`n")
+        }
+
+        3 
+        { 
+            $Cap = $Titles[3] | % { [_Fun]::New($_) }
+            $Item.Add(1,@{"[Document]"= $Titles[3]})
+            $Item.Add(2,$Cap[3].Content -Split "`n")
+        }
+
+        Default
+        {
+            Write-Theme "[!] Error" 12,4,15,0
+        }
+    }
+
+    Write-Theme $Item
+}

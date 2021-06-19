87f41e84782ff7ca3b3a4bf9ded209dc1f17c49e
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-06-18 20:20:31 -0400

    Update Get-SSHUnixTree.ps1

--- LSMap/Get-SSHUnixTree.ps1
+++ LSMap/Get-SSHUnixTree.ps1
@@ -5,9 +5,10 @@ Function Get-SSHUnixTree
         [Parameter(Mandatory,Position=0)][String]$ComputerName,
         [Parameter(Mandatory,Position=1)][String]$KeyFile,
         [Parameter(Mandatory,Position=2)][String]$FilePath,
-        [Parameter()][PSCredential]$Credential
+        [Parameter()][PSCredential]$Credential,
     )
 
+    # Class
     Class _Item
     {
         [UInt32]$Index
@@ -26,20 +27,20 @@ Function Get-SSHUnixTree
         }
     }
 
-    Class _Stock
+    Class _Stack
     {
         [Object] $Stock
         [Object] $Stack
         _Stock([String]$ID)
         {
-            $This.Stock = @{ }
-            $Path       = $Null
+            $This.Stack       = @{ }
+            $Path             = $Null
 
             Invoke-SSHCommand -SessionID $ID -Command "cd .."
             Invoke-SSHCommand -SessionID $ID -Command "cd .."
             Invoke-SSHCommand -SessionID $ID -Command "ls -R /" | % Output | % { 
         
-                $String = $_
+                $String       = $_
             
                 Switch -Regex ($String)
                 {
@@ -47,42 +48,50 @@ Function Get-SSHUnixTree
                     {
                         $Type = "Folder"
                         $Path = $String -Replace ":",""
-                        $Name = @($Null,$String.Split("/")[-1])[$String.Split("/").Count -gt 1] -Replace ":",""
                     }
             
                     Default
                     {
                         $Type = "File"
-                        $Name = ($Path -Split "/" | Select-Object -Last 1)
                     }
                 }
+
+                $Name         = ($String -Split "/" | Select-Object -Last 1)
                 
                 $This.Stock.Add($This.Stock.Count,[_Item]::New($This.Stock.Count,$Type,$Path,$Name))
             }
+
+            [Void](Get-SSHSession | ? SessionID -match $ID | Remove-SSHSession)
 
             $This.Stack = ForEach ( $X in 0..($This.Stock.Count - 1))
             {
                 $This.Stock[$X]
             }
+
+            $This.Output($FilePath)
+        }
+
+        [Void] Output([String]$FilePath)
+        {
+            Set-Content $FilePath -Value @( 
+                
+                ForEach ($X in 0..($This.Stack.Count-1))
+                {
+                    $Item = $This.Stack[$X]
+                    $Item.Hex
+                    $Item.Type
+                    $Item.Path
+                    $Item.Name
+                    " "
+                }
+            ) -Verbose
         }
     }
 
     $Session    = New-SSHSession -ComputerName $ComputerName -KeyFile $KeyFile -Credential $Credential
     $ID         = $Session.SessionID
-    $Stack      = [_Stock] $ID | % Stack
-
-    Set-Content $FilePath -Value @( 
-    ForEach ($X in 0..($Stack.Count-1))
-    {
-        $Item = $Stack[$X]
-        $Item.Hex
-        $Item.Type
-        $Item.Path
-        $Item.Name
-        " "
-    }) -Verbose
-
-    [Void](Get-SSHSession | ? SessionID -match $ID | Remove-SSHSession)
+    $Stack      = [_Stock]::New($ID)
+    $Stack.Output($FilePath) 
 
     $Stack
 }

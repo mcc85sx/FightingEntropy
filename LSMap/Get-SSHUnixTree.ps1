3cd729e19af8dc666fa86edc2dc99e965b25f73a
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-06-19 12:00:27 -0400

    Update Get-SSHUnixTree.ps1

--- LSMap/Get-SSHUnixTree.ps1
+++ LSMap/Get-SSHUnixTree.ps1
@@ -16,62 +16,45 @@ Function Get-SSHUnixTree
         [String]$Path
         [String]$Name
         [Object]$Content
-        _Item([UInt32]$Index,[String]$Type,[String]$Path,[String]$Name)
+        _Item([UInt32]$Index,[String]$Type,[String]$Path)
         {
             $This.Index = $Index
             $This.Hex   = ("0x{0:X6}" -f $Index)
-            $This.Path  = $Path
-            $This.Name  = $Name
             $This.Type  = $Type
+            $This.Path  = $Path
+            $This.Name  = $Path -Split "(\/|\\)" | Select-Object -Last 1
         }
     }
+
+    $Time              = [System.Diagnostics.Stopwatch]::StartNew()
+    Write-Host "[$($Time.Elapsed)] Connecting [~]"
 
     $Session           = New-SSHSession -ComputerName $ComputerName -KeyFile $KeyFile -Credential $Credential
     $ID                = $Session.SessionID
+    $List              = Invoke-SSHCommand -SessionID $ID -Command "du -a /" | % Output
+
     $Stack             = @{ }
-
-    $Path              = $Null
-
-    [Void](Invoke-SSHCommand -SessionID $ID -Command "cd ..")
-    [Void](Invoke-SSHCommand -SessionID $ID -Command "cd ..")
-    Invoke-SSHCommand -SessionID $ID -Command "ls -R /" | % Output | % { 
+    Write-Host "[$($Time.Elapsed)] Populating [~] hashtable"
+    ForEach ( $X in 0..( $List.Count - 1 ) )
+    { 
+        $Item          = $List[$X] -Split "`t"
+        $Type          = $Item[0]
+        $Path          = $Item[1]
         
-        $String        = $_
-            
-        Switch -Regex ($String)
-        {
-            (\:)
-            {
-                $Type  = "Folder"
-                $Path  = $String -Replace ":",""
-            }
-            
-            Default
-            {
-                $Type  = "File"
-            }
-        }
+        # Write-Progress -Activity $Path -Status "[$X/$($List.Count)]" -PercentComplete (($X/$List.Count) * 100)
 
-        $Name          = ($String -Split "/" | Select-Object -Last 1)
-        If ( $Name[-1] -eq ":" )
-        {
-            $Name.TrimEnd(":")
-        }
-        $Stack.Add($Stack.Count,[_Item]::New($Stack.Count,$Type,$Path,$Name))
+        $Stack.Add($Stack.Count,[_Item]::New($Stack.Count,$Type,$Path))
     }
 
-    [Void](Get-SSHSession | ? SessionID -match $ID | Remove-SSHSession)
+    Write-Host "[$($Time.Elapsed)] [~] Assigning hashtable to array"
+    $Output = 0..($Stack.Count - 1) | % { $Stack[$_] }
 
-    $Stock = ForEach ( $X in 0..($Stack.Count - 1))
-    {
-        $Stack[$X]
-    }
-
+    Write-Host "[$($Time.Elapsed)] [~]   Writing [$FilePath]"
     Set-Content -LiteralPath $FilePath -Value @( 
             
-        ForEach ($X in 0..($Stock.Count-1))
+        ForEach ($X in 0..($Output.Count-1))
         {
-            $Item = $Stock[$X]
+            $Item = $Output[$X]
             $Item.Hex
             $Item.Type
             $Item.Path
@@ -80,5 +63,10 @@ Function Get-SSHUnixTree
         }
     ) -Verbose
 
-    $Stock
+    $Output
+
+    [Void](Get-SSHSession | ? SessionID -match $ID | Remove-SSHSession)
+
+    $Time.Stop()
+    Write-Host "[$($Time.Elapsed)] Complete [+]"
 }

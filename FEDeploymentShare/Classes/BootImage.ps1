118f7ae91704ba3aa8fb3e499fecd70c99265a3c
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-07-04 15:59:55 -0400

    Update BootImage.ps1

--- FEDeploymentShare/Classes/BootImage.ps1
+++ FEDeploymentShare/Classes/BootImage.ps1
@@ -6,22 +6,13 @@ Class BootImage
         [Object] $ISO
         [Object] $WIM
         [Object] $XML
-
         BootImage([String]$Path,[String]$Name)
         {
             $This.Path = $Path
             $This.Name = $Name
             $This.Type = Switch ([UInt32]($This.Name -match "\(x64\)")) { 0 { "x86" } 1 { "x64" } }
-            $Regex     = "($($This.Name -Replace "\(","\(" -Replace "\)","\)"))"
-
-            ForEach ( $Item in ( Get-ChildItem $Path | ? Name -match $Regex ) )
-            { 
-                Switch($Item.Extension)
-                {
-                    .iso { $This.ISO = $Item }
-                    .wim { $This.WIM = $Item }
-                    .xml { $This.XML = $Item }
-                }
-            }
+            $This.ISO  = "$Path\$Name.iso"
+            $This.WIM  = "$Path\$Name.wim"
+            $This.XML  = "$Path\$Name.xml"
         }
     }

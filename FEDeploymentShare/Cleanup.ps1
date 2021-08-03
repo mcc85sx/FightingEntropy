64592a9355a27b883681651895d4b60c65cc477e
mcc85sx <70099887+mcc85sx@users.noreply.github.com>
2021-08-03 14:31:54 -0400

    Update Cleanup.ps1

--- FEDeploymentShare/Cleanup.ps1
+++ FEDeploymentShare/Cleanup.ps1
@@ -5,7 +5,7 @@ $Code       = "(\w{2}\-){3}\d{5}"
 $Gateway    = Get-ADComputer -Filter * | ? Name -match $Code
 $Gateway    | Remove-ADComputer -Confirm:$False -Verbose
 $OUList     = Get-ADObject -LDAPFilter "(objectClass=organizationalUnit)" -SearchBase $Base | ? Name -match "Gateway|((\w{2}\-){3}\d{5})" 
-$OUList     | % { Set-ADObject -Identity $_.DistinguishedName -ProtectedFromAccidentalDeletion 0 -Verbose }
+$OUList     | ? Name -ne CP-NY-US-12065 | % { Set-ADObject -Identity $_.DistinguishedName -ProtectedFromAccidentalDeletion 0 -Verbose }
 $OUList     | Remove-ADObject -Confirm:$False -Recursive -Verbose -EA 0
 $Subnet     = Get-ADObject -LDAPfilter "(objectClass=subnet)" -SearchBase "CN=Configuration,$Base"
 $Subnet     | ? Name -ne 172.16.0.0/19  | Remove-ADObject -Confirm:$False -Verbose
@@ -16,11 +16,11 @@ $SiteList   | ? Name -ne CP-NY-US-12065 | Remove-ADObject -Confirm:$False -Recur
 # -------------------
 $Zone     = "securedigitsplus.com"
 $ScopeID  = Get-DhcpServerv4Scope | % ScopeID
-$DHCP     = @( Get-DhcpServerv4Lease -ScopeId $ScopeID | ? Hostname -match OPNsense )
+$DHCP     = Get-DhcpServerv4Reservation -ScopeID $ScopeID | ? Name -match "((\w{2}-){3}(\d{5})|OPNsense)"
 
 If ($DHCP.Count -gt 0)
 {
-    $DHCP | Remove-DHCPServerV4Lease -Verbose -EA 0
+    $DHCP | Remove-DHCPServerV4Reservation -Verbose -EA 0
 }
 
 # VM Guide Cleanup Script
